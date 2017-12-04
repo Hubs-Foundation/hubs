3e24705127db5f662e8f4cbac58251b44d7f56ca
Robert Long <robert@robertlong.me>
2017-12-04 15:00:05 -0800

    Add HandlebarsTemplatePlugin and asset helper.

--- webpack.common.js
+++ webpack.common.js
@@ -1,4 +1,6 @@
 const path = require("path");
+const HandlebarsTemplatePlugin = require("./templates/HandlebarsTemplatePlugin");
+const Handlebars = require("handlebars");
 
 module.exports = {
   entry: {
@@ -12,7 +14,7 @@ module.exports = {
   module: {
     rules: [
       {
-        test: /.js$/,
+        test: /\.js$/,
         include: [path.resolve(__dirname, "src")],
         exclude: [path.resolve(__dirname, "node_modules")],
         loader: "babel-loader"
@@ -22,5 +24,28 @@ module.exports = {
         use: ["style-loader", "css-loader"]
       }
     ]
-  }
+  },
+  plugins: [
+    new HandlebarsTemplatePlugin({
+      templatesPath: path.resolve(__dirname, "templates"),
+      helpers: {
+        /**
+         * Register a handlebars helper that prepends the base asset path.
+         * Useful for things like placing assets on a CDN and cache busting.
+         * Example:
+         * input: <img src="{{asset "asset.png"}}"/>
+         * output: <img src="https://cdn.mysite.com/asset.png?c="/>
+         */
+        asset: assetPath => {
+          const isProd = process.env.NODE_ENV === "production";
+          const baseAssetsPath = process.env.BASE_ASSETS_PATH || "/";
+          const cacheBustQueryString = isProd ? "?c=" + Date.now() : "";
+
+          const url = baseAssetsPath + assetPath + cacheBustQueryString;
+
+          return new Handlebars.SafeString(url);
+        }
+      }
+    })
+  ]
 };
