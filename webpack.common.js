e7f0a7c10c3ea5be73bdf24b3e27670f3cf2d59c
Robert Long <robert@robertlong.me>
2017-12-06 10:53:12 -0800

    Move templatre generation into common and add env var to generate smoke tests.

--- webpack.common.js
+++ webpack.common.js
@@ -1,4 +1,48 @@
 const path = require("path");
+const glob = require("glob");
+const HandlebarsTemplatePlugin = require("./templates/HandlebarsTemplatePlugin");
+const helpers = require("./templates/helpers");
+const config = require("./config");
+
+const templatePaths = glob.sync("./templates/*.hbs");
+const pages = [];
+
+for (const templatePath of templatePaths) {
+  const fileName = path.basename(templatePath, ".hbs");
+
+  // Generate html pages for each .hbs template in /templates
+  pages.push({
+    fileName: fileName + ".html",
+    templatePath,
+    data: {
+      baseAssetsPath: process.env.BASE_ASSETS_PATH,
+      config
+    }
+  });
+
+  // Generate html pages for smoke tests
+  if (process.env.GENERATE_SMOKE_TESTS) {
+    pages.push({
+      fileName: "smoke-" + fileName + ".html",
+      templatePath,
+      data: {
+        baseAssetsPath:
+          process.env.BASE_ASSETS_PATH &&
+          process.env.BASE_ASSETS_PATH.replace("https://", "https://smoke-"),
+        config: {
+          ...config,
+          global: {
+            ...config.global,
+            janus_server_url: config.global.janus_server_url.replace(
+              "wss://",
+              "wss://smoke-"
+            )
+          }
+        }
+      }
+    });
+  }
+}
 
 module.exports = {
   entry: {
@@ -22,5 +66,11 @@ module.exports = {
         use: ["style-loader", "css-loader"]
       }
     ]
-  }
+  },
+  plugins: [
+    new HandlebarsTemplatePlugin({
+      pages,
+      helpers
+    })
+  ]
 };
--- webpack.prod.js
+++ webpack.prod.js
@@ -1,55 +1,8 @@
 const merge = require("webpack-merge");
 const MinifyPlugin = require("babel-minify-webpack-plugin");
 const common = require("./webpack.common");
-const path = require("path");
-const config = require("./config");
-const HandlebarsTemplatePlugin = require("./templates/HandlebarsTemplatePlugin");
-const helpers = require("./templates/helpers");
 
 module.exports = merge(common, {
   devtool: "source-map",
-  plugins: [
-    new MinifyPlugin(),
-    new HandlebarsTemplatePlugin({
-      pages: [
-        {
-          fileName: "index.html",
-          templatePath: path.resolve(__dirname, "templates", "index.hbs"),
-          data: {
-            baseAssetsPath: process.env.BASE_ASSETS_PATH,
-            config
-          }
-        },
-        {
-          fileName: "room.html",
-          templatePath: path.resolve(__dirname, "templates", "room.hbs"),
-          data: {
-            baseAssetsPath: process.env.BASE_ASSETS_PATH,
-            config
-          }
-        },
-        {
-          fileName: "smoke-test.html",
-          templatePath: path.resolve(__dirname, "templates", "room.hbs"),
-          data: {
-            baseAssetsPath: process.env.BASE_ASSETS_PATH.replace(
-              "https://",
-              "https://smoke-"
-            ),
-            config: {
-              ...config,
-              global: {
-                ...config.global,
-                janus_server_url: config.global.janus_server_url.replace(
-                  "wss://",
-                  "wss://smoke-"
-                )
-              }
-            }
-          }
-        }
-      ],
-      helpers
-    })
-  ]
+  plugins: [new MinifyPlugin()]
 });
