28e10e69ef8d8811e8673aa7bf1ade403092c7da
netpro2k <netpro2k@gmail.com>
2017-12-07 14:22:56 -0800

    Add updated watch model

---
+++ src/components/bone-mute-state-indicator.js
@@ -0,0 +1,49 @@
+/**
+ * Toggles the position of 2 bones into "on" and "off" positions to indicate mute state.
+ */
+AFRAME.registerComponent("bone-mute-state-indicator", {
+  schema: {
+    unmutedBoneName: { default: "Watch_Joint_MicON" },
+    mutedBoneName: { default: "Watch_Joint_Muted" },
+    onPos: { default: 0.81 },
+    offPos: { default: 0.5 }
+  },
+
+  init() {
+    this.onStateToggled = this.onStateToggled.bind(this);
+    this.el.addEventListener("model-loaded", () => {
+      this.unmutedBone = this.el.object3D.getObjectByName(
+        this.data.unmutedBoneName
+      );
+      this.mutedBone = this.el.object3D.getObjectByName(
+        this.data.mutedBoneName
+      );
+      console.log(this.unmutedBone, this.mutedBone);
+      this.modelLoaded = true;
+
+      this.updateMuteState();
+    });
+  },
+
+  play() {
+    this.el.sceneEl.addEventListener("stateadded", this.onStateToggled);
+    this.el.sceneEl.addEventListener("stateremoved", this.onStateToggled);
+  },
+
+  pause() {
+    this.el.sceneEl.removeEventListener("stateadded", this.onStateToggled);
+    this.el.sceneEl.removeEventListener("stateremoved", this.onStateToggled);
+  },
+
+  onStateToggled(e) {
+    if (!e.detail.state === "muted") return;
+    this.updateMuteState();
+  },
+
+  updateMuteState() {
+    if (!this.modelLoaded) return;
+    const muted = this.el.sceneEl.is("muted");
+    this.mutedBone.position.y = muted ? this.data.onPos : this.data.offPos;
+    this.unmutedBone.position.y = !muted ? this.data.onPos : this.data.offPos;
+  }
+});
