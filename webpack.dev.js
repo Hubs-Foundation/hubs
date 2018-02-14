eb14dc7012624642f4128173d14ac6402153d034
Kevin Lee <klee@mozilla.com>
2018-02-14 15:21:30 -0600

    set date in header for dev server; update naf-janus-adapter to 0.3.0

--- webpack.dev.js
+++ webpack.dev.js
@@ -2,12 +2,24 @@ const path = require("path");
 const merge = require("webpack-merge");
 const common = require("./webpack.common");
 const WriteFilePlugin = require("write-file-webpack-plugin");
+const fs = require("fs");
 
 module.exports = merge(common, {
   devtool: "inline-source-map",
   devServer: {
     contentBase: path.resolve(__dirname, "public"),
-    disableHostCheck: true
+    disableHostCheck: true,
+    before: function (app) {
+      app.head('\*', function (req, res, next) {
+        if (req.method === 'HEAD') {
+          res.append("Date", (new Date()).toGMTString());
+          res.send('');
+        }
+        else {
+          next();
+        }
+      });
+    }
   },
   plugins: [new WriteFilePlugin()]
 });
