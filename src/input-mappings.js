697b0f27fb0258c72fa836d6099d891c37ab7374
netpro2k <netpro2k@gmail.com>
2018-04-10 18:18:25 -0700

    rename the vive/oclus dpad behaviours to be more generic, use them to rework the input schemas across platforms.

--- src/input-mappings.js
+++ src/input-mappings.js
@@ -21,29 +21,35 @@ const config = {
   behaviours: {
     default: {
       "oculus-touch-controls": {
-        joystick: "oculus_touch_joystick_dpad4"
+        joystick: "joystick_dpad4"
       },
       "vive-controls": {
-        trackpad: "vive_trackpad_dpad4"
+        trackpad: "trackpad_dpad4"
+      },
+      "daydream-controls": {
+        trackpad: "trackpad_dpad4"
+      },
+      "gearvr-controls": {
+        trackpad: "trackpad_dpad4"
       }
     }
   },
   mappings: {
     default: {
       "vive-controls": {
-        menudown: ["action_mute", "thumb_down"],
-        menuup: "thumb_up",
         "trackpad.pressedmove": { left: "move" },
         trackpad_dpad4_pressed_west_down: { right: "snap_rotate_left" },
         trackpad_dpad4_pressed_east_down: { right: "snap_rotate_right" },
         trackpad_dpad4_pressed_center_down: { right: "action_teleport_down" },
         trackpadup: { right: "action_teleport_up" },
-        gripdown: ["action_grab", "middle_ring_pinky_down", "index_down"],
-        gripup: ["action_release", "middle_ring_pinky_up", "index_up"],
+        menudown: "thumb_down",
+        menuup: "thumb_up",
+        gripdown: ["middle_ring_pinky_down", "index_down"],
+        gripup: ["middle_ring_pinky_up", "index_up"],
         trackpadtouchstart: "thumb_down",
         trackpadtouchend: "thumb_up",
-        triggerdown: "index_down",
-        triggerup: "index_up"
+        triggerdown: ["action_grab", "index_down"],
+        triggerup: ["action_release", "index_up"]
       },
       "oculus-touch-controls": {
         joystick_dpad4_west: {
@@ -52,7 +58,6 @@ const config = {
         joystick_dpad4_east: {
           right: "snap_rotate_right"
         },
-        xbuttondown: "action_mute",
         gripdown: ["action_grab", "middle_ring_pinky_down"],
         gripup: ["action_release", "middle_ring_pinky_up"],
         abuttontouchstart: "thumb_down",
@@ -67,20 +72,22 @@ const config = {
         surfacetouchend: "thumb_up",
         thumbsticktouchstart: "thumb_down",
         thumbsticktouchend: "thumb_up",
-        triggerdown: ["action_teleport_down", "index_down"],
-        triggerup: ["action_teleport_up", "index_up"],
+        triggerdown: "index_down",
+        triggerup: "index_up",
         "axismove.reverseY": { left: "move" },
-        right_dpad_east: "snap_rotate_right",
-        right_dpad_west: "snap_rotate_left",
         abuttondown: "action_teleport_down",
         abuttonup: "action_teleport_up"
       },
       "daydream-controls": {
-        trackpaddown: "action_teleport_down",
+        trackpad_dpad4_pressed_west_down: "snap_rotate_left",
+        trackpad_dpad4_pressed_east_down: "snap_rotate_right",
+        trackpad_dpad4_pressed_center_down: "action_teleport_down",
         trackpadup: "action_teleport_up"
       },
       "gearvr-controls": {
-        trackpaddown: "action_teleport_down",
+        trackpad_dpad4_pressed_west_down: "snap_rotate_left",
+        trackpad_dpad4_pressed_east_down: "snap_rotate_right",
+        trackpad_dpad4_pressed_center_down: "action_teleport_down",
         trackpadup: "action_teleport_up"
       },
       keyboard: {
@@ -100,14 +107,14 @@ const config = {
         s_up: "s_up",
         d_down: "d_down",
         d_up: "d_up",
-        W_down: "w_down",
-        W_up: "w_up",
-        A_down: "a_down",
-        A_up: "a_up",
-        S_down: "s_down",
-        S_up: "s_up",
-        D_down: "d_down",
-        D_up: "d_up"
+        arrowup_down: "w_down",
+        arrowup_up: "w_up",
+        arrowleft_down: "a_down",
+        arrowleft_up: "a_up",
+        arrowdown_down: "s_down",
+        arrowdown_up: "s_up",
+        arrowright_down: "d_down",
+        arrowright_up: "d_up"
       }
     },
     hud: {
@@ -116,8 +123,8 @@ const config = {
         triggerup: { right: "action_ui_select_up" }
       },
       "oculus-touch-controls": {
-        triggerdown: { right: "action_ui_select_down" },
-        triggerup: { right: "action_ui_select_up" },
+        abuttondown: "action_ui_select_down",
+        abuttonup: "action_ui_select_up",
         gripdown: "middle_ring_pinky_down",
         gripup: "middle_ring_pinky_up",
         abuttontouchstart: "thumb_down",
