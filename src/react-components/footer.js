abc605859aaa8d0b219d3af6454ac1538c7425bf
Brian Peiris <brianpeiris@gmail.com>
2018-04-16 08:28:43 -0700

    refactor footer into class

--- src/react-components/footer.js
+++ src/react-components/footer.js
@@ -1,26 +1,27 @@
-import React from "react";
+import React, { Component } from "react";
 import PropTypes from "prop-types";
 import FontAwesomeIcon from "@fortawesome/react-fontawesome";
 import faAngleLeft from "@fortawesome/fontawesome-free-solid/faAngleLeft";
 
 import styles from "../assets/stylesheets/footer.css";
 
-const Footer = ({ hubName, participantCount }) => (
-  <div className={styles.container}>
-    <span className={styles.hubName}>{hubName}</span>
-    <button className="{style.shareButton}" onClick={this.emitChangeToPrevious}>
-      <FontAwesomeIcon icon={faAngleLeft} />
-    </button>
-    <div>
-      <FontAwesomeIcon icon={faAngleLeft} />
-      <span>{participantCount}</span>
-    </div>
-  </div>
-);
-
-Footer.propTypes = {
-  hubName: PropTypes.string,
-  participantCount: PropTypes.number
-};
-
-export default Footer;
+export default class Footer extends Component {
+  static propTypes = {
+    hubName: PropTypes.string,
+    participantCount: PropTypes.number
+  };
+  render() {
+    return (
+      <div className={styles.container}>
+        <span className={styles.hubName}>{this.props.hubName}</span>
+        <button className="{styles.shareButton}" onClick={this.emitChangeToPrevious}>
+          <FontAwesomeIcon icon={faAngleLeft} />
+        </button>
+        <div>
+          <FontAwesomeIcon icon={faAngleLeft} />
+          <span>{this.props.participantCount}</span>
+        </div>
+      </div>
+    );
+  }
+}
