36d9e773962eb216bec847ab22af32d78f6b05fc
netpro2k <netpro2k@gmail.com>
2018-04-17 21:29:31 -0700

    Restyle 2d hud

--- src/react-components/2d-hud.js
+++ src/react-components/2d-hud.js
@@ -2,18 +2,19 @@ import React from "react";
 import PropTypes from "prop-types";
 import cx from "classnames";
 
-import styles from "../assets/stylesheets/2d-hud.css";
+import styles from "../assets/stylesheets/2d-hud.scss";
 
 const TwoDHUD = ({ muted, frozen, onToggleMute, onToggleFreeze }) => (
   <div className={styles.container}>
     <div className={cx("ui-interactive", styles.panel, styles.left)}>
-      <div className={cx(styles.mic, { [styles.muted]: muted })} onClick={onToggleMute} />
+      <div className={cx(styles.iconButton, styles.mute, { [styles.active]: muted })} onClick={onToggleMute} />
     </div>
-    <div className={cx("ui-interactive", styles.modeButton, { [styles.frozen]: frozen })} onClick={onToggleFreeze}>
-      <div className={styles.avatar} />
-    </div>
+    <div
+      className={cx("ui-interactive", styles.iconButton, styles.large, styles.freeze, { [styles.active]: frozen })}
+      onClick={onToggleFreeze}
+    />
     <div className={cx("ui-interactive", styles.panel, styles.right)}>
-      <div className={cx(styles.mic, { [styles.muted]: muted })} onClick={onToggleMute} />
+      <div className={cx(styles.iconButton, styles.bubble, { [styles.active]: muted })} onClick={onToggleMute} />
     </div>
   </div>
 );
