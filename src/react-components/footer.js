85022cf36ad4307f5e9140e964a9f108f17fe058
Brian Peiris <brianpeiris@gmail.com>
2018-04-20 17:21:18 -0700

    add and style menu button

--- src/react-components/footer.js
+++ src/react-components/footer.js
@@ -1,7 +1,8 @@
 import React, { Component } from "react";
 import PropTypes from "prop-types";
 import FontAwesomeIcon from "@fortawesome/react-fontawesome";
-import faShare from "@fortawesome/fontawesome-free-solid/faShare";
+import faEllipsisH from "@fortawesome/fontawesome-free-solid/faEllipsisH";
+import faShareAlt from "@fortawesome/fontawesome-free-solid/faShareAlt";
 import faUsers from "@fortawesome/fontawesome-free-solid/faUsers";
 
 import styles from "../assets/stylesheets/footer.scss";
@@ -16,8 +17,10 @@ export default class Footer extends Component {
       <div className={styles.container}>
         <div className={styles.hubInfo}>
           <span className={styles.hubName}>{this.props.hubName}</span>
-          <button className={styles.shareButton} onClick={this.emitChangeToPrevious}>
-            <FontAwesomeIcon icon={faShare} className={styles.shareButtonIcon} />
+        </div>
+        <div className={styles.menu}>
+          <button className={styles.menuButton}>
+            <FontAwesomeIcon icon={faEllipsisH} className={styles.menuButtonIcon} />
           </button>
         </div>
         <div className={styles.hubStats}>
