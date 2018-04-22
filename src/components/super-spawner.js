96ff21c5a1210654f03becc53bcd3de374b1a8c3
Kevin Lee <klee@mozilla.com>
2018-04-22 15:15:10 -0700

    move the event strings in super-spawner into attributes

--- src/components/super-spawner.js
+++ src/components/super-spawner.js
@@ -2,7 +2,8 @@ AFRAME.registerComponent("super-spawner", {
   schema: {
     template: { default: "" },
     useCustomSpawnPosition: { default: false },
-    spawnPosition: { type: "vec3" }
+    spawnPosition: { type: "vec3" },
+    events: { default: ["action_primary_down", "action_grab"] }
   },
 
   init: function() {
@@ -67,8 +68,9 @@ AFRAME.registerComponent("super-spawner", {
   _emitEvents: function(entity) {
     const data = this.entities.get(entity);
     if (data.componentInitialized && data.bodyLoaded) {
-      data.hand.emit("action_primary_down", { targetEntity: entity });
-      data.hand.emit("action_grab", { targetEntity: entity });
+      for (let i = 0; i < this.data.events.length; i++) {
+        data.hand.emit(this.data.events[i], { targetEntity: entity });
+      }
       const eventData = { bubbles: true, cancelable: true, detail: { hand: data.hand, target: entity } };
       const event = new CustomEvent("grab-start", eventData);
       entity.dispatchEvent(event);
