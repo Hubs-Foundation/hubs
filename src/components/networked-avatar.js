88406334f86de4966cdefec49036eb56ab88f3bf
joni <johnfshaughnessy@gmail.com>
2018-04-22 08:07:25 -0700

    Remove log

--- src/components/networked-avatar.js
+++ src/components/networked-avatar.js
@@ -2,8 +2,5 @@ AFRAME.registerComponent("networked-avatar", {
   schema: {
     left_hand_pose: { default: 0 },
     right_hand_pose: { default: 0 }
-  },
-  init() {
-    console.log("networked-avatar", this.el);
   }
 });
