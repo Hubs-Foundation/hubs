a951081c13c2522873671cd4462e36890352e62d
Greg Fodor <gfodor@gmail.com>
2018-04-22 20:15:07 -0700

    Fix up homepage on small phones in landscape

--- src/react-components/home-root.js
+++ src/react-components/home-root.js
@@ -172,10 +172,8 @@ class HomeRoot extends Component {
                   </a>
                 </div>
                 <div className="footer-content__links__bottom">
-                  <FormattedMessage id="home.made_with_love" />
-                  <span style={{ fontWeight: "bold", color: "white" }}>moz://a</span>
-                  <span>
-                    &nbsp;&nbsp;|&nbsp;&nbsp;Medieval Fantasy Book by{" "}
+                  <div>
+                    Medieval Fantasy Book by{" "}
                     <a
                       target="_blank"
                       rel="noreferrer noopener"
@@ -183,7 +181,11 @@ class HomeRoot extends Component {
                     >
                       Pixel
                     </a>
-                  </span>
+                  </div>
+                  <div>
+                    <FormattedMessage id="home.made_with_love" />
+                    <span style={{ fontWeight: "bold", color: "white" }}>moz://a</span>
+                  </div>
                 </div>
               </div>
             </div>
