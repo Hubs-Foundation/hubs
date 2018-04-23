f2ed8d90bbae43a54feed2adfd13dadcf207094b
Robert Long <robert@robertlong.me>
2018-04-23 14:16:40 -0700

    Update nav-mesh component and inflator to support precomputed nav-mesh

--- src/gltf-component-mappings.js
+++ src/gltf-component-mappings.js
@@ -1,7 +1,14 @@
 import "./components/gltf-model-plus";
+import { resolveURL } from "./utils/resolveURL";
 
 AFRAME.GLTFModelPlus.registerComponent("scale-audio-feedback", "scale-audio-feedback");
 AFRAME.GLTFModelPlus.registerComponent("loop-animation", "loop-animation");
 AFRAME.GLTFModelPlus.registerComponent("shape", "shape");
 AFRAME.GLTFModelPlus.registerComponent("visible", "visible");
-AFRAME.GLTFModelPlus.registerComponent("nav-mesh", "nav-mesh");
+AFRAME.GLTFModelPlus.registerComponent("nav-mesh", "nav-mesh", (el, componentName, componentData, gltfPath) => {
+  if (componentData.src) {
+    componentData.src = resolveURL(componentData.src, gltfPath);
+  }
+
+  el.setAttribute(componentName, componentData);
+});
