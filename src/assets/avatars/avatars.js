53240319c5e1f0ea2cebd57769944d1d69272cde
Robert Long <robert@robertlong.me>
2018-04-24 11:53:53 -0700

    Revert avatar assets url

--- src/assets/avatars/avatars.js
+++ src/assets/avatars/avatars.js
@@ -1,42 +1,42 @@
 export const avatars = [
   {
     id: "botdefault",
-    model: process.env.ASSET_SERVER + "/bots/BotDefault_Avatar-9f71f8ff22.gltf"
+    model: "https://asset-bundles-prod.reticulum.io/bots/BotDefault_Avatar-9f71f8ff22.gltf"
   },
   {
     id: "botbobo",
-    model: process.env.ASSET_SERVER + "/bots/BotBobo_Avatar-f9740a010b.gltf"
+    model: "https://asset-bundles-prod.reticulum.io/bots/BotBobo_Avatar-f9740a010b.gltf"
   },
   {
     id: "botdom",
-    model: process.env.ASSET_SERVER + "/bots/BotDom_Avatar-6aa1b5d781.gltf"
+    model: "https://asset-bundles-prod.reticulum.io/bots/BotDom_Avatar-6aa1b5d781.gltf"
   },
   {
     id: "botgreg",
-    model: process.env.ASSET_SERVER + "/bots/BotGreg_Avatar-98d39797bb.gltf"
+    model: "https://asset-bundles-prod.reticulum.io/bots/BotGreg_Avatar-98d39797bb.gltf"
   },
   {
     id: "botguest",
-    model: process.env.ASSET_SERVER + "/bots/BotGuest_Avatar-78cd857332.gltf"
+    model: "https://asset-bundles-prod.reticulum.io/bots/BotGuest_Avatar-78cd857332.gltf"
   },
   {
     id: "botjim",
-    model: process.env.ASSET_SERVER + "/bots/BotJim_Avatar-d28005a687.gltf"
+    model: "https://asset-bundles-prod.reticulum.io/bots/BotJim_Avatar-d28005a687.gltf"
   },
   {
     id: "botkev",
-    model: process.env.ASSET_SERVER + "/bots/BotKev_Avatar-a95787bb51.gltf"
+    model: "https://asset-bundles-prod.reticulum.io/bots/BotKev_Avatar-a95787bb51.gltf"
   },
   {
     id: "botpinky",
-    model: process.env.ASSET_SERVER + "/bots/BotPinky_Avatar-b0b93f8675.gltf"
+    model: "https://asset-bundles-prod.reticulum.io/bots/BotPinky_Avatar-b0b93f8675.gltf"
   },
   {
     id: "botrobert",
-    model: process.env.ASSET_SERVER + "/bots/BotRobert_Avatar-e9554880f3.gltf"
+    model: "https://asset-bundles-prod.reticulum.io/bots/BotRobert_Avatar-e9554880f3.gltf"
   },
   {
     id: "botwoody",
-    model: process.env.ASSET_SERVER + "/bots/BotWoody_Avatar-0140485a23.gltf"
+    model: "https://asset-bundles-prod.reticulum.io/bots/BotWoody_Avatar-0140485a23.gltf"
   }
 ];
