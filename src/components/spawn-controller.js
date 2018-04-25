de7dfd761b43392dbf22b33a0d4957bdc3a29874
Robert Long <robert@robertlong.me>
2018-04-24 18:47:33 -0700

    Fix spawn points

--- src/components/spawn-controller.js
+++ src/components/spawn-controller.js
@@ -1,5 +1,13 @@
 AFRAME.registerComponent("spawn-controller", {
+  schema: {
+    target: { type: "selector" },
+    loadedEvent: { type: "string" }
+  },
   init() {
+    this.onLoad = this.onLoad.bind(this);
+    this.data.target.addEventListener(this.data.loadedEvent, this.onLoad);
+  },
+  onLoad() {
     const spawnPoints = document.querySelectorAll("[spawn-point]");
 
     if (spawnPoints.length === 0) {
@@ -7,10 +15,10 @@ AFRAME.registerComponent("spawn-controller", {
       return;
     }
 
-    const spawnPointIndex = Math.round(spawnPoints.length * Math.random());
+    const spawnPointIndex = Math.round((spawnPoints.length - 1) * Math.random());
     const spawnPoint = spawnPoints[spawnPointIndex];
 
-    this.el.object3D.position.copy(spawnPoint.object3D.position);
+    spawnPoint.object3D.getWorldPosition(this.el.object3D.position);
     this.el.object3D.rotation.copy(spawnPoint.object3D.rotation);
   }
 });
