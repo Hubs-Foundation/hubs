d3c6873db460659a0f61b15148ca498b9da850a3
Robert Long <robert@robertlong.me>
2018-05-01 19:24:18 -0700

    Remove deprecated shadow properties

--- src/components/scene-shadow.js
+++ src/components/scene-shadow.js
@@ -1,21 +1,9 @@
 // For use in environment gltf bundles to set scene shadow properties.
 AFRAME.registerComponent("scene-shadow", {
   schema: {
-    autoUpdate: {
-      type: "boolean",
-      default: true
-    },
     type: {
       type: "string",
       default: "pcf"
-    },
-    renderReverseSided: {
-      type: "boolean",
-      default: true
-    },
-    renderSingleSided: {
-      type: "boolean",
-      default: true
     }
   },
   init() {
