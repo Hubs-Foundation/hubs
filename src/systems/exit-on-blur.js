3b028d61306d8b0acce2eba48e42541d6568d6d8
Robert Long <robert@robertlong.me>
2018-05-04 17:07:07 -0700

    Added hack to exit scene when Oculus Go goes into standby. Clean up virtual gamepad controls on exit.

--- src/systems/exit-on-blur.js
+++ src/systems/exit-on-blur.js
@@ -2,6 +2,9 @@ AFRAME.registerSystem("exit-on-blur", {
   init() {
     this.onBlur = this.onBlur.bind(this);
     this.onFocus = this.onFocus.bind(this);
+    this.onTimeout = this.onTimeout.bind(this);
+
+    this.isOculusBrowser = navigator.userAgent.match(/Oculus/);
 
     window.addEventListener("blur", this.onBlur);
     window.addEventListener("focus", this.onFocus);
@@ -9,11 +12,16 @@ AFRAME.registerSystem("exit-on-blur", {
     this.exitTimeout = null;
   },
 
+  tick() {
+    if (this.isOculusBrowser) {
+      clearTimeout(this.exitTimeout);
+      this.exitTimeout = setTimeout(this.onTimeout, 15 * 1000);
+    }
+  },
+
   onBlur() {
     if (this.el.isMobile) {
-      this.exitTimeout = setTimeout(() => {
-        this.el.dispatchEvent(new CustomEvent("exit"));
-      }, 30 * 1000);
+      this.exitTimeout = setTimeout(this.onTimeout, 30 * 1000);
     }
   },
 
@@ -21,6 +29,10 @@ AFRAME.registerSystem("exit-on-blur", {
     if (this.el.isMobile) {
       clearTimeout(this.exitTimeout);
     }
+  },
+
+  onTimeout() {
+    this.el.dispatchEvent(new CustomEvent("exit"));
   },
 
   remove() {
