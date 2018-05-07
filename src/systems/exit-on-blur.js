2c0c4fe19022e7abb79076d71c3bb332d36b22bc
Robert Long <robert@robertlong.me>
2018-05-07 16:57:09 -0700

    Added comment to explain Oculus Go hack and additional clearTimeout.

--- src/systems/exit-on-blur.js
+++ src/systems/exit-on-blur.js
@@ -13,6 +13,10 @@ AFRAME.registerSystem("exit-on-blur", {
   },
 
   tick() {
+    // This is a hack to detect when an Oculus Go user has taken off the headset and the headset has
+    // entered standby mode. Currently Oculus Browser is not emitting a blur, vrdisplaydeactivate,
+    // vrdisplayblur, visibilitychange, or vrdisplaypresentchange event, so we wait 15 seconds after
+    // the last requestAnimationFrame callback to determine if the headset has gone into standby mode.
     if (this.isOculusBrowser) {
       clearTimeout(this.exitTimeout);
       this.exitTimeout = setTimeout(this.onTimeout, 15 * 1000);
@@ -21,6 +25,7 @@ AFRAME.registerSystem("exit-on-blur", {
 
   onBlur() {
     if (this.el.isMobile) {
+      clearTimeout(this.exitTimeout);
       this.exitTimeout = setTimeout(this.onTimeout, 30 * 1000);
     }
   },
