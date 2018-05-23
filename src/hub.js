0af960f3d2180da09627e84d64ac70597584da3c
Greg Fodor <gfodor@gmail.com>
2018-05-22 22:07:51 -0700

    Copy hack into hub.js

--- src/hub.js
+++ src/hub.js
@@ -6,6 +6,12 @@ import { patchWebGLRenderingContext } from "./utils/webgl";
 patchWebGLRenderingContext();
 
 import "aframe-xr";
+
+// HACK: this has to run after A-Frame but before our bundle, since A-Frame blows away the local storage setting
+if (/[&?]debug=true/.test(window.location.search) && typeof localStorage !== "undefined") {
+  localStorage.debug = "naf-janus-adapter:*";
+}
+
 import "./vendor/GLTFLoader";
 import "networked-aframe/src/index";
 import "naf-janus-adapter";
