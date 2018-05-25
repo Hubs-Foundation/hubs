d28f1a83e144a12bd4e1ba5e976142fc91e022ed
j-conrad <31968496+j-conrad@users.noreply.github.com>
2018-05-25 14:45:50 -0700

    corrected upper vs. lowercase in rooftop URL

--- src/assets/environments/environments.js
+++ src/assets/environments/environments.js
@@ -2,7 +2,7 @@ export const ENVIRONMENT_URLS = [
   process.env.ASSET_BUNDLE_SERVER + "/rooms/meetingroom/MeetingRoom.bundle.json",
   process.env.ASSET_BUNDLE_SERVER + "/rooms/atrium/Atrium.bundle.json",
   process.env.ASSET_BUNDLE_SERVER + "/rooms/MedievalFantasyBook/MedievalFantasyBook.bundle.json",
-  process.env.ASSET_BUNDLE_SERVER + "/rooms/RooftopBuilding1/RooftopBuilding1.bundle.json"
+  process.env.ASSET_BUNDLE_SERVER + "/rooms/rooftopbuilding1/RooftopBuilding1.bundle.json"
 ];
 
 export const DEFAULT_ENVIRONMENT_URL = ENVIRONMENT_URLS[0];
