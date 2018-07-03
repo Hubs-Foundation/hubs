3b844422fc2f1c882c694c550d37d091baf5af18
tony056 <ytung@mozilla.com>
2018-07-03 14:52:16 -0700

    added fadingEffect for entering/exiting tunnel vision.

--- src/systems/tunnel-effect.js
+++ src/systems/tunnel-effect.js
@@ -7,12 +7,13 @@ import "./shaderlib/VignetteShader";
 
 AFRAME.registerSystem ('tunneleffect', {
   schema: {
-    checkThresholdMs: { type: 'number', default: 500 },
-    vignetteFadingMs: { type: 'number', default: 1200},
+    checkThresholdMs: { type: 'number', default: 200 },
+    vignetteFadingMs: { type: 'number', default: 800 },
     movingEvent: { type: 'string', default: 'move' },
-    radius: { type: 'number', default: 0.9, min: 0.5},
-    softness: { type: 'number', default: 0.1, min: 0.0},
-    opacity: { type: 'number', default: 0.9, min: 0.0}
+    radius: { type: 'number', default: 0.9, min: 0.5 },
+    minRadius: { type: 'number', default: 0.5, min: 0.1 },
+    softness: { type: 'number', default: 0.1, min: 0.0 },
+    opacity: { type: 'number', default: 0.9, min: 0.0 }
   },
 
   init: function () {
@@ -26,7 +27,7 @@ AFRAME.registerSystem ('tunneleffect', {
     this.fadingMs = data.vignetteFadingMs;
     this.initMs = Date.now();
     this.radius = data.radius;
-    this.minRadius = 0.5;
+    this.minRadius = 0.65;
     this.softness = data.softness;
     this.opacity = data.opacity;
     this.movingStartTimeMs = 0;
@@ -51,24 +52,18 @@ AFRAME.registerSystem ('tunneleffect', {
   },
 
   tick: function (time, deltaTime) {
-    if (this.isMoving) {
-      if (time - this.movingStartTimeMs < this.fadingMs) {
-        console.log('fading');
-        const progress = (time - this.movingStartTimeMs) / this.fadingMs;
-        const r = this.radius - (this.radius - this.minRadius) * progress;
-        this._updateVignettePass(r, this.softness, this.opacity);
-      } else {
-        if (time - this.lastMovingTimeMs > this.thresholdMs) {
-          console.log('idle');
-          this.isMoving = false;
-          this.scene.renderer.render = this.originalRenderFunc;
-          this.lastMovingTimeMs = time;
-          this.movingStartTimeMs = time;
-        }
-      }
+    if (!this.isMoving) { return; }
+    if (time - this.movingStartTimeMs < this.fadingMs) {
+      this._fadeInEffect(time, this.movingStartTimeMs, this.fadingMs);
     } else {
-      if (this.scene.renderer.render !== this.originalRenderFunc) {
+      if (time - this.lastMovingTimeMs < this.thresholdMs) { return; }
+      if (time - this.lastMovingTimeMs < this.fadingMs / 1.5) {
+        this._fadeOutEffect(time, this.lastMovingTimeMs, this.fadingMs / 1.5);
+      } else {
+        this.isMoving = false;
         this.scene.renderer.render = this.originalRenderFunc;
+        this.lastMovingTimeMs = time;
+        this.movingStartTimeMs = time;
       }
     }
   },
@@ -81,6 +76,7 @@ AFRAME.registerSystem ('tunneleffect', {
     if (!this.composer) {
       this.composer = new THREE.EffectComposer(this.renderer);
     }
+    this.composer.resize();
     if (!this.scenePass) {
       this.scenePass = new THREE.RenderPass(this.scene.object3D, this.camera);
     }
@@ -100,7 +96,7 @@ AFRAME.registerSystem ('tunneleffect', {
     this.vignettePass.uniforms['radius'].value = radius;
     this.vignettePass.uniforms['softness'].value = softness;
     this.vignettePass.uniforms['opacity'].value = opacity;
-    this.vignettePass['resolution'] = new THREE.Uniform(new THREE.Vector2(width * pixelRatio, height * pixelRatio));
+    this.vignettePass['resolution'] = new THREE.Uniform(new THREE.Vector2(width * pixelRatio , height * pixelRatio));
     if (!this.vignettePass.renderToScreen) {
       this.vignettePass.renderToScreen = true;
     }
@@ -121,6 +117,21 @@ AFRAME.registerSystem ('tunneleffect', {
         isDigest = false;
       }
     };
+  },
+
+  _fadingEffect: function (currentTime, baseTime, fadingDuration, originRadius, targetRadius) {
+    const progress = (currentTime - baseTime) / fadingDuration;
+    const deltaR = (originRadius - targetRadius) * progress;
+    const r = this.radius - deltaR;
+    this._updateVignettePass(r, this.softness, this.opacity);
+  },
+
+  _fadeOutEffect: function (currentTime, baseTime, fadingDuration) {
+    this._fadingEffect(currentTime, baseTime, fadingDuration, this.minRadius, this.radius);
+  },
+
+  _fadeInEffect: function (currentTime, baseTime, fadingDuration) {
+    this._fadingEffect(currentTime, baseTime, fadingDuration, this.radius, this.minRadius);
   },
 
   enableTunnelEffect: function () {
