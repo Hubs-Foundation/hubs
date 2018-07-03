998845156f903785b59bdbc1628e575a742656c4
tony056 <ytung@mozilla.com>
2018-07-03 14:53:29 -0700

    included post processing essential files.

---
+++ src/systems/shaderlib/VignetteShader.js
@@ -0,0 +1,44 @@
+
+THREE.VignetteShader = {
+
+	uniforms: {
+
+		"tDiffuse": { value: null },
+		"radius": { value: 0.65 },
+    "opacity": { value: 0.9 },
+    "softness": { value: 0.2 },
+    "resolution": new THREE.Uniform(new THREE.Vector2(1920, 1080))
+
+	},
+
+	vertexShader: [
+		"varying vec2 vUv;",
+		"void main() {",
+			"vUv = uv;",
+			"gl_Position = projectionMatrix * modelViewMatrix * vec4( position, 1.0 );",
+		"}"
+
+	].join( "\n" ),
+
+	fragmentShader: [
+		"uniform sampler2D tDiffuse;",
+    "uniform float radius;",
+    "uniform float opacity;",
+    "uniform float softness;",
+    "uniform vec2 resolution;",
+
+		"varying vec2 vUv;",
+
+		"void main() {",
+			"vec4 texel = texture2D( tDiffuse, vUv);",
+			"vec2 uv = (vUv.xy) - vec2(0.5);",
+      "uv.x *= resolution.x / resolution.y;",
+      "float len = length(uv);",
+      "float vignette = smoothstep(radius, radius-softness, len);",
+      "texel.rgb = mix (texel.rgb, texel.rgb * vignette, opacity);",
+      "gl_FragColor = vec4(texel.rgb, 1.0);",
+		"}"
+
+	].join( "\n" )
+
+};
