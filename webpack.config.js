efb815db7919b8441728a82c870dc316702b49a9
Marshall Quander <marshall@quander.me>
2018-07-23 14:39:08 -0700

    Upgrade other random deps

--- webpack.config.js
+++ webpack.config.js
@@ -157,7 +157,6 @@ const config = {
               loader: "css-loader",
               options: {
                 name: "[path][name]-[hash].[ext]",
-                minimize: process.env.NODE_ENV === "production",
                 localIdentName: "[name]__[local]__[hash:base64:5]",
                 camelCase: true
               }
@@ -174,7 +173,6 @@ const config = {
             loader: "css-loader",
             options: {
               name: "[path][name]-[hash].[ext]",
-              minimize: process.env.NODE_ENV === "production",
               localIdentName: "[name]__[local]__[hash:base64:5]",
               camelCase: true
             }
