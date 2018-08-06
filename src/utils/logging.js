47b1a1a283a770465715ce2a6f57034f39ff0b9b
Greg Fodor <gfodor@gmail.com>
2018-08-06 21:48:38 +0000

    Break out qsTruthy

--- src/utils/logging.js
+++ src/utils/logging.js
@@ -1,15 +1,9 @@
 // A-Frame blows away any npm debug log filters so this allow the user to set the log filter
 // via the query string.
 import debug from "debug";
+import qsTruthy from "./qs_truthy";
 
 const qs = new URLSearchParams(location.search);
-
-function qsTruthy(param) {
-  const val = qs.get(param);
-  // if the param exists but is not set (e.g. "?foo&bar"), its value is the empty string.
-  return val === "" || /1|on|true/i.test(val);
-}
-
 const isDebug = qsTruthy("debug");
 const logFilter = qs.get("log_filter") || (isDebug && "naf-janus-adapter:*");
 
