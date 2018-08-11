eaec8034f81da9b5aff2f0e702ab652c332a8dcf
johnshaughnessy <johnfshaughnessy@gmail.com>
2018-08-11 13:58:41 -0700

    typo

--- src/assets/environments/environments.js
+++ src/assets/environments/environments.js
@@ -1,5 +1,5 @@
 export const ENVIRONMENT_URLS = [
-  "https://www.jonicoma.com/Museum/Museum_Attempt_1.json",
+  "https://www.jonicoma.com/Museum/Museum_Attempt_1.bundle.json",
   process.env.ASSET_BUNDLE_SERVER + "/rooms/meetingroom/MeetingRoom.bundle.json",
   process.env.ASSET_BUNDLE_SERVER + "/rooms/atrium/Atrium.bundle.json",
   process.env.ASSET_BUNDLE_SERVER + "/rooms/MedievalFantasyBook/MedievalFantasyBook.bundle.json",
