b0bac385eb2d8f5898f70c876ce43b4c926c9a4d
Dominick D'Aniello <netpro2k@gmail.com>
2018-08-24 18:50:59 -0700

    Add wide open space environment

--- src/assets/environments/environments.js
+++ src/assets/environments/environments.js
@@ -2,7 +2,8 @@ export const ENVIRONMENT_URLS = [
   process.env.ASSET_BUNDLE_SERVER + "/rooms/meetingroom/MeetingRoom.bundle.json",
   process.env.ASSET_BUNDLE_SERVER + "/rooms/atrium/Atrium.bundle.json",
   process.env.ASSET_BUNDLE_SERVER + "/rooms/MedievalFantasyBook/MedievalFantasyBook.bundle.json",
-  process.env.ASSET_BUNDLE_SERVER + "/rooms/rooftopbuilding1/RooftopBuilding1.bundle.json"
+  process.env.ASSET_BUNDLE_SERVER + "/rooms/rooftopbuilding1/RooftopBuilding1.bundle.json",
+  process.env.ASSET_BUNDLE_SERVER + "/rooms/wideopenspace/WideOpenSpace.bundle.json"
 ];
 
 export const DEFAULT_ENVIRONMENT_URL = ENVIRONMENT_URLS[0];
