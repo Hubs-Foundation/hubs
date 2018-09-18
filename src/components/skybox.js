ef2a23f3ce5652e915a77503cdce0f3c4c2183aa
Brian Peiris <brianpeiris@gmail.com>
2018-09-17 21:16:02 -0700

    HACK temporary fix for existing environments

--- src/components/skybox.js
+++ src/components/skybox.js
@@ -272,7 +272,11 @@ AFRAME.registerComponent("skybox", {
 
       uniforms.sunPosition.value.set(x, y, z).normalize();
 
-      this.sky.scale.set(distance, distance, distance);
+      // HACK Remove this if condition and always set the scale based on distance when the existing environments
+      // have their sky scales set to 1.
+      if (this.el.object3D.scale.x === 1) {
+        this.sky.scale.set(distance, distance, distance);
+      }
     }
   },
 
