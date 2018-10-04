d479b47368f4e91e6603ea53e713336b78f6b982
Greg Fodor <gfodor@gmail.com>
2018-10-04 22:03:50 +0000

    Initial layout flow

--- src/spoke.js
+++ src/spoke.js
@@ -2,6 +2,7 @@ import ReactDOM from "react-dom";
 import React, { Component } from "react";
 //import PropTypes from "prop-types";
 //import classNames from "classnames";
+import { playVideoWithStopOnBlur } from "./utils/video-utils.js";
 import { IntlProvider, /*FormattedMessage, */ addLocaleData } from "react-intl";
 import styles from "./assets/stylesheets/spoke.scss";
 
@@ -25,10 +26,51 @@ class SpokeLanding extends Component {
     super(props);
   }
 
+  componentDidMount() {
+    this.loadVideo();
+  }
+
+  loadVideo() {
+    const videoEl = document.querySelector("#preview-video");
+    playVideoWithStopOnBlur(videoEl);
+  }
+
   render() {
     return (
       <IntlProvider locale={lang} messages={messages}>
-        <div className={styles.ui}>HI</div>
+        <div className={styles.ui}>
+          <div className={styles.header}>
+            <div className={styles.headerLinks}>
+              <a href="/about" rel="noopener noreferrer">
+                About
+              </a>
+              <a href="https://github.com/mozillareality/spoke" rel="noopener noreferrer">
+                Source
+              </a>
+              <a href="https://github.com/mozilla/hubs" rel="noopener noreferrer">
+                Hubs
+              </a>
+            </div>
+          </div>
+          <div className={styles.content}>
+            <div className={styles.heroPane}>
+              <div className={styles.heroMessage}>Message</div>
+              <div className={styles.heroVideo}>
+                <video playsInline muted loop autoPlay className={styles.previewVideo} id="preview-video">
+                  <source
+                    src="https://assets-prod.reticulum.io/assets/video/home-aee18c619a9005bd4b0d31295670af80.webm"
+                    type="video/webm"
+                  />
+                  <source
+                    src="https://assets-prod.reticulum.io/assets/video/home-5af051d2c531928dbaaf51b9badaabde.mp4"
+                    type="video/mp4"
+                  />
+                </video>
+              </div>
+            </div>
+          </div>
+          <div className={styles.bg} />
+        </div>
       </IntlProvider>
     );
   }
