4a398049bcdf2f4808b954eb73d9448f1cfb900d
Greg Fodor <gfodor@gmail.com>
2018-10-24 03:59:17 +0000

    Stub out GLTF uploading for pinning

--- src/scene-entry-manager.js
+++ src/scene-entry-manager.js
@@ -211,6 +211,38 @@ export default class SceneEntryManager {
       spawnMediaInfrontOfPlayer(e.detail, contentOrigin);
     });
 
+    this.scene.addEventListener("object_pinned", e => {
+      const el = e.detail.el;
+      if (!NAF.utils.isMine(el)) return;
+
+      // Construct a GLTF node from this entity
+      const object3D = el.object3D;
+      const components = el.components;
+      const networkId = components.networked.data.networkId;
+
+      const gltfComponents = {};
+      const gltfNode = { name: networkId, extensions: { HUBS_components: gltfComponents } };
+
+      // Adapted from three.js GLTFExporter
+      const equalArray = (x, y) => x.length === y.length && x.every((v, i) => v === y[i]);
+      const rotation = object3D.quaternion.toArray();
+      const position = object3D.position.toArray();
+      const scale = object3D.scale.toArray();
+
+      if (!equalArray(rotation, [0, 0, 0, 1])) gltfNode.rotation = rotation;
+      if (!equalArray(position, [0, 0, 0])) gltfNode.translation = position;
+      if (!equalArray(scale, [1, 1, 1])) gltfNode.scale = scale;
+
+      if (components["media-loader"]) {
+        gltfComponents.media = { src: components["media-loader"].data.src, networkId };
+      }
+    });
+
+    this.scene.addEventListener("object_unpinned", e => {
+      const el = e.detail.el;
+      if (!NAF.utils.isMine(el)) return;
+    });
+
     this.scene.addEventListener("object_spawned", e => {
       this.hubChannel.sendObjectSpawnedEvent(e.detail.objectType);
     });
