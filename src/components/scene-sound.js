90752c0ed2816b020e97d97640da60aa17cd2fec
johnshaughnessy <johnfshaughnessy@gmail.com>
2018-10-25 11:31:24 -0700

    Add comment giving motivation for scene-sound

--- src/components/scene-sound.js
+++ src/components/scene-sound.js
@@ -1,3 +1,6 @@
+// As temporary measure to avoid having to customize the `sound` component that currently lives in `aframe`,
+// we say that a `scene-sound` will have an associated `sound` component that is triggered when the given
+// event is fired on the scene.
 AFRAME.registerComponent("scene-sound", {
   multiple: true,
   schema: {
