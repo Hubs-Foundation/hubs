35c78fb5bfd026f6829814495a14ff1aadc14d8b
Greg Fodor <gfodor@gmail.com>
2018-11-13 01:47:01 +0000

    New hack

--- src/scene-entry-manager.js
+++ src/scene-entry-manager.js
@@ -98,6 +98,10 @@ export default class SceneEntryManager {
       });
     })();
 
+    // HACK - A-Frame calls getVRDisplays at module load, we want to do it here to
+    // force gamepads to become live if they were not active at launch.
+    navigator.getVRDisplays();
+
     this.scene.addState("entered");
   };
 
