db6ca639ca550955c085825ff6742a880b2abd81
johnshaughnessy <johnfshaughnessy@gmail.com>
2018-11-13 15:10:06 -0800

    Failed attempts to fix the buttons in the freeze menu on oculus touch.

--- src/systems/userinput/bindings/oculus-touch-user.js
+++ src/systems/userinput/bindings/oculus-touch-user.js
@@ -492,7 +492,7 @@ export const oculusTouchUserBindings = addSetsToBindings({
     {
       src: { value: rightTriggerPressed2 },
       dest: { value: paths.actions.cursor.grab },
-      xform: xforms.rising,
+      xform: xforms.falling,
       priority: 2
     }
   ],
