0a570be8b3b3ad8108237c7cb57ed128a6ccfc99
Brian Peiris <brianpeiris@gmail.com>
2018-11-13 16:21:12 -0800

    Remove sign in UI from home page

--- src/index.js
+++ src/index.js
@@ -4,9 +4,6 @@ import ReactDOM from "react-dom";
 import "./assets/stylesheets/index.scss";
 import registerTelemetry from "./telemetry";
 import HomeRoot from "./react-components/home-root";
-import AuthChannel from "./utils/auth-channel";
-import { connectToReticulum } from "./utils/phoenix-utils";
-import Store from "./storage/store";
 
 const qs = new URLSearchParams(location.search);
 registerTelemetry();
@@ -14,16 +11,10 @@ registerTelemetry();
 const { pathname } = document.location;
 const sceneId = qs.get("scene_id") || (pathname.startsWith("/scenes/") && pathname.substring(1).split("/")[1]);
 
-const store = new Store();
-const authChannel = new AuthChannel(store);
-authChannel.setSocket(connectToReticulum());
-
 const root = (
   <HomeRoot
     initialEnvironment={qs.get("initial_environment")}
     sceneId={sceneId || ""}
-    store={store}
-    authChannel={authChannel}
     authVerify={qs.has("auth_topic")}
     authTopic={qs.get("auth_topic")}
     authToken={qs.get("auth_token")}
