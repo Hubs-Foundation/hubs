37d023756c5aa7357adf1e6c2167f60b3dc54e66
johnshaughnessy <johnfshaughnessy@gmail.com>
2019-01-24 10:38:37 -0800

    Drop cursor on vive

--- src/systems/userinput/bindings/vive-user.js
+++ src/systems/userinput/bindings/vive-user.js
@@ -726,6 +726,17 @@ export const viveUserBindings = addSetsToBindings({
     }
   ],
 
+  [sets.cursorHoldingUI]: [
+    {
+      src: { value: rightTriggerPressed2 },
+      dest: {
+        value: cursorDrop2
+      },
+      xform: xforms.falling,
+      priority: 4
+    }
+  ],
+
   [sets.cursorHoldingInteractable]: [
     {
       src: {
@@ -742,15 +753,7 @@ export const viveUserBindings = addSetsToBindings({
       priority: 1
     },
     {
-      src: { value: rightTriggerPressed2 },
-      dest: {
-        value: cursorDrop2
-      },
-      xform: xforms.falling,
-      priority: 2
-    },
-    {
-      src: [cursorDrop1],
+      src: [cursorDrop1, cursorDrop2],
       dest: { value: paths.actions.cursor.drop },
       xform: xforms.any
     },
