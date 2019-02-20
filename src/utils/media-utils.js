142c7370271e3a3fc8d18e831f059f2ca2740b90
Greg Fodor <gfodor@gmail.com>
2019-02-20 02:56:33 +0000

    Fix for spawner

--- src/utils/media-utils.js
+++ src/utils/media-utils.js
@@ -194,6 +194,7 @@ export const addMedia = (src, template, contentOrigin, resolve = false, resize =
       clearTimeout(fireLoadingTimeout);
 
       entity.removeAttribute("animation__loader_spawn-start");
+      const [sx, sy, sz] = [entity.object3D.scale.x, entity.object3D.scale.y, entity.object3D.scale.z];
       entity.object3D.scale.set(sx / 2, sy / 2, sz / 2);
       entity.object3D.matrixNeedsUpdate = true;
 
