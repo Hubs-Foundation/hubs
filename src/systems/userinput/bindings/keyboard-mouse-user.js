4c7c617b9b641b3272420d5ef12ba4f59a00f436
Greg Fodor <gfodor@gmail.com>
2019-02-22 02:50:43 +0000

    Bump priority on ctrl-Z so it will start working again with pen selected

--- src/systems/userinput/bindings/keyboard-mouse-user.js
+++ src/systems/userinput/bindings/keyboard-mouse-user.js
@@ -371,6 +371,7 @@ export const keyboardMouseUserBindings = addSetsToBindings({
         value: paths.device.keyboard.key("z")
       },
       dest: { value: paths.actions.cursor.undoDrawing },
+      priority: 1001,
       xform: xforms.rising
     }
   ],
