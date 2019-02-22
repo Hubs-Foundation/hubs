ec03d3242dd16e8da15bf76c7af060b1b996a2ba
Greg Fodor <gfodor@gmail.com>
2019-02-22 19:33:48 +0000

    Fix for race condition on bot launcher

--- src/scene-entry-manager.js
+++ src/scene-entry-manager.js
@@ -482,18 +482,32 @@ export default class SceneEntryManager {
     } while (!audioInput || !dataInput);
 
     console.log("Input fields ready");
-    audioInput.onchange = () => {
+    const getAudio = () => {
       audioEl.loop = true;
       audioEl.muted = true;
       audioEl.crossorigin = "anonymous";
       audioEl.src = URL.createObjectURL(audioInput.files[0]);
       document.body.appendChild(audioEl);
     };
-    dataInput.onchange = () => {
+
+    if (audioInput.files && audioInput.files.length > 0) {
+      getAudio();
+    } else {
+      audioInput.onchange = getAudio;
+    }
+
+    const getRecording = () => {
       console.log("Recording selected, setting attribute");
       const url = URL.createObjectURL(dataInput.files[0]);
       this.playerRig.setAttribute("avatar-replay", { recordingUrl: url });
     };
+
+    if (dataInput.files && dataInput.files.length > 0) {
+      getRecording();
+    } else {
+      dataInput.onchange = getRecording;
+    }
+
     await new Promise(resolve => audioEl.addEventListener("canplay", resolve));
     mediaStream.addTrack(
       audioEl.captureStream
