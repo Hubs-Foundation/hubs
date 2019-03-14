5db4da516bb4eae5927e591404892645d7ad8a98
johnshaughnessy <johnfshaughnessy@gmail.com>
2019-03-14 16:24:02 -0700

    Get cursor working more cleanly

--- src/systems/interactions.js
+++ src/systems/interactions.js
@@ -1,130 +1,83 @@
-// Goal : Collect all of the threejs objects that we might care about hovering onto.
-//        Notice when we hover on or off them.
-
 import { sets } from "./userinput/sets";
 import { paths } from "./userinput/paths";
 
-const AGGREGATE_AND_MARK_THREEJS_OBJECTS_FOR_INTERACTION = "aggregate-and-mark-objects";
-AFRAME.registerComponent(AGGREGATE_AND_MARK_THREEJS_OBJECTS_FOR_INTERACTION, {
-  schema: {
-    offerConstraintWhenHovered: { type: "boolean", default: false },
-    isPen: { type: "boolean", default: false },
-    isCamera: { type: "boolean", default: false },
-    isUI: { type: "boolean", default: false }
-  },
+const THREEJS_OBJECT_DESCENDENTS = {
+  uuidToEl: new Map(),
+  rootToDescendents: new Map(),
+  descendentsToRoot: new Map(),
+  associate: function(el, descendentUuids) {
+    this.uuidToEl.set(el.object3D.uuid, el);
+    this.rootToDescendents.set(el.object3D.uuid, descendentUuids);
+    for (let uuid of descendentUuids) {
+      this.descendentsToRoot.set(uuid, el.object3D.uuid);
+    }
+  }
+};
 
+AFRAME.registerComponent("threejs-object-descendents-aggregator", {
   init: function() {
-    this.onObjectAdded = this.onObjectAdded.bind(this);
-    this.kids = new Set();
-    this.el.object3D.traverse(o => {
-      this.kids.add(o.uuid);
-      o.addEventListener("added", this.onObjectAdded);
-      o.el.addEventListener("object3dset", this.onObjectAdded);
-    });
+    this.addDescendents = this.addDescendents.bind(this);
 
-    AFRAME.scenes[0].systems.interaction.register(this.el.object3D.uuid, this.kids, this.data);
+    this.descendents = new Set();
+    this.addDescendents();
   },
 
-  onObjectAdded: function() {
+  addDescendents: function() {
     this.el.object3D.traverse(o => {
-      if (!this.kids.has(o.uuid)) {
-        this.kids.add(o.uuid);
-        o.addEventListener("added", this.onObjectAdded);
-        o.el.addEventListener("object3dset", this.onObjectAdded);
+      if (!this.descendents.has(o.uuid)) {
+        this.descendents.add(o.uuid);
+        o.el.addEventListener("object3dset", this.addDescendents);
       }
     });
-    AFRAME.scenes[0].systems.interaction.register(this.el.object3D, this.kids, this.data);
+    THREEJS_OBJECT_DESCENDENTS.associate(this.el, this.descendents);
   }
 });
 
-AFRAME.registerSystem("interaction", {
-  init() {
-    this.rootToKids = new Map();
-    this.kidsToRoot = new Map();
-    this.rootToData = new Map();
-    this.uuidToObject3D = new Map();
-  },
-  register(root, kids, data) {
-    this.uuidToObject3D.set(root.uuid, root);
-    this.rootToKids.set(root.uuid, kids);
-    this.rootToData.set(root.uuid, data);
-    for (let kid of kids.keys()) {
-      this.kidsToRoot.set(kid, root.uuid);
-    }
-  },
+AFRAME.registerComponent("offers-remote-constraint", {});
+AFRAME.registerComponent("is-ui", {});
 
+AFRAME.registerSystem("interaction", {
+  init: function() {
+    this.rightRemoteHoverTarget = null;
+    this.rightRemoteConstraintTarget = null;
+  },
   updateCursorIntersections: function(raw) {
-    const userinput = AFRAME.scenes[0].systems.userinput;
     if (!raw[0]) {
-      this.currentlyHoveredRoot = null;
+      this.rightRemoteHoverTarget = null;
       return;
     }
-    const root = this.kidsToRoot.get(raw[0].object.uuid);
-    if (!root) {
-      this.currentlyHoveredRoot = null;
+    const rootUuid = THREEJS_OBJECT_DESCENDENTS.descendentsToRoot.get(raw[0].object.uuid);
+    if (!rootUuid) {
+      this.rightRemoteHoverTarget = null;
       return;
     }
-    this.currentlyHoveredRoot = root;
+    this.rightRemoteHoverTarget = THREEJS_OBJECT_DESCENDENTS.uuidToEl.get(rootUuid);
   },
 
   tick: (function() {
-    const m = new THREE.Matrix4();
     return function() {
       const userinput = AFRAME.scenes[0].systems.userinput;
-      if (!this.currentlyConstrainedRoot) {
-        if (!!this.currentlyHoveredRoot && this.rootToData.get(this.currentlyHoveredRoot).offerConstraintWhenHovered) {
-          const grab = userinput.get(paths.actions.cursor.grab);
-          if (grab) {
-            this.currentlyConstrainedRoot = this.currentlyHoveredRoot;
-          }
+      if (this.rightRemoteConstraintTarget) {
+        if (userinput.get(paths.actions.cursor.drop)) {
+          this.rightRemoteConstraintTarget = null;
         }
       } else {
-        const drop = userinput.get(paths.actions.cursor.drop);
-        if (drop) {
-          this.currentlyConstrainedRoot = null;
+        if (this.rightRemoteHoverTarget && this.rightRemoteHoverTarget.components["offers-remote-constraint"]) {
+          const grab = userinput.get(paths.actions.cursor.grab);
+          if (grab) {
+            this.rightRemoteConstraintTarget = this.rightRemoteHoverTarget;
+          }
         }
       }
 
-      if (!!this.currentlyConstrainedRoot) {
-        const b = document.querySelector("#cursor").object3D;
-        const a = this.uuidToObject3D.get(this.currentlyConstrainedRoot);
-        const m = new THREE.Matrix4();
-        b.matrixIsModified = true;
-        b.updateMatrices();
-        a.matrixIsModified = true;
-        a.updateMatrices();
-        m.getInverse(a.matrixWorld).multiply(b.matrixWorld);
-        a.matrix.copy(m);
-        a.matrixWorldNeedsUpdate = true;
-        a.childrenNeedMatrixWorldUpdate = true;
-        a.updateMatrices();
+      if (this.rightRemoteConstraintTarget) {
+        this.cursor = this.cursor || document.querySelector("#cursor").object3D;
+        const o = this.rightRemoteConstraintTarget.object3D;
+        this.cursor.updateMatrices();
+        o.updateMatrices();
+        o.matrix.getInverse(o.parent.matrixWorld).multiply(this.cursor.matrixWorld);
+        o.matrixWorldNeedsUpdate = true;
       }
     };
   })()
-});
-
-AFRAME.registerComponent("test-track", {
-  tick: function() {
-    const b = document.querySelector("#cursor").object3D;
-    const a = this.el.object3D;
-    const m = new THREE.Matrix4();
-    b.matrixIsModified = true;
-    b.updateMatrices();
-    a.matrixIsModified = true;
-    a.updateMatrices();
-    m.getInverse(a.parent.matrixWorld).multiply(b.matrixWorld);
-    a.matrix.copy(m);
-//    a.matrix.decompose(a.position, a.quaternion, a.scale);
-    a.matrixWorldNeedsUpdate = true;
-    a.updateMatrixWorld();
-    this.foo = this.foo || 0;
-    this.foo += 1;
-    if (this.foo % 100 === 0) {
-      let s = "";
-      for (let i = 0; i < 16; i++) {
-        s += "" + a.matrixWorld.elements[i] + " " + b.matrixWorld.elements[i] + "\n";
-      }
-      console.log(s);
-    }
-  }
 });
--- src/systems/userinput/resolve-action-sets.js
+++ src/systems/userinput/resolve-action-sets.js
@@ -1,173 +1,62 @@
 import { sets } from "./sets";
 
-let rightHandState;
-let leftHandState;
-let cursorHand;
 let leftTeleporter;
 let rightTeleporter;
-let cursorController;
 
 export function resolveActionSets() {
-  rightHandState = new Map(); //rightHandState || document.querySelector("#player-right-controller").components["super-hands"].state;
-  leftHandState = new Map(); //leftHandState || document.querySelector("#player-left-controller").components["super-hands"].state;
-  cursorHand = new Map(); //cursorHand || document.querySelector("#cursor").components["super-hands"].state;
   leftTeleporter = leftTeleporter || document.querySelector("#player-left-controller").components["teleporter"];
   rightTeleporter = rightTeleporter || document.querySelector("#player-right-controller").components["teleporter"];
-  cursorController = cursorController || document.querySelector("#cursor-controller").components["cursor-controller"];
-
-  const leftHandHoveringOnInteractable =
-    !leftTeleporter.isTeleporting &&
-    leftHandState.has("hover-start") &&
-    leftHandState.get("hover-start").matches(".interactable, .interactable *");
-  const leftHandHoveringOnPen =
-    !leftTeleporter.isTeleporting &&
-    leftHandState.has("hover-start") &&
-    leftHandState.get("hover-start").matches(".pen, .pen *");
-  const leftHandHoveringOnCamera =
-    !leftTeleporter.isTeleporting &&
-    leftHandState.has("hover-start") &&
-    leftHandState.get("hover-start").matches(".icamera, .icamera *");
-  const leftHandHoldingInteractable =
-    leftHandState.has("grab-start") && leftHandState.get("grab-start").matches(".interactable, .interactable *");
-  const leftHandHoldingPen = leftHandState.has("grab-start") && leftHandState.get("grab-start").matches(".pen, .pen *");
-  const leftHandHoldingCamera =
-    leftHandState.has("grab-start") && leftHandState.get("grab-start").matches(".icamera, .icamera *");
-  const leftHandHovering = !leftTeleporter.isTeleporting && leftHandState.has("hover-start");
-  const leftHandHoveringOnNothing = !leftHandHovering && !leftHandState.has("grab-start");
-  const leftHandTeleporting = leftTeleporter.isTeleporting;
-
-  const cursorGrabbing = cursorHand.has("grab-start");
-
-  const rightHandTeleporting = rightTeleporter.isTeleporting;
-  const rightHandHovering = !rightHandTeleporting && !cursorGrabbing && rightHandState.has("hover-start");
-  const rightHandGrabbing = !rightHandTeleporting && !cursorGrabbing && rightHandState.has("grab-start");
-
-  const rightHandHoveringOnInteractable =
-    !rightHandTeleporting &&
-    !cursorGrabbing &&
-    rightHandState.has("hover-start") &&
-    rightHandState.get("hover-start").matches(".interactable, .interactable *");
-  const rightHandHoveringOnPen =
-    !rightHandTeleporting &&
-    !cursorGrabbing &&
-    rightHandState.has("hover-start") &&
-    rightHandState.get("hover-start").matches(".pen, .pen *");
-  const rightHandHoveringOnCamera =
-    !rightTeleporter.isTeleporting &&
-    !cursorGrabbing &&
-    rightHandState.has("hover-start") &&
-    rightHandState.get("hover-start").matches(".icamera, .icamera *");
-  const rightHandHoldingInteractable =
-    !cursorGrabbing &&
-    rightHandState.has("grab-start") &&
-    rightHandState.get("grab-start").matches(".interactable, .interactable *");
-  const rightHandHoldingPen =
-    !cursorGrabbing && rightHandState.has("grab-start") && rightHandState.get("grab-start").matches(".pen, .pen *");
-  const rightHandHoldingCamera =
-    !cursorGrabbing &&
-    rightHandState.has("grab-start") &&
-    rightHandState.get("grab-start").matches(".icamera, .icamera *");
-
-  const rightHandHoveringOnNothing =
-    !rightHandTeleporting &&
-    !rightHandHovering &&
-    !cursorHand.has("hover-start") &&
-    !cursorGrabbing &&
-    !rightHandState.has("grab-start");
-
-  // Cursor
-  cursorController.enabled = !(rightHandTeleporting || rightHandHovering || rightHandGrabbing);
-  const cursorHoldingUI =
-    cursorController.enabled &&
-    !rightHandTeleporting &&
-    !rightHandHovering &&
-    !rightHandGrabbing &&
-    (cursorHand.has("grab-start") && cursorHand.get("grab-start").matches(".ui, .ui *"));
-  const cursorHoveringOnUI = cursorHand.has("hover-start") && cursorHand.get("hover-start").matches(".ui, .ui *");
-  const cursorCanHover =
-    cursorController.enabled &&
-    !rightHandTeleporting &&
-    !rightHandHovering &&
-    !rightHandGrabbing &&
-    !cursorHoveringOnUI &&
-    cursorHand.has("hover-start");
-
-  const cursorHoveringOnInteractable =
-    cursorCanHover && cursorHand.get("hover-start").matches(".interactable, .interactable *");
-  const cursorHoveringOnCamera = cursorCanHover && cursorHand.get("hover-start").matches(".icamera, .icamera *");
-  const cursorHoveringOnPen = cursorCanHover && cursorHand.get("hover-start").matches(".pen, .pen *");
-  const cursorHoveringOnVideo = cursorCanHover && cursorHand.get("hover-start").components["media-video"];
-  const cursorHoldingInteractable =
-    !rightHandTeleporting &&
-    cursorHand.has("grab-start") &&
-    cursorHand.get("grab-start").matches(".interactable, .interactable *");
-  const cursorHoldingPen =
-    !rightHandTeleporting && cursorHand.has("grab-start") && cursorHand.get("grab-start").matches(".pen, .pen *");
-
-  const cursorHoldingCamera =
-    !rightTeleporter.isTeleporting &&
-    cursorHand.has("grab-start") &&
-    cursorHand.get("grab-start").matches(".icamera, .icamera *");
-
-  const cursorHoveringOnNothing =
-    !rightHandTeleporting &&
-    !rightHandHovering &&
-    !rightHandGrabbing &&
-    !cursorHand.has("hover-start") &&
-    !cursorHand.has("grab-start");
 
   const userinput = AFRAME.scenes[0].systems.userinput;
-  userinput.toggleSet(sets.leftHandHoveringOnInteractable, leftHandHoveringOnInteractable);
-  userinput.toggleSet(sets.leftHandHoveringOnPen, leftHandHoveringOnPen);
-  userinput.toggleSet(sets.leftHandHoveringOnCamera, leftHandHoveringOnCamera);
-  userinput.toggleSet(sets.leftHandHoveringOnNothing, leftHandHoveringOnNothing);
-  userinput.toggleSet(sets.leftHandHoldingPen, leftHandHoldingPen);
-  userinput.toggleSet(sets.leftHandHoldingInteractable, leftHandHoldingInteractable);
-  userinput.toggleSet(sets.leftHandHoldingCamera, leftHandHoldingCamera);
-  userinput.toggleSet(sets.leftHandTeleporting, leftHandTeleporting);
-
-  userinput.toggleSet(sets.rightHandHoveringOnInteractable, rightHandHoveringOnInteractable);
-  userinput.toggleSet(sets.rightHandHoveringOnPen, rightHandHoveringOnPen);
-  userinput.toggleSet(sets.rightHandHoveringOnNothing, rightHandHoveringOnNothing);
-  userinput.toggleSet(sets.rightHandHoveringOnCamera, rightHandHoveringOnCamera);
-  userinput.toggleSet(sets.rightHandHoldingPen, rightHandHoldingPen);
-  userinput.toggleSet(sets.rightHandHoldingInteractable, rightHandHoldingInteractable);
-  userinput.toggleSet(sets.rightHandTeleporting, rightHandTeleporting);
-  userinput.toggleSet(sets.rightHandHoldingCamera, rightHandHoldingCamera);
+  //  userinput.toggleSet(sets.leftHandHoveringOnInteractable, leftHandHoveringOnInteractable);
+  //  userinput.toggleSet(sets.leftHandHoveringOnPen, leftHandHoveringOnPen);
+  //  userinput.toggleSet(sets.leftHandHoveringOnCamera, leftHandHoveringOnCamera);
+  //  userinput.toggleSet(sets.leftHandHoveringOnNothing, leftHandHoveringOnNothing);
+  //  userinput.toggleSet(sets.leftHandHoldingPen, leftHandHoldingPen);
+  //  userinput.toggleSet(sets.leftHandHoldingInteractable, leftHandHoldingInteractable);
+  //  userinput.toggleSet(sets.leftHandHoldingCamera, leftHandHoldingCamera);
+  //  userinput.toggleSet(sets.leftHandTeleporting, leftHandTeleporting);
+  //
+  //  userinput.toggleSet(sets.rightHandHoveringOnInteractable, rightHandHoveringOnInteractable);
+  //  userinput.toggleSet(sets.rightHandHoveringOnPen, rightHandHoveringOnPen);
+  //  userinput.toggleSet(sets.rightHandHoveringOnNothing, rightHandHoveringOnNothing);
+  //  userinput.toggleSet(sets.rightHandHoveringOnCamera, rightHandHoveringOnCamera);
+  //  userinput.toggleSet(sets.rightHandHoldingPen, rightHandHoldingPen);
+  //  userinput.toggleSet(sets.rightHandHoldingInteractable, rightHandHoldingInteractable);
+  //  userinput.toggleSet(sets.rightHandTeleporting, rightHandTeleporting);
+  //  userinput.toggleSet(sets.rightHandHoldingCamera, rightHandHoldingCamera);
 
   const interaction = AFRAME.scenes[0].systems.interaction;
-  userinput.toggleSet(
-    sets.cursorHoveringOnPen,
-    !!interaction.currentlyHoveredRoot && interaction.rootToData.get(interaction.currentlyHoveredRoot).isPen
-  );
+  const rightRemoteConstraintTarget = interaction.rightRemoteConstraintTarget;
+  const rightRemoteHoverTarget = !rightRemoteConstraintTarget && interaction.rightRemoteHoverTarget;
+  userinput.toggleSet(sets.cursorHoveringOnNothing, !rightRemoteConstraintTarget && !rightRemoteHoverTarget);
+  userinput.toggleSet(sets.cursorHoveringOnPen, rightRemoteHoverTarget && !!rightRemoteHoverTarget.components["pen"]);
   userinput.toggleSet(
     sets.cursorHoveringOnCamera,
-    !!interaction.currentlyHoveredRoot && interaction.rootToData.get(interaction.currentlyHoveredRoot).isCamera
+    rightRemoteHoverTarget && !!rightRemoteHoverTarget.components["camera-tool"]
   );
   userinput.toggleSet(
     sets.cursorHoveringOnInteractable,
-    !interaction.currentlyConstrainedRoot &&
-      !!interaction.currentlyHoveredRoot &&
-      interaction.rootToData.get(interaction.currentlyHoveredRoot).offerConstraintWhenHovered
+    rightRemoteHoverTarget && !!rightRemoteHoverTarget.components["offers-remote-constraint"]
   );
+  userinput.toggleSet(sets.cursorHoveringOnUI, rightRemoteHoverTarget && !!rightRemoteHoverTarget.components["is-ui"]);
   userinput.toggleSet(
-    sets.cursorHoveringOnUI,
-    !!interaction.currentlyHoveredRoot && interaction.rootToData.get(interaction.currentlyHoveredRoot).isUI
+    sets.cursorHoveringOnVideo,
+    rightRemoteHoverTarget && !!rightRemoteHoverTarget.components["media-video"]
   );
-  userinput.toggleSet(sets.cursorHoveringOnVideo, cursorHoveringOnVideo);
-  userinput.toggleSet(sets.cursorHoveringOnNothing, !interaction.currentlyHoveredRoot);
+
+  userinput.toggleSet(sets.cursorHoldingInteractable, !!rightRemoteConstraintTarget);
   userinput.toggleSet(
     sets.cursorHoldingPen,
-    !!interaction.currentlyConstrainedRoot && interaction.rootToData.get(interaction.currentlyConstrainedRoot).isPen
+    rightRemoteConstraintTarget && !!rightRemoteConstraintTarget.components["pen"]
   );
   userinput.toggleSet(
     sets.cursorHoldingCamera,
-    !!interaction.currentlyConstrainedRoot && interaction.rootToData.get(interaction.currentlyConstrainedRoot).isCamera
+    rightRemoteConstraintTarget && !!rightRemoteConstraintTarget.components["camera-tool"]
   );
-  userinput.toggleSet(sets.cursorHoldingInteractable, !!interaction.currentlyConstrainedRoot);
   userinput.toggleSet(
     sets.cursorHoldingUI,
-    !!interaction.currentlyConstrainedRoot && interaction.rootToData.get(interaction.currentlyConstrainedRoot).isUI
+    rightRemoteConstraintTarget && !!rightRemoteConstraintTarget.components["is-ui"]
   );
   userinput.toggleSet(
     sets.inputFocused,
