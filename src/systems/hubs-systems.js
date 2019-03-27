3c6822ae11b5c4b691d6cae45489cd8352573da7
johnshaughnessy <johnfshaughnessy@gmail.com>
2019-03-27 16:44:32 -0700

    Add TwoPointStretchingSystem

--- src/systems/hubs-systems.js
+++ src/systems/hubs-systems.js
@@ -1,10 +1,12 @@
-import {CursorTargettingSystem} from "./cursor-targetting-system";
-import {ConstraintsSystem} from "./constraints-system";
+import { CursorTargettingSystem } from "./cursor-targetting-system";
+import { ConstraintsSystem } from "./constraints-system";
+import { TwoPointStretchingSystem } from "./two-point-stretching-system";
 
 AFRAME.registerSystem("hubs-systems", {
   init() {
     this.cursorTargettingSystem = new CursorTargettingSystem();
     this.constraintsSystem = new ConstraintsSystem();
+    this.twoPointStretchingSystem = new TwoPointStretchingSystem();
   },
   tick(t, dt) {
     const systems = AFRAME.scenes[0].systems;
@@ -12,5 +14,6 @@ AFRAME.registerSystem("hubs-systems", {
     this.cursorTargettingSystem.tick();
     systems.interaction.tick2();
     this.constraintsSystem.tick();
+    this.twoPointStretchingSystem.tick();
   }
 });
