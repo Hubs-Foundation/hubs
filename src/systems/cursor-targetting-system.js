e247b3ff7a42156399acf066514f963a0aa2da46
Marshall Polaris <marshall@pol.rs>
2019-04-03 17:07:29 -0700

    Moderately improve cursor target acquisition

--- src/systems/cursor-targetting-system.js
+++ src/systems/cursor-targetting-system.js
@@ -3,23 +3,19 @@ import { waitForDOMContentLoaded } from "../utils/async-utils";
 export class CursorTargettingSystem {
   constructor() {
     this.targets = [];
-    this.setDirty = this.setDirty.bind(this);
     this.dirty = true;
-
+    this.onMutation = this.onMutation.bind(this);
+    this.onObject3DSet = this.onObject3DSet.bind(this);
+    this.onObject3DRemove = this.onObject3DRemove.bind(this);
     // TODO: Use the MutationRecords passed into the callback function to determine added/removed nodes!
-    this.observer = new MutationObserver(this.setDirty);
-
+    this.observer = new MutationObserver(this.onMutation);
     waitForDOMContentLoaded().then(() => {
       const scene = document.querySelector("a-scene");
       this.rightRemote = document.querySelector("#cursor-controller");
       this.observer.observe(scene, { childList: true, attributes: true, subtree: true });
-      scene.addEventListener("object3dset", this.setDirty);
-      scene.addEventListener("object3dremove", this.setDirty);
+      scene.addEventListener("object3dset", this.onObject3DSet);
+      scene.addEventListener("object3dremove", this.onObject3DRemove);
     });
-  }
-
-  setDirty() {
-    this.dirty = true;
   }
 
   tick(t) {
@@ -29,6 +25,21 @@ export class CursorTargettingSystem {
     }
 
     this.rightRemote.components["cursor-controller"].tick2(t);
+  }
+
+  onObject3DSet() {
+    this.dirty = true;
+  }
+
+  onObject3DRemove() {
+    this.dirty = true;
+  }
+
+  onMutation(records) {
+    // let's try to avoid re-querying the targets on attribute changes we know we can ignore
+    if (records.some(r => r.type === "childList" || (r.type === "attributes" && r.attributeName === "class"))) {
+      this.dirty = true;
+    }
   }
 
   populateEntities(targets) {
@@ -44,7 +55,7 @@ export class CursorTargettingSystem {
 
   remove() {
     this.observer.disconnect();
-    AFRAME.scenes[0].removeEventListener("object3dset", this.setDirty);
-    AFRAME.scenes[0].removeEventListener("object3dremove", this.setDirty);
+    AFRAME.scenes[0].removeEventListener("object3dset", this.onObject3DSet);
+    AFRAME.scenes[0].removeEventListener("object3dremove", this.onObject3DRemove);
   }
 }
