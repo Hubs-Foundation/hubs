84b066e6c6af2233521ff26c64039cef18973198
netpro2k <netpro2k@gmail.com>
2019-04-15 16:37:21 -0700

    Basic avatar preview WIP

--- src/react-components/avatar-editor.js
+++ src/react-components/avatar-editor.js
@@ -11,6 +11,39 @@ import styles from "../assets/stylesheets/profile.scss";
 
 const BOT_PARENT_AVATAR =
   location.hostname === "hubs.mozilla.com" || location.hostname === "smoke-hubs.mozilla.com" ? "gZ6gPvQ" : "xf9xkIY";
+const BOT_PARENT_AVATAR = "muKnhtJ";
+
+const TEXTURE_PROPS = {
+  base_map: ["map"],
+
+  emissive_map: ["emissiveMap"],
+  normal_map: ["normalMap"],
+
+  ao_map: ["aoMap"],
+  roughness_map: ["roughnessMap"],
+  metalic_map: ["metalnessMap"],
+
+  orm_map: ["aoMap", "roughnessMap", "metalnessMap"]
+};
+
+// tries to match our aframe renderer="antialias: true; colorManagement: true; sortObjects: true; physicallyCorrectLights: true; alpha: false; webgl2: true; multiview: false;"
+function createRenderer(canvas) {
+  const context = canvas.getContext("webgl2", {
+    alpha: true,
+    depth: true,
+    antialias: true,
+    premultipliedAlpha: true,
+    preserveDrawingBuffer: false,
+    powerPreference: "default"
+  });
+
+  const renderer = new THREE.WebGLRenderer({ canvas, context });
+  renderer.gammaOutput = true;
+  renderer.gammaFactor = 2.2;
+  renderer.physicallyCorrectLights = true;
+  renderer.setSize(canvas.clientWidth, canvas.clientHeight);
+  return renderer;
+}
 
 export default class AvatarEditor extends Component {
   static propTypes = {
@@ -50,7 +83,79 @@ export default class AvatarEditor extends Component {
     }
   };
 
+  setupPreviewScene = canvas => {
+    if (!canvas || this.previewRenderer) return;
+
+    const scene = new THREE.Scene();
+    scene.background = new THREE.Color(0xeaeaea);
+
+    const camera = new THREE.PerspectiveCamera(75, canvas.clientWidth / canvas.clientHeight, 0.1, 1000);
+    const controls = new THREE.OrbitControls(camera, canvas);
+
+    const light = new THREE.DirectionalLight(0xfdf5c2, 3);
+    light.position.set(0, 10, 10);
+    scene.add(light);
+    scene.add(new THREE.HemisphereLight(0xb1e3ff, 0xb1e3ff, 3));
+
+    camera.position.set(-0.2, 0.5, 0.5);
+    camera.matrixAutoUpdate = true;
+
+    controls.target.set(0, 0.45, 0);
+    controls.update();
+
+    this.loadPreviwAvatar(this.state.avatar).then(scene.add.bind(scene));
+
+    this.previewRenderer = createRenderer(canvas);
+    this.previewRenderer.setAnimationLoop(() => {
+      // controls.update();
+      this.previewRenderer.render(scene, camera);
+    });
+  };
+
+  loadPreviwAvatar = async avatar => {
+    const loader = new THREE.GLTFLoader();
+    const imgLoader = new THREE.ImageBitmapLoader();
+
+    const gltf = await new Promise((resolve, reject) => loader.load(avatar.base_gltf_url, resolve, null, reject));
+    this.previewMesh = gltf.scene.getObjectByName("Avatar");
+    const material = this.previewMesh.material;
+    const getImage = p => delete material[p].onUpdate && material[p].image;
+    this.originalMaps = {
+      base_map: TEXTURE_PROPS["base_map"].map(getImage),
+      emissive_map: TEXTURE_PROPS["emissive_map"].map(getImage),
+      normal_map: TEXTURE_PROPS["normal_map"].map(getImage),
+      orm_map: TEXTURE_PROPS["orm_map"].map(getImage)
+    };
+
+    const imgFiles = avatar.files;
+    await Promise.all([
+      Promise.all(
+        Object.keys(this.originalMaps).map(
+          m =>
+            imgFiles[m] &&
+            new Promise((resolve, reject) => imgLoader.load(imgFiles[m], resolve, null, reject)).then(
+              this.applyMapToPreview.bind(this, m)
+            )
+        )
+      ),
+      createDefaultEnvironmentMap().then(t => {
+        this.previewMesh.material.envMap = t;
+        this.previewMesh.material.needsUpdate = true;
+      })
+    ]);
+    return gltf.scene;
+  };
+
+  applyMapToPreview = (name, image) => {
+    TEXTURE_PROPS[name].forEach(prop => {
+      const texture = this.previewMesh.material[prop];
+      texture.image = image;
+      texture.needsUpdate = true;
+    });
+  };
+
   componentDidUpdate = () => {
+    this.setupPreviewScene(this.previewCanvas);
     if (this.props.signedIn && !this.state.avatar) {
       return this.getPersonalAvatar();
     }
@@ -172,17 +277,20 @@ export default class AvatarEditor extends Component {
         accept={accept}
         disabled={disabled}
         onChange={e => {
-          this.inputFiles[name] = e.target.files[0];
+          const file = e.target.files[0];
+          e.target.value = null;
+          this.inputFiles[name] = file;
           URL.revokeObjectURL(this.state.avatar.files[name]);
           this.setState({
             avatar: {
               ...this.state.avatar,
               files: {
                 ...this.state.avatar.files,
-                [name]: URL.createObjectURL(e.target.files[0])
+                [name]: URL.createObjectURL(file)
               }
             }
           });
+          createImageBitmap(file).then(this.applyMapToPreview.bind(this, name));
         }}
       />
       {this.state.avatar.files[name] && (
@@ -198,6 +306,11 @@ export default class AvatarEditor extends Component {
                   [name]: null
                 }
               }
+            });
+            this.originalMaps[name].forEach((bm, i) => {
+              const texture = this.previewMesh.material[TEXTURE_PROPS[name][i]];
+              texture.image = bm;
+              texture.needsUpdate = true;
             });
           }}
         >
@@ -254,31 +367,35 @@ export default class AvatarEditor extends Component {
 
     return (
       <div className={classNames(styles.avatarSelectorContainer, "avatar-editor")}>
-        <div className="form-body">
-          {debug && this.textField("avatar_id", "Avatar ID", true)}
-          {debug && this.textField("parent_avatar_id", "Parent Avatar ID")}
-          {debug && this.textField("name", "Name")}
-          {debug && this.textField("description", "Description")}
-          {debug && this.checkbox("allow_remixing", "Allow Remixing")}
-          {debug && this.checkbox("allow_promotion", "Allow Promotion")}
-          {debug && this.fileField("glb", "Avatar GLB", "model/gltf+binary,.glb")}
+        <div className="split">
+          <div className="form-body">
+            {debug && this.textField("avatar_id", "Avatar ID", true)}
+            {debug && this.textField("parent_avatar_id", "Parent Avatar ID")}
+            {debug && this.textField("name", "Name")}
+            {debug && this.textField("description", "Description")}
+            {debug && this.checkbox("allow_remixing", "Allow Remixing")}
+            {debug && this.checkbox("allow_promotion", "Allow Promotion")}
+            {debug && this.fileField("glb", "Avatar GLB", "model/gltf+binary,.glb")}
 
-          {this.fileField("base_map", "Base Map", "image/*")}
-          {this.fileField("emissive_map", "Emissive Map", "image/*")}
-          {this.fileField("normal_map", "Normal Map", "image/*")}
+            {this.fileField("base_map", "Base Map", "image/*")}
+            {this.fileField("emissive_map", "Emissive Map", "image/*")}
+            {this.fileField("normal_map", "Normal Map", "image/*")}
 
-          {this.fileField("orm_map", "ORM Map", "image/*", false, "Occlussion (r), Roughness (g), Metallic (b)")}
+            {this.fileField("orm_map", "ORM Map", "image/*", false, "Occlussion (r), Roughness (g), Metallic (b)")}
 
-          {/* {this.fileField("ao_map", "AO Map", "images/\*", true)} */}
-          {/* {this.fileField("metallic_map", "Metallic Map", "image/\*", true)} */}
-          {/* {this.fileField("roughness_map", "Roughness Map", "image/\*", true)} */}
-
-          <div className={styles.info}>
-            <FormattedMessage id="avatar-editor.info" />
-            <a target="_blank" rel="noopener noreferrer" href="https://github.com/j-conrad/hubs-avatar-pipelines">
-              <FormattedMessage id="avatar-editor.info-link" />
-            </a>
+            {/* {this.fileField("ao_map", "AO Map", "images/\*", true)} */}
+            {/* {this.fileField("metallic_map", "Metallic Map", "image/\*", true)} */}
+            {/* {this.fileField("roughness_map", "Roughness Map", "image/\*", true)} */}
           </div>
+          <div className="preview">
+            <canvas ref={c => (this.previewCanvas = c)} />
+          </div>
+        </div>
+        <div className={styles.info}>
+          <FormattedMessage id="avatar-editor.info" />
+          <a target="_blank" rel="noopener noreferrer" href="https://github.com/j-conrad/hubs-avatar-pipelines">
+            <FormattedMessage id="avatar-editor.info-link" />
+          </a>
         </div>
         <div>
           <input
