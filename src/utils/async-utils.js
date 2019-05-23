4a8e5ae8386c5b5f8f7960a48b6094726b76f899
Marshall Polaris <marshall@pol.rs>
2019-05-22 17:55:33 -0700

    Fix lint

--- src/utils/async-utils.js
+++ src/utils/async-utils.js
@@ -2,7 +2,9 @@ export const waitForDOMContentLoaded = function() {
   if (document.readyState === "complete" || document.readyState === "loaded" || document.readyState === "interactive") {
     return Promise.resolve(null);
   } else {
-    return new Promise(resolve => { window.addEventListener("DOMContentLoaded", resolve); });
+    return new Promise(resolve => {
+      window.addEventListener("DOMContentLoaded", resolve);
+    });
   }
 };
 
