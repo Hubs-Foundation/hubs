fa0fe7c1e3312989f5a5cc6b6c2aa45c69cfab64
johnshaughnessy <johnfshaughnessy@gmail.com>
2019-05-28 18:55:26 -0700

    Draw sprite components

--- src/systems/sprites.js
+++ src/systems/sprites.js
@@ -1,3 +1,5 @@
+/* global AFRAME THREE */
+
 // Sprite rendering
 // We have a lot of 2D images that are each rendered separately in hubs.
 // Our goal is to render them all at once.
@@ -10,166 +12,65 @@ import { waitForDOMContentLoaded } from "../utils/async-utils";
 import vert from "./sprites/sprite.vert";
 import frag from "./sprites/sprite.frag";
 
-// We'll replace
-//   <a-image ... ></a-image>
-// with,
-//   <a-entity sprite="name: foo"></a-entity>
-// where sprite is an aframe component:
+function isVisible(o) {
+  if (!o.visible) return false;
+  if (!o.parent) return true;
+  return isVisible(o.parent);
+}
+
 AFRAME.registerComponent("sprite", {
-  schema: { name: { type: "string" } }
+  schema: { name: { type: "string" } },
+  tick() {
+    if (!this.didRegisterWithSystem) {
+      this.didRegisterWithSystem = true;
+      this.el.sceneEl.systems["hubs-systems"].spriteSystem.add(this);
+    }
+  },
+  remove(){
+    this.el.sceneEl.systems["hubs-systems"].spriteSystem.remove(this);
+  }
 });
 
-const spritesheetSize = spritesheet.meta.size;
-const sprite1Frame = spritesheet.frames["create_object-hover.png"].frame;
-const sprite1 = {
-  x: sprite1Frame.x / spritesheetSize.w,
-  y: sprite1Frame.y / spritesheetSize.h,
-  w: sprite1Frame.w / spritesheetSize.w,
-  h: sprite1Frame.h / spritesheetSize.h
-};
-
-const sprite2Frame = spritesheet.frames["spawn.png"].frame;
-const sprite2 = {
-  x: sprite2Frame.x / spritesheetSize.w,
-  y: sprite2Frame.y / spritesheetSize.h,
-  w: sprite2Frame.w / spritesheetSize.w,
-  h: sprite2Frame.h / spritesheetSize.h
-};
-
-const bufferDatas = {
-  mvCols: new Float32Array([
-    1.0, 0, 0, 0,
-    0, -1, 0, 0,
-    0, 0, 1, 0,
-    -0.5, 4, 0, 1,
-
-    1.0, 0, 0, 0,
-    0, -1, 0, 0,
-    0, 0, 1, 0,
-    -0.5, 4, 0, 1,
-
-    1.0, 0, 0, 0,
-    0, -1, 0, 0,
-    0, 0, 1, 0,
-    -0.5, 4, 0, 1,
-
-    1.0, 0, 0, 0,
-    0, -1, 0, 0,
-    0, 0, 1, 0,
-    -0.5, 4, 0, 1,
-
-    1.0, 0, 0, 0,
-    0, -1, 0, 0,
-    0, 0, 1, 0,
-    0.5, 3, 1, 1,
-
-    1.0, 0, 0, 0,
-    0, -1, 0, 0,
-    0, 0, 1, 0,
-    0.5, 3, 1, 1,
-
-    1.0, 0, 0, 0,
-    0, -1, 0, 0,
-    0, 0, 1, 0,
-    0.5, 3, 1, 1,
-
-    1.0, 0, 0, 0,
-    0, -1, 0, 0,
-    0, 0, 1, 0,
-    0.5, 3, 1, 1
-  ]),
-  mvs: new Float32Array([
-    1, 0, 0, 0,
-    0, 1, 0, 0,
-    0, 0, 1, 0,
-    0, 5, 3, 1,
-
-    0.5, 0, 0, 0,
-    0, 0.5, 0, 0,
-    0, 0, 0.5, 0,
-    0, 5.0, 0, 1
-  ]),
-  uvs: new Float32Array([
-    sprite1.x,
-    sprite1.y + sprite1.h,
-
-    sprite1.x + sprite1.w,
-    sprite1.y + sprite1.h,
-
-    sprite1.x,
-    sprite1.y,
-
-    sprite1.x + sprite1.w,
-    sprite1.y,
-
-    sprite2.x,
-    sprite2.y + sprite2.h,
-
-    sprite2.x + sprite2.w,
-    sprite2.y + sprite2.h,
-
-    sprite2.x,
-    sprite2.y,
-
-    sprite2.x + sprite2.w,
-    sprite2.y
-  ]),
-  vertices: new Float32Array([
-    -0.5,
-    0.5,
-    0,
-
-    0.5,
-    0.5,
-    0,
-
-    -0.5,
-    -0.5,
-    0,
-
-    0.5,
-    -0.5,
-    0,
-
-    -0.1,
-    0.1,
-    0,
-
-    0.1,
-    0.1,
-    0,
-
-    -0.1,
-    -0.1,
-    0,
-
-    0.1,
-    -0.1,
-    0
-  ]),
-  indices: new Uint16Array([0, 2, 1, 1, 2, 3, 4, 6, 5, 5, 6, 7])
-};
-
-function updateAttribute(mesh, name){
-  mesh.geometry.attributes[name].setY(1, 0);
-  mesh.geometry.attributes[name].data.needsUpdate = true;
+function normalizedFrame(name, spritesheet) {
+  const size = spritesheet.meta.size;
+  const frame = spritesheet.frames[name].frame;
+  return {
+    x: frame.x / size.w,
+    y: frame.y / size.h,
+    w: frame.w / size.w,
+    h: frame.h / size.h
+  };
 }
 
 export class SpriteSystem {
   constructor(scene) {
+    this.spriteComponents = [];
+    this.maxSprites = 3;
     Promise.all([createImageTexture(spritesheetPng), waitForDOMContentLoaded()]).then(([spritesheetTexture]) => {
       const geometry = new THREE.BufferGeometry();
-      geometry.addAttribute("a_vertices", new THREE.BufferAttribute(bufferDatas.vertices, 3, false));
-      geometry.addAttribute("a_uvs", new THREE.BufferAttribute(bufferDatas.uvs, 2, false));
-      const mvCols = new THREE.InterleavedBuffer(new Float32Array(16*4*2), 16);
+      geometry.addAttribute(
+        "a_vertices",
+        new THREE.BufferAttribute(new Float32Array(this.maxSprites * 3 * 4), 3, false)
+      );
+      geometry.addAttribute("a_uvs", new THREE.BufferAttribute(new Float32Array(this.maxSprites * 2 * 4), 2, false));
+      const mvCols = new THREE.InterleavedBuffer(new Float32Array(this.maxSprites * 16 * 4), 16);
       geometry.addAttribute("mvCol0", new THREE.InterleavedBufferAttribute(mvCols, 4, 0, false));
       geometry.addAttribute("mvCol1", new THREE.InterleavedBufferAttribute(mvCols, 4, 4, false));
       geometry.addAttribute("mvCol2", new THREE.InterleavedBufferAttribute(mvCols, 4, 8, false));
       geometry.addAttribute("mvCol3", new THREE.InterleavedBufferAttribute(mvCols, 4, 12, false));
-      geometry.setIndex([0, 2, 1, 1, 2, 3, 4, 6, 5, 5, 6, 7]);
+      const indices = new Array(3 * 2 * this.maxSprites);
+      for (let i = 0; i < this.maxSprites; i++) {
+        indices[i * 3 * 2 + 0] = i * 4 + 0;
+        indices[i * 3 * 2 + 1] = i * 4 + 2;
+        indices[i * 3 * 2 + 2] = i * 4 + 1;
+        indices[i * 3 * 2 + 3] = i * 4 + 1;
+        indices[i * 3 * 2 + 4] = i * 4 + 2;
+        indices[i * 3 * 2 + 5] = i * 4 + 3;
+      }
+      geometry.setIndex(indices);
       const material = new THREE.RawShaderMaterial({
         uniforms: {
-          u_spritesheet: { value: spritesheetTexture },
+          u_spritesheet: { value: spritesheetTexture }
           //u_projection: { value: new Float32Array(new THREE.Matrix4().identity().elements) }
         },
         vertexShader: vert,
@@ -184,8 +85,60 @@ export class SpriteSystem {
       this.mesh.frustumCulled = false;
     });
   }
+
   tick(scene) {
     if (!this.mesh) return;
-//    this.mesh.material.uniforms.u_projection.value = new Float32Array(scene.camera.projectionMatrix.elements);
+    for (let i = 0; i < this.spriteComponents.length; i++) {
+      this.spriteComponents[i].el.object3D.updateMatrices();
+      this.set(
+        this.spriteComponents[i].data.name,
+        i,
+        this.spriteComponents[i].el.object3D.matrixWorld,
+        isVisible(this.spriteComponents[i].el.object3D)
+      );
+    }
+    this.mesh.geometry.attributes["a_uvs"].array.fill(0, 8 * this.spriteComponents.length);
+    this.mesh.geometry.attributes["a_vertices"].array.fill(0, 12 * this.spriteComponents.length);
+    this.mesh.geometry.attributes["mvCol0"].data.array.fill(0, 16 * 4 * this.spriteComponents.length);
+  }
+
+  set(spritename, i, mat4, visible) {
+    if (i >= this.maxSprites) {
+      console.warn("too many sprites");
+      return;
+    }
+    const frame = normalizedFrame(spritename, spritesheet);
+    if (visible) {
+      this.mesh.geometry.attributes["a_uvs"].setXY(i * 4 + 0, frame.x, frame.y);
+      this.mesh.geometry.attributes["a_uvs"].setXY(i * 4 + 1, frame.x + frame.w, frame.y);
+      this.mesh.geometry.attributes["a_uvs"].setXY(i * 4 + 2, frame.x, frame.y + frame.h);
+      this.mesh.geometry.attributes["a_uvs"].setXY(i * 4 + 3, frame.x + frame.w, frame.y + frame.h);
+      this.mesh.geometry.attributes["a_uvs"].needsUpdate = true;
+      this.mesh.geometry.attributes["a_vertices"].setXYZ(i * 4 + 0, -0.5, 0.5, 0);
+      this.mesh.geometry.attributes["a_vertices"].setXYZ(i * 4 + 1, 0.5, 0.5, 0);
+      this.mesh.geometry.attributes["a_vertices"].setXYZ(i * 4 + 2, -0.5, -0.5, 0);
+      this.mesh.geometry.attributes["a_vertices"].setXYZ(i * 4 + 3, 0.5, -0.5, 0);
+      this.mesh.geometry.attributes["a_vertices"].needsUpdate = true;
+      this.mesh.geometry.attributes["mvCol0"].data.array.set(mat4.elements, i * 4 * 16 + 0);
+      this.mesh.geometry.attributes["mvCol0"].data.array.set(mat4.elements, i * 4 * 16 + 1 * 16);
+      this.mesh.geometry.attributes["mvCol0"].data.array.set(mat4.elements, i * 4 * 16 + 2 * 16);
+      this.mesh.geometry.attributes["mvCol0"].data.array.set(mat4.elements, i * 4 * 16 + 3 * 16);
+      this.mesh.geometry.attributes["mvCol0"].data.needsUpdate = true;
+    } else {
+      this.mesh.geometry.attributes["a_vertices"].setXYZ(i * 4 + 0, 0, 0, 0);
+      this.mesh.geometry.attributes["a_vertices"].setXYZ(i * 4 + 1, 0, 0, 0);
+      this.mesh.geometry.attributes["a_vertices"].setXYZ(i * 4 + 2, 0, 0, 0);
+      this.mesh.geometry.attributes["a_vertices"].setXYZ(i * 4 + 3, 0, 0, 0);
+      this.mesh.geometry.attributes["a_vertices"].needsUpdate = true;
+    }
+    // .data is the interleaved buffer. so we don't set mvCol1/2/3 separately
+  }
+
+  add(sprite) {
+    this.spriteComponents.push(sprite);
+  }
+
+  remove(sprite) {
+    this.spriteComponents.splice(this.spriteComponents.indexOf(sprite), 1);
   }
 }
