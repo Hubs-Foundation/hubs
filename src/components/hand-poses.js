f329ae2291f0394aef953718775a62f05daac39c
Greg Fodor <gfodor@gmail.com>
2019-06-03 22:50:09 +0000

    Revert "Merge pull request #1397 from mozilla/revert-1386-perf/misc-optimizations"
    This reverts commit 36f1be59c43f6f684aeb11fce23482a1ff9be339, reversing
    changes made to 48b9c6381bb1ef8c1edf46a748f2cc2f0dd5b9de.

--- src/components/hand-poses.js
+++ src/components/hand-poses.js
@@ -34,6 +34,7 @@ AFRAME.registerComponent("hand-pose", {
     }
     this.from = this.to = this.mixer.clipAction(POSES.open + suffix);
     this.from.play();
+    this.networkField = `${this.id}_hand_pose`;
 
     const getNetworkedAvatar = el => {
       if (!el) {
@@ -55,13 +56,13 @@ AFRAME.registerComponent("hand-pose", {
     if (
       !this.networkedAvatar ||
       !this.networkedAvatar.data ||
-      this.networkedAvatar.data[`${this.id}_hand_pose`] === this.pose
+      this.networkedAvatar.data[this.networkField] === this.pose
     ) {
       return;
     }
 
-    this.animatePose(NETWORK_POSES[this.pose], NETWORK_POSES[this.networkedAvatar.data[`${this.id}_hand_pose`]]);
-    this.pose = this.networkedAvatar.data[`${this.id}_hand_pose`];
+    this.animatePose(NETWORK_POSES[this.pose], NETWORK_POSES[this.networkedAvatar.data[this.networkField]]);
+    this.pose = this.networkedAvatar.data[this.networkField];
   },
 
   animatePose(prev, curr) {
