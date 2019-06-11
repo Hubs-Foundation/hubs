299d3eca64b2e51df1a737a34bb6af66836fa589
Greg Fodor <gfodor@gmail.com>
2019-06-11 15:50:42 +0000

    Fix pager v2

--- src/react-components/media-browser.js
+++ src/react-components/media-browser.js
@@ -391,6 +391,7 @@ class MediaBrowser extends Component {
               history={this.props.history}
               urlSource={urlSource}
               handleEntryClicked={this.handleEntryClicked}
+              handlePager={this.handlePager}
             />
           ) : (
             <div className={styles.emptyString}>
