f1c3bb17f5519653ec1695fc61611f225a35d26d
srigaurav1986 <srigaurav1986@gmail.com>
2019-06-20 11:17:57 +0530

    Adding first file upload
    Adding first file upload

---
+++ chrome-extension/background-script.js
@@ -0,0 +1,35 @@
+﻿// this background script is used to invoke desktopCapture API
+// to capture screen-MediaStream.
+
+var screenOptions = ['screen', 'window'];
+
+chrome.runtime.onConnect.addListener(function (port) {
+    port.onMessage.addListener(portOnMessageHanlder);
+    
+    // this one is called for each message from "content-script.js"
+    function portOnMessageHanlder(message) {
+        if(message == 'get-sourceId') {
+		            chrome.desktopCapture.chooseDesktopMedia(screenOptions, port.sender.tab, onAccessApproved);
+        }
+
+        if(message == 'audio-plus-tab') {
+            screenOptions = ['audio', 'tab'];
+            chrome.desktopCapture.chooseDesktopMedia(screenOptions, port.sender.tab, onAccessApproved);
+        }
+    }
+
+    // on getting sourceId
+    // "sourceId" will be empty if permission is denied.
+    function onAccessApproved(sourceId) {
+        // if "cancel" button is clicked
+        if(!sourceId || !sourceId.length) {
+            return port.postMessage('PermissionDeniedError');
+        }
+        
+        // "ok" button is clicked; share "sourceId" with the
+        // content-script which will forward it to the webpage
+        port.postMessage({
+            sourceId: sourceId
+        });
+    }
+});
