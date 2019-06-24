d1ed745443889cdbd3649b859f3913aa2e0754dc
Greg Fodor <gfodor@gmail.com>
2019-06-24 18:27:11 +0000

    WIP

--- src/utils/auth-channel.js
+++ src/utils/auth-channel.js
@@ -28,6 +28,18 @@ export default class AuthChannel {
     this._signedIn = false;
   };
 
+  async verifyAuthentication(authTopic, authToken) {
+    const channel = this.stocket.channel(authTopic);
+    await new Promise((resolve, reject) =>
+      channel
+        .join()
+        .receive("ok", () => {
+          channel.push("auth_verified", { token: authToken });
+        })
+        .receive("error", reject)
+    );
+  }
+
   async startAuthentication(email, hubChannel) {
     const channel = this.socket.channel(`auth:${uuid()}`);
     await new Promise((resolve, reject) =>
@@ -39,11 +51,7 @@ export default class AuthChannel {
 
     const authComplete = new Promise(resolve =>
       channel.on("auth_credentials", async ({ credentials: token }) => {
-        this.store.update({ credentials: { email, token } });
-        if (hubChannel) {
-          await hubChannel.signIn(token);
-        }
-        this._signedIn = true;
+        await this.handleAuthCredentials(email, token, hubChannel);
         resolve();
       })
     );
@@ -53,5 +61,15 @@ export default class AuthChannel {
     // Returning an object with the authComplete promise since we want the caller to wait for the above await but not
     // for authComplete.
     return { authComplete };
+  }
+
+  async handleAuthCredentials(email, token, hubChannel) {
+    this.store.update({ credentials: { email, token } });
+
+    if (hubChannel) {
+      await hubChannel.signIn(token);
+    }
+
+    this._signedIn = true;
   }
 }
