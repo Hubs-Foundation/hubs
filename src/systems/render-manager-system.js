9cbe360c181870103e501331ed4ac2d113398bda
netpro2k <netpro2k@gmail.com>
2019-07-08 17:58:41 -0700

    Change how BatchRawUniformGroup update() works

--- src/systems/render-manager-system.js
+++ src/systems/render-manager-system.js
@@ -7,6 +7,8 @@ const tempVec3 = new Array(3);
 const tempVec4 = new Array(4);
 
 export const INSTANCE_DATA_BYTE_LENGTH = 112;
+const DEFAULT_COLOR = new THREE.Color(1, 1, 1);
+const HIDE_MATRIX = new THREE.Matrix4().makeScale(0, 0, 0);
 
 class HubsBatchRawUniformGroup extends BatchRawUniformGroup {
   constructor(maxInstances, meshToEl) {
@@ -42,55 +44,65 @@ class HubsBatchRawUniformGroup extends BatchRawUniformGroup {
     this.meshToEl = meshToEl;
   }
 
-  update(instanceId, mesh) {
-    mesh.updateMatrices();
+  update(time) {
+    let interactorOne, interactorTwo;
+    for (let instanceId = 0; instanceId < this.meshes.length; instanceId++) {
+      const mesh = this.meshes[instanceId];
+      if (!mesh) continue;
 
-    super.update(instanceId, mesh);
+      mesh.updateMatrices();
 
-    const el = this.meshToEl.get(mesh);
-    if (el) {
-      const obj = el.object3D;
-      const hoverableVisuals = el.components["hoverable-visuals"];
-      const worldY = obj.matrixWorld.elements[13];
-      const scaledRadius = obj.scale.y * hoverableVisuals.boundingSphere.radius;
+      // TODO need to account for nested visibility deeper than 1 level
+      this.setInstanceTransform(instanceId, mesh.visible && mesh.parent.visible ? mesh.matrixWorld : HIDE_MATRIX);
+      this.setInstanceColor(instanceId, mesh.material.color || DEFAULT_COLOR, mesh.material.opacity || 1);
 
-      const isPinned = el.components.pinnable && el.components.pinnable.data.pinned;
-      const isSpawner = !!el.components["super-spawner"];
-      const isFrozen = el.sceneEl.is("frozen");
-      const hideDueToPinning = !isSpawner && isPinned && !isFrozen;
+      const el = this.meshToEl.get(mesh);
+      if (el) {
+        const obj = el.object3D;
+        const hoverableVisuals = el.components["hoverable-visuals"];
+        const worldY = obj.matrixWorld.elements[13];
+        const scaledRadius = obj.scale.y * hoverableVisuals.boundingSphere.radius;
 
-      const interaction = AFRAME.scenes[0].systems.interaction;
-      let interactorOne, interactorTwo;
-      if (interaction.state.leftHand.hovered === el || interaction.state.leftHand.held === el) {
-        interactorOne = interaction.options.leftHand.entity.object3D;
-      }
+        const isPinned = el.components.pinnable && el.components.pinnable.data.pinned;
+        const isSpawner = !!el.components["super-spawner"];
+        const isFrozen = el.sceneEl.is("frozen");
+        const hideDueToPinning = !isSpawner && isPinned && !isFrozen;
 
-      if (interaction.state.rightRemote.hovered === el || interaction.state.rightRemote.held === el) {
-        interactorTwo = interaction.options.rightRemote.entity.object3D;
-      } else if (interaction.state.rightHand.hovered === el || interaction.state.rightHand.held === el) {
-        interactorTwo = interaction.options.rightHand.entity.object3D;
-      }
+        const interaction = AFRAME.scenes[0].systems.interaction;
+        if (interaction.state.leftHand.hovered === el || interaction.state.leftHand.held === el) {
+          interactorOne = interaction.options.leftHand.entity.object3D;
+        }
 
-      tempVec4[0] = worldY - scaledRadius;
-      tempVec4[1] = worldY + scaledRadius;
-      tempVec4[2] = !!interactorOne && !hideDueToPinning;
-      tempVec4[3] = !!interactorTwo && !hideDueToPinning;
-      this.hubs_sweepParams.set(tempVec4, instanceId * 4);
+        if (interaction.state.rightRemote.hovered === el || interaction.state.rightRemote.held === el) {
+          interactorTwo = interaction.options.rightRemote.entity.object3D;
+        } else if (interaction.state.rightHand.hovered === el || interaction.state.rightHand.held === el) {
+          interactorTwo = interaction.options.rightHand.entity.object3D;
+        }
 
-      if (interactorOne) {
-        tempVec3[0] = interactorOne.matrixWorld.elements[12];
-        tempVec3[1] = interactorOne.matrixWorld.elements[13];
-        tempVec3[2] = interactorOne.matrixWorld.elements[14];
-        this.hubs_interactorOnePos.set(tempVec3);
-      }
-
-      if (interactorTwo) {
-        tempVec3[0] = interactorTwo.matrixWorld.elements[12];
-        tempVec3[1] = interactorTwo.matrixWorld.elements[13];
-        tempVec3[2] = interactorTwo.matrixWorld.elements[14];
-        this.hubs_interactorTwoPos.set(tempVec3);
+        tempVec4[0] = worldY - scaledRadius;
+        tempVec4[1] = worldY + scaledRadius;
+        tempVec4[2] = !!interactorOne && !hideDueToPinning;
+        tempVec4[3] = !!interactorTwo && !hideDueToPinning;
+        this.hubs_sweepParams.set(tempVec4, instanceId * 4);
       }
     }
+
+    if (interactorOne) {
+      tempVec3[0] = interactorOne.matrixWorld.elements[12];
+      tempVec3[1] = interactorOne.matrixWorld.elements[13];
+      tempVec3[2] = interactorOne.matrixWorld.elements[14];
+      this.hubs_interactorOnePos.set(tempVec3);
+    }
+
+    if (interactorTwo) {
+      tempVec3[0] = interactorTwo.matrixWorld.elements[12];
+      tempVec3[1] = interactorTwo.matrixWorld.elements[13];
+      tempVec3[2] = interactorTwo.matrixWorld.elements[14];
+      this.hubs_interactorTwoPos.set(tempVec3);
+    }
+
+    this.hubs_time[0] = time;
+    this.hubs_isFrozen[0] = AFRAME.scenes[0].is("frozen") ? 1 : 0;
   }
 }
 
@@ -135,8 +147,6 @@ export class RenderManagerSystem {
   }
 
   tick(time) {
-    this.ubo.hubs_time[0] = time;
-    this.ubo.hubs_isFrozen[0] = AFRAME.scenes[0].is("frozen") ? 1 : 0;
     this.batchManager.update(time);
   }
 
