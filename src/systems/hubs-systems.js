665b520c8cac99b428d22ffcc40769e3b9f4e6ca
netpro2k <netpro2k@gmail.com>
2019-07-11 16:28:45 -0700

    Remove unused HoverVisualsSystem

--- src/systems/hubs-systems.js
+++ src/systems/hubs-systems.js
@@ -6,57 +6,7 @@ import { HoverMenuSystem } from "./hover-menu-system";
 import { SuperSpawnerSystem } from "./super-spawner-system";
 import { HapticFeedbackSystem } from "./haptic-feedback-system";
 import { SoundEffectsSystem } from "./sound-effects-system";
-// import HoverVisualsSystem from "./highlight/hover-visuas-system";
 
-export default class HoverVisualsSystem {
-  constructor() {
-    this.prevHighlights = {
-      rightRemote: {
-        entity: null,
-        meshes: []
-      },
-      rightHand: {
-        entity: null,
-        meshes: []
-      },
-      leftHand: {
-        entity: null,
-        meshes: []
-      }
-    };
-  }
-
-  updateMesh(hand) {
-    const interactionSystem = AFRAME.scenes[0].systems.interaction;
-    const interaction = interactionSystem.state[hand];
-
-    const entity = interaction.hovered || interaction.held;
-    if (entity != this.prevHighlights[hand].entity) {
-      this.prevHighlights[hand].meshes.forEach(o => {
-        o.layers.disable(0);
-      });
-      this.prevHighlights[hand].meshes.length = 0;
-      this.prevHighlights[hand].entity = entity;
-
-      if (entity && entity.object3D && entity.components["hoverable-visuals"]) {
-        entity.object3D.traverseVisible(o => {
-          if (!o.isMesh || !o.batched) return;
-          o.layers.enable(0);
-          o.material.polygonOffset = true;
-          o.material.polygonOffsetFactor = -2;
-          o.material.polygonOffsetUnits = -3;
-          this.prevHighlights[hand].meshes.push(o);
-        });
-      }
-    }
-  }
-
-  tick() {
-    this.updateMesh("rightRemote");
-    this.updateMesh("rightHand");
-    this.updateMesh("leftHand");
-  }
-}
 import { BatchManagerSystem } from "./render-manager-system";
 import { LobbyCameraSystem } from "./lobby-camera-system";
 
@@ -72,7 +22,6 @@ AFRAME.registerSystem("hubs-systems", {
     this.superSpawnerSystem = new SuperSpawnerSystem();
     this.hapticFeedbackSystem = new HapticFeedbackSystem();
     this.soundEffectsSystem = new SoundEffectsSystem();
-    this.hoverVisualsSystem = new HoverVisualsSystem(this.el.sceneEl.object3D);
     this.lobbyCameraSystem = new LobbyCameraSystem();
     this.batchManagerSystem = new BatchManagerSystem(this.el.sceneEl.object3D, this.el.sceneEl.renderer);
   },
@@ -91,7 +40,6 @@ AFRAME.registerSystem("hubs-systems", {
     this.hoverMenuSystem.tick();
     this.hapticFeedbackSystem.tick(this.twoPointStretchingSystem, this.singleActionButtonSystem.didInteractThisFrame);
     this.soundEffectsSystem.tick();
-    this.hoverVisualsSystem.tick(t);
     this.lobbyCameraSystem.tick();
     // batchManager is ticked in "post-physics" system
   },
