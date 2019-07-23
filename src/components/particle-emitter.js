a363263f675648e4db3fa86fc20312def6a75708
Robert Long <robert@robertlong.me>
2019-07-23 15:35:20 -0700

    Simplify particle-emitter update method

--- src/components/particle-emitter.js
+++ src/components/particle-emitter.js
@@ -50,88 +50,37 @@ AFRAME.registerComponent("particle-emitter", {
       );
     }
 
-    if (prevData.startColor !== data.startColor) {
-      particleEmitter.startColor.set(data.startColor);
+    if (
+      prevData.startColor !== data.startColor ||
+      prevData.startSize !== data.startSize ||
+      prevData.sizeRandomness !== data.sizeRandomness ||
+      prevData.ageRandomness !== data.ageRandomness ||
+      prevData.lifetime !== data.lifetime ||
+      prevData.lifetimeRandomness !== data.lifetimeRandomness ||
+      prevData.particleCount !== data.particleCount
+    ) {
       this.updateParticles = true;
     }
 
-    if (prevData.middleColor !== data.middleColor) {
-      particleEmitter.middleColor.set(data.middleColor);
-    }
-
-    if (prevData.endColor !== data.endColor) {
-      particleEmitter.endColor.set(data.endColor);
-    }
-
-    if (prevData.startOpacity !== data.startOpacity) {
-      particleEmitter.startOpacity = data.startOpacity;
-    }
-
-    if (prevData.middleOpacity !== data.middleOpacity) {
-      particleEmitter.middleOpacity = data.middleOpacity;
-    }
-
-    if (prevData.endOpacity !== data.endOpacity) {
-      particleEmitter.endOpacity = data.endOpacity;
-    }
-
-    if (prevData.colorCurve !== data.colorCurve) {
-      particleEmitter.colorCurve = data.colorCurve;
-    }
-
-    if (prevData.sizeCurve !== data.sizeCurve) {
-      particleEmitter.sizeCurve = data.sizeCurve;
-    }
-
-    if (prevData.startSize !== data.startSize) {
-      particleEmitter.startSize = data.startSize;
-      this.updateParticles = true;
-    }
-
-    if (prevData.endSize !== data.endSize) {
-      particleEmitter.endSize = data.endSize;
-    }
-
-    if (prevData.sizeRandomness !== data.sizeRandomness) {
-      particleEmitter.sizeRandomness = data.sizeRandomness;
-      this.updateParticles = true;
-    }
-
-    if (prevData.ageRandomness !== data.ageRandomness) {
-      particleEmitter.ageRandomness = data.ageRandomness;
-      this.updateParticles = true;
-    }
-
-    if (prevData.lifetime !== data.lifetime) {
-      particleEmitter.lifetime = data.lifetime;
-      this.updateParticles = true;
-    }
-
-    if (prevData.lifetimeRandomness !== data.lifetimeRandomness) {
-      particleEmitter.lifetimeRandomness = data.lifetimeRandomness;
-      this.updateParticles = true;
-    }
-
-    if (prevData.particleCount !== data.particleCount) {
-      particleEmitter.particleCount = data.particleCount;
-      this.updateParticles = true;
-    }
-
-    if (prevData.startVelocity !== data.startVelocity) {
-      particleEmitter.startVelocity.copy(data.startVelocity);
-    }
-
-    if (prevData.endVelocity !== data.endVelocity) {
-      particleEmitter.endVelocity.copy(data.endVelocity);
-    }
-
-    if (prevData.velocityCurve !== data.velocityCurve) {
-      particleEmitter.velocityCurve = data.velocityCurve;
-    }
-
-    if (prevData.angularVelocity !== data.angularVelocity) {
-      particleEmitter.angularVelocity = data.angularVelocity;
-    }
+    particleEmitter.startColor.set(data.startColor);
+    particleEmitter.middleColor.set(data.middleColor);
+    particleEmitter.endColor.set(data.endColor);
+    particleEmitter.startOpacity = data.startOpacity;
+    particleEmitter.middleOpacity = data.middleOpacity;
+    particleEmitter.endOpacity = data.endOpacity;
+    particleEmitter.colorCurve = data.colorCurve;
+    particleEmitter.sizeCurve = data.sizeCurve;
+    particleEmitter.startSize = data.startSize;
+    particleEmitter.endSize = data.endSize;
+    particleEmitter.sizeRandomness = data.sizeRandomness;
+    particleEmitter.ageRandomness = data.ageRandomness;
+    particleEmitter.lifetime = data.lifetime;
+    particleEmitter.lifetimeRandomness = data.lifetimeRandomness;
+    particleEmitter.particleCount = data.particleCount;
+    particleEmitter.startVelocity.copy(data.startVelocity);
+    particleEmitter.endVelocity.copy(data.endVelocity);
+    particleEmitter.velocityCurve = data.velocityCurve;
+    particleEmitter.angularVelocity = data.angularVelocity;
   },
 
   tick(time, dt) {
