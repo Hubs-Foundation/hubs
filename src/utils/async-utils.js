fd4c157f2be20eb073a0a3d17af813634871922a
netpro2k <netpro2k@gmail.com>
2019-07-30 14:56:08 -0700

    Must define waitForEvent before it is used

--- src/utils/async-utils.js
+++ src/utils/async-utils.js
@@ -1,13 +1,13 @@
+export const waitForEvent = function(eventName, eventObj) {
+  return new Promise(resolve => {
+    eventObj.addEventListener(eventName, resolve, { once: true });
+  });
+};
+
 export const waitForDOMContentLoaded = function() {
   if (document.readyState === "complete" || document.readyState === "loaded" || document.readyState === "interactive") {
     return Promise.resolve(null);
   } else {
     return waitForEvent("DOMContentLoaded", window);
   }
-};
-
-export const waitForEvent = function(eventName, eventObj) {
-  return new Promise(resolve => {
-    eventObj.addEventListener(eventName, resolve, { once: true });
-  });
 };
