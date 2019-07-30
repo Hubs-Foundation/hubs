8777ec62c5843738e3d3edb1cee4b762235609c6
netpro2k <netpro2k@gmail.com>
2019-07-30 14:41:32 -0700

    Wait for DOMContentLoaded to query for head node

--- src/utils/async-utils.js
+++ src/utils/async-utils.js
@@ -2,9 +2,7 @@ export const waitForDOMContentLoaded = function() {
   if (document.readyState === "complete" || document.readyState === "loaded" || document.readyState === "interactive") {
     return Promise.resolve(null);
   } else {
-    return new Promise(resolve => {
-      window.addEventListener("DOMContentLoaded", resolve);
-    });
+    return waitForEvent("DOMContentLoaded", window);
   }
 };
 
