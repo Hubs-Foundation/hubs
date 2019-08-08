6eab70a4f6bd7d77a3dfd7504d17131aa64afa74
John Shaughnessy <johnfshaughnessy@gmail.com>
2019-08-08 11:31:47 -0700

    Hopefully fix initialization issue

--- src/systems/userinput/resolve-action-sets.js
+++ src/systems/userinput/resolve-action-sets.js
@@ -3,8 +3,15 @@ import { sets } from "./sets";
 let leftTeleporter, rightTeleporter;
 
 export function resolveActionSets() {
-  leftTeleporter = leftTeleporter || document.querySelector("#player-left-controller").components.teleporter;
-  rightTeleporter = rightTeleporter || document.querySelector("#player-right-controller").components.teleporter;
+  leftTeleporter =
+    leftTeleporter ||
+    (document.getElementById("player-left-controller") &&
+      document.getElementById("player-left-controller").components.teleporter);
+  rightTeleporter =
+    rightTeleporter ||
+    (document.getElementById("player-right-controller") &&
+      document.getElementById("player-right-controller").components.teleporter);
+  if (!leftTeleporter && !rightTeleporter) return;
   const userinput = AFRAME.scenes[0].systems.userinput;
   const { leftHand, rightHand, rightRemote } = AFRAME.scenes[0].systems.interaction.state;
 
