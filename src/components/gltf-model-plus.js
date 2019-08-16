ea28c250b6ddd64ee74646b93a7ae88d4067f80e
Brian Peiris <brianpeiris@gmail.com>
2019-08-16 10:39:59 -0700

    refactor

--- src/components/gltf-model-plus.js
+++ src/components/gltf-model-plus.js
@@ -370,6 +370,15 @@ export async function loadModel(src, contentType = null, useCache = false, jsonP
   }
 }
 
+function resolveAsset(src) {
+  // If the src attribute is a selector, get the url from the asset item.
+  if (src && src.charAt(0) === "#") {
+    const assetEl = document.getElementById(src.substring(1));
+    return assetEl.getAttribute("src");
+  }
+  return src;
+}
+
 /**
  * Loads a GLTF model, optionally recursively "inflates" the child nodes of a model into a-entities and sets
  * whitelisted components on them if defined in the node's extras.
@@ -394,14 +403,14 @@ AFRAME.registerComponent("gltf-model-plus", {
   },
 
   update() {
-    this.applySrc(this.data.src, this.data.contentType);
+    this.applySrc(resolveAsset(this.data.src), this.data.contentType);
   },
 
   remove() {
     if (this.data.batch && this.model) {
       this.el.sceneEl.systems["hubs-systems"].batchManagerSystem.removeObject(this.el.object3DMap.mesh);
     }
-    const src = this.resolveAsset(this.data.src);
+    const src = resolveAsset(this.data.src);
     if (src) {
       gltfCache.release(src);
     }
@@ -415,19 +424,8 @@ AFRAME.registerComponent("gltf-model-plus", {
     });
   },
 
-  resolveAsset(src) {
-    // If the src attribute is a selector, get the url from the asset item.
-    if (src && src.charAt(0) === "#") {
-      const assetEl = document.getElementById(src.substring(1));
-      return assetEl.getAttribute("src");
-    }
-    return src;
-  },
-
   async applySrc(src, contentType) {
     try {
-      src = this.resolveAsset(src);
-
       if (src === this.lastSrc) return;
 
       const lastSrc = this.lastSrc;
