7cee35f978ed114ad1110fc9ed98cc23ccd482f7
Greg Fodor <gfodor@gmail.com>
2019-10-08 23:10:04 +0000

    new build script

--- scripts/hab-build-and-push.sh
+++ scripts/hab-build-and-push.sh
@@ -6,41 +6,60 @@ export RETICULUM_SERVER=$3
 export THUMBNAIL_SERVER=$4
 export CORS_PROXY_SERVER=$5
 export NON_CORS_PROXY_DOMAINS=$6
-export TARGET_S3_URL=$7
+export TARGET_S3_BUCKET=$7
 export SENTRY_DSN=$8
 export GA_TRACKING_ID=${9}
 export BUILD_NUMBER=${10}
 export GIT_COMMIT=${11}
+export HAB_ORG=${12}
+export HAB_RING=${13}
+export HAB_HOST=${14}
 export BUILD_VERSION="${BUILD_NUMBER} (${GIT_COMMIT})"
 
-# To build + push to S3 run:
-# hab studio run "bash scripts/hab-build-and-push.sh"
-# On exit, need to make all files writable so CI can clean on next build
-
-trap 'chmod -R a+rw .' EXIT
+# Build the package, upload it, and start the service so we deploy to staging target.
 
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+source "$DIR/../habitat/plan.sh"
+PKG="$pkg_origin/$pkg_name"
 
 pushd "$DIR/.."
 
-rm /usr/bin/env
-ln -s "$(hab pkg path core/coreutils)/bin/env" /usr/bin/env
-hab pkg install -b core/coreutils core/bash core/node10 core/git core/aws-cli core/python2
+trap "chmod -R a+rw . && rm /hab/svc/$pkg_name/var/deploying" EXIT
 
-npm ci --verbose --no-progress
-npm rebuild node-sass # HACK sometimes node-sass build fails
-npm rebuild node-sass # HACK sometimes node-sass build fails
-npm rebuild node-sass # HACK sometimes node-sass build fails
-npm run build
-mkdir dist/pages
-mv dist/*.html dist/pages
-mv dist/hub.service.js dist/pages
-mv dist/manifest.webmanifest dist/pages
+# Wait for a lock file so we serialize deploys
+mkdir -p /hab/svc/$pkg_name/var
+while [ -f /hab/svc/$pkg_name/var/deploying ]; do sleep 1; done
+touch /hab/svc/$pkg_name/var/deploying
 
-# we need to upload wasm blobs with wasm content type explicitly because, unlike all our
-# other assets, AWS's built-in MIME type dictionary doesn't know about that one
-aws s3 sync --acl public-read --cache-control "max-age=31556926" --include "*" --exclude "*.wasm" dist/assets "$TARGET_S3_URL/hubs/assets"
-aws s3 sync --acl public-read --cache-control "max-age=31556926" --exclude "*" --include "*.wasm" --content-type "application/wasm" dist/assets "$TARGET_S3_URL/hubs/assets"
+rm -rf results
+mkdir -p results
+hab pkg build .
+hab svc unload $PKG
+/usr/bin/hab-pkg-install results/*.hart
+hab svc load $PKG
+hab svc stop $PKG
 
-aws s3 sync --acl public-read --cache-control "no-cache" --delete dist/pages "$TARGET_S3_URL/hubs/pages/latest"
-aws s3 sync --acl public-read --cache-control "no-cache" --delete dist/pages "$TARGET_S3_URL/hubs/pages/releases/${BUILD_NUMBER}"
+cat > build-config.toml << EOTOML
+[general]
+default_scene_sid = "$DEFAULT_SCENES_SID"
+base_assets_path = "$BASE_ASSETS_PATH"
+reticulum_server = "$RETICULUM_SERVER"
+thumbnail_server = "$THUMBNAIL_SERVER"
+cors_proxy_server = "$CORS_PROXY_SERVER"
+non_cors_proxy_domains = "$NON_CORS_PROXY_DOMAINS"
+sentry_dsn = "$SENTRY_DSN"
+ga_tracking_id = "$GA_TRACKING_ID"
+
+[deploy]
+type = "s3"
+target = "$TARGET_S3_BUCKET"
+region = "us-west-1"
+EOTOML
+
+cat build-config.toml | hab config apply -r $HAB_HOST "$pkg_name.default@$HAB_ORG" $(date +%s)
+cat build-config.toml
+rm build-config.toml
+hab svc start $PKG
+
+# Add a short buffer to help ensure s3 sync completes before slack message
+sleep 5
