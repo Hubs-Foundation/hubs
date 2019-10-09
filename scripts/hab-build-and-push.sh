7817758b0f5d5cd4f1f16f377e048fef2f4b6085
Greg Fodor <gfodor@gmail.com>
2019-10-09 05:21:56 +0000

    More fixes to building

--- scripts/hab-build-and-push.sh
+++ scripts/hab-build-and-push.sh
@@ -11,9 +11,6 @@ export SENTRY_DSN=$8
 export GA_TRACKING_ID=${9}
 export BUILD_NUMBER=${10}
 export GIT_COMMIT=${11}
-export HAB_ORG=${12}
-export HAB_RING=${13}
-export HAB_HOST=${14}
 export BUILD_VERSION="${BUILD_NUMBER} (${GIT_COMMIT})"
 
 # Build the package, upload it, and start the service so we deploy to staging target.
@@ -24,7 +21,7 @@ PKG="$pkg_origin/$pkg_name"
 
 pushd "$DIR/.."
 
-trap "chmod -R a+rw . && rm /hab/svc/$pkg_name/var/deploying" EXIT
+trap "sudo /usr/bin/hab-clean-perms && chmod -R a+rw . && rm /hab/svc/$pkg_name/var/deploying" EXIT
 
 # Wait for a lock file so we serialize deploys
 mkdir -p /hab/svc/$pkg_name/var
@@ -33,33 +30,31 @@ touch /hab/svc/$pkg_name/var/deploying
 
 rm -rf results
 mkdir -p results
-hab pkg build .
+sudo /usr/bin/hab-docker-studio -k mozillareality run build
 hab svc unload $PKG
-/usr/bin/hab-pkg-install results/*.hart
+sudo /usr/bin/hab-pkg-install results/*.hart
 hab svc load $PKG
 hab svc stop $PKG
 
+# Apparently these vars come in from jenkins with quotes already
 cat > build-config.toml << EOTOML
 [general]
-default_scene_sid = "$DEFAULT_SCENES_SID"
-base_assets_path = "$BASE_ASSETS_PATH"
-reticulum_server = "$RETICULUM_SERVER"
-thumbnail_server = "$THUMBNAIL_SERVER"
-cors_proxy_server = "$CORS_PROXY_SERVER"
-non_cors_proxy_domains = "$NON_CORS_PROXY_DOMAINS"
-sentry_dsn = "$SENTRY_DSN"
-ga_tracking_id = "$GA_TRACKING_ID"
+default_scene_sid = $DEFAULT_SCENE_SID
+base_assets_path = $BASE_ASSETS_PATH
+reticulum_server = $RETICULUM_SERVER
+thumbnail_server = $THUMBNAIL_SERVER
+cors_proxy_server = $CORS_PROXY_SERVER
+non_cors_proxy_domains = $NON_CORS_PROXY_DOMAINS
+sentry_dsn = $SENTRY_DSN
+ga_tracking_id = $GA_TRACKING_ID
 
 [deploy]
 type = "s3"
-target = "$TARGET_S3_BUCKET"
+target = $TARGET_S3_BUCKET
 region = "us-west-1"
 EOTOML
 
-cat build-config.toml | hab config apply -r $HAB_HOST "$pkg_name.default@$HAB_ORG" $(date +%s)
 cat build-config.toml
-rm build-config.toml
+sudo /usr/bin/hab-user-toml-install hubs build-config.toml
 hab svc start $PKG
-
-# Add a short buffer to help ensure s3 sync completes before slack message
-sleep 5
+sudo /usr/bin/hab-pkg-upload results/*.hart
