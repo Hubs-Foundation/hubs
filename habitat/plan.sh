a43c1d9c197dfc7a825407c7bc4f08baacb319c8
Marshall Polaris <marshall@pol.rs>
2019-10-10 14:12:57 -0700

    Fix up admin build to work with new Habitat build

--- habitat/plan.sh
+++ habitat/plan.sh
@@ -19,6 +19,8 @@ pkg_deps=(
 
 do_build() {
   ln -s "$(hab pkg path core/coreutils)/bin/env" /usr/bin/env
+
+  # main client
   npm ci --verbose --no-progress
   npm rebuild node-sass # HACK sometimes node-sass build fails
   npm rebuild node-sass # HACK sometimes node-sass build fails
@@ -27,16 +29,29 @@ do_build() {
   # We inject random tokens into the build that will be replaced at run webhook/deploy time with the actual runtime configs.
   export DEFAULT_SCENE_SID="$(echo "default_scene_sid" | sha256sum | cut -d' ' -f1)"
   export BASE_ASSETS_PATH="$(echo "base_assets_path" | sha256sum | cut -d' ' -f1)/" # HACK need a trailing slash so webpack'ed semantics line up
-  export RETICULUM_SERVER=$(echo "reticulum_server" | sha256sum | cut -d' ' -f1) 
-  export THUMBNAIL_SERVER=$(echo "thumbnail_server" | sha256sum | cut -d' ' -f1) 
-  export CORS_PROXY_SERVER=$(echo "cors_proxy_server" | sha256sum | cut -d' ' -f1) 
-  export NON_CORS_PROXY_DOMAINS=$(echo "non_cors_proxy_domains" | sha256sum | cut -d' ' -f1) 
-  export POSTGREST_SERVER=$(echo "postgrest_server" | sha256sum | cut -d' ' -f1) 
-  export SENTRY_DSN=$(echo "sentry_dsn" | sha256sum | cut -d' ' -f1) 
-  export GA_TRACKING_ID=$(echo "ga_tracking_id" | sha256sum | cut -d' ' -f1) 
+  export RETICULUM_SERVER=$(echo "reticulum_server" | sha256sum | cut -d' ' -f1)
+  export THUMBNAIL_SERVER=$(echo "thumbnail_server" | sha256sum | cut -d' ' -f1)
+  export CORS_PROXY_SERVER=$(echo "cors_proxy_server" | sha256sum | cut -d' ' -f1)
+  export NON_CORS_PROXY_DOMAINS=$(echo "non_cors_proxy_domains" | sha256sum | cut -d' ' -f1)
+  export SENTRY_DSN=$(echo "sentry_dsn" | sha256sum | cut -d' ' -f1)
+  export GA_TRACKING_ID=$(echo "ga_tracking_id" | sha256sum | cut -d' ' -f1)
   export BUILD_VERSION="${pkg_version}.$(echo $pkg_prefix | cut -d '/' -f 7)"
 
   npm run build
+
+  # admin
+  cd admin
+  npm ci --verbose --no-progress
+  npm rebuild node-sass # HACK sometimes node-sass build fails
+  npm rebuild node-sass # HACK sometimes node-sass build fails
+  npm rebuild node-sass # HACK sometimes node-sass build fails
+
+  export RETICULUM_SERVER=$(echo "reticulum_server" | sha256sum | cut -d' ' -f1)
+  export ITA_SERVER=$(echo "ita_server" | sha256sum | cut -d' ' -f1)
+  export POSTGREST_SERVER=$(echo "postgrest_server" | sha256sum | cut -d' ' -f1)
+  npm run build
+  cp -R dist/* ../dist # it will get packaged with the rest of the stuff, below
+  cd ..
 
   mkdir -p dist/pages
   mv dist/*.html dist/pages
