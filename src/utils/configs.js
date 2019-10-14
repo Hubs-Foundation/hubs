56d68920797e623bffcd8b170d7a59e502ae5acc
Greg Fodor <gfodor@gmail.com>
2019-10-14 23:29:06 +0000

    Try setting base asset path at runtime

--- src/utils/configs.js
+++ src/utils/configs.js
@@ -9,10 +9,16 @@ const configs = {};
   "NON_CORS_PROXY_DOMAINS",
   "SENTRY_DSN",
   "GA_TRACKING_ID",
-  "BUILD_VERSION"
+  "BUILD_VERSION",
+  "BASE_ASSETS_PATH"
 ].forEach(x => {
   const el = document.querySelector(`meta[name='env:${x.toLowerCase()}']`);
   configs[x] = el ? el.getAttribute("content") : process.env[x];
+
+  if (x === "BASE_ASSETS_PATH" && configs[x]) {
+    // eslint-disable-next-line no-undef
+    __webpack_public_path__ = configs[x];
+  }
 });
 
 export default configs;
