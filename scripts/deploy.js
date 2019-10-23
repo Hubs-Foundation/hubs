52d7c26bdf0c17acf1f24fea590dd4fd6be58aa1
Greg Fodor <gfodor@gmail.com>
2019-10-23 17:29:30 +0000

    Deploy working

--- scripts/deploy.js
+++ scripts/deploy.js
@@ -1,8 +1,10 @@
-import { readFileSync, existsSync } from "fs";
+import { readFileSync, existsSync, unlinkSync } from "fs";
 import { exec } from "child_process";
 import rmdir from "rimraf";
 import ncp from "ncp";
 import tar from "tar";
+import request from "request";
+import ora from "ora";
 
 if (!existsSync(".ret.credentials")) {
   console.log("Not logged in, so cannot deploy. To log in, run npm run login.");
@@ -10,7 +12,8 @@ if (!existsSync(".ret.credentials")) {
 }
 
 const { host, token } = JSON.parse(readFileSync(".ret.credentials"));
-console.log(`Deploying to ${host}.`);
+const spinner = ora(`Deploying to ${host}.`).start();
+const step = ora({ indent: 2 }).start();
 
 const getTs = (() => {
   const p = n => (n < 10 ? `0${n}` : n);
@@ -24,7 +27,8 @@ const getTs = (() => {
 
 (async () => {
   const headers = {
-    Authorization: `Bearer ${token}`
+    Authorization: `Bearer ${token}`,
+    "Content-Type": "application/json"
   };
 
   const res = await fetch(`https://${host}/api/ita/configs/hubs`, { headers });
@@ -37,62 +41,70 @@ const getTs = (() => {
   buildEnv.BUILD_VERSION = `1.0.0.${getTs()}`;
   const env = Object.assign(process.env, buildEnv);
 
-  //for (const d in ["./dist", "./admin/dist"]) {
-  //  rmdir(d, err => {
-  //    if (err) {
-  //      console.error(err);
-  //      process.exit(1);
-  //    }
-  //  });
-  //}
+  for (const d in ["./dist", "./admin/dist"]) {
+    rmdir(d, err => {
+      if (err) {
+        console.error(err);
+        process.exit(1);
+      }
+    });
+  }
 
-  console.log("Building Client.");
+  step.text = "Building Client.";
 
-  //await new Promise((resolve, reject) => {
-  //  exec("npm ci", { }, err => {
-  //    if (err) reject(err);
-  //    resolve();
-  //  });
-  //});
+  await new Promise((resolve, reject) => {
+    exec("npm ci", {}, err => {
+      if (err) reject(err);
+      resolve();
+    });
+  });
 
-  //await new Promise((resolve, reject) => {
-  //  exec("npm run build", { env }, (err, stdout) => {
-  //    if (err) reject(err);
-  //    resolve();
-  //  });
-  //});
+  await new Promise((resolve, reject) => {
+    exec("npm run build", { env }, err => {
+      if (err) reject(err);
+      resolve();
+    });
+  });
 
-  console.log("Building Admin Console.");
+  step.text = "Building Admin Console.";
 
-  //await new Promise((resolve, reject) => {
-  //  exec("npm ci", { cwd: "./admin" }, err => {
-  //    if (err) reject(err);
-  //    resolve();
-  //  });
-  //});
+  await new Promise((resolve, reject) => {
+    exec("npm ci", { cwd: "./admin" }, err => {
+      if (err) reject(err);
+      resolve();
+    });
+  });
 
-  //await new Promise((resolve, reject) => {
-  //  exec("npm run build", { cwd: "./admin", env }, err => {
-  //    if (err) reject(err);
-  //    resolve();
-  //  });
-  //});
+  await new Promise((resolve, reject) => {
+    exec("npm run build", { cwd: "./admin", env }, err => {
+      if (err) reject(err);
+      resolve();
+    });
+  });
 
-  //await new Promise(res => {
-  //  ncp("./admin/dist", "./dist", err => {
-  //    if (err) {
-  //      console.error(err);
-  //      process.exit(1);
-  //    }
+  await new Promise(res => {
+    ncp("./admin/dist", "./dist", err => {
+      if (err) {
+        console.error(err);
+        process.exit(1);
+      }
 
-  //    res();
-  //  });
-  //});
-  console.log("Packaging Build.");
-  const buildFile = `hubs-build-${buildEnv.BUILD_VERSION}.tar.gz`;
-  await tar.c({ gzip: true, C: "dist", file: buildFile }, ["."]);
+      res();
+    });
+  });
+  step.text = "Packaging Build.";
   const uploadRes = await fetch(`https://${host}/api/ita/deploy/hubs/upload_url`, { headers });
-  const uploadInfo = await uploadRes.json();
-
+  const { url, version } = await uploadRes.json();
+  await tar.c({ gzip: true, C: "dist", file: "_build.tar.gz" }, ["."]);
+  step.text = `Uploading Build ${buildEnv.BUILD_VERSION}.`;
+  const req = request({ url, method: "put", body: readFileSync("_build.tar.gz") }); // Tried and failed to get this to use a stream :P
+  unlinkSync("_build.tar.gz");
+  await new Promise(res => req.on("end", res));
+  step.text = "Build uploaded, deploying.";
+  await fetch(`https://${host}/api/ita/deploy/hubs`, { headers, method: "POST", body: { version } });
+  step.text = "Deploy finished.";
+  step.stop();
+  spinner.text = `Deployed to ${host}`;
+  spinner.succeed();
   process.exit(0);
 })();
