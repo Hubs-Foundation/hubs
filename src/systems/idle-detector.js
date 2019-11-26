5c075374b914a82e77cbe5fb57d6c930711f46c4
Brian Peiris <brianpeiris@gmail.com>
2019-11-26 11:37:33 -0800

    Refactor activity paths

--- src/systems/idle-detector.js
+++ src/systems/idle-detector.js
@@ -3,6 +3,20 @@ const qs = new URLSearchParams(location.search);
 const IDLE_TIMEOUT_MS = (parseInt(qs.get("idle_timeout"), 10) || 2 * 60 * 60) * 1000;
 const INPUT_CHECK_INTERVAL_MS = 1000;
 
+const CHARACTER_ACCELERATION_PATH = paths.actions.characterAcceleration;
+const BASIC_ACTIVITY_PATHS = [
+  paths.actions.startGazeTeleport,
+  paths.actions.rightHand.startTeleport,
+  paths.actions.leftHand.startTeleport,
+  paths.actions.snapRotateRight,
+  paths.actions.snapRotateLeft,
+  paths.actions.cursor.right.grab,
+  paths.actions.cursor.left.grab,
+  paths.actions.rightHand.grab,
+  paths.actions.leftHand.grab,
+  paths.actions.angularVelocity
+];
+
 AFRAME.registerSystem("idle-detector", {
   init() {
     this.resetTimeout = this.resetTimeout.bind(this);
@@ -29,21 +43,18 @@ AFRAME.registerSystem("idle-detector", {
     if (time - this.lastInputCheck < INPUT_CHECK_INTERVAL_MS) return;
 
     const userinput = this.el.systems.userinput;
-    const characterAcceleration = userinput.get(paths.actions.characterAcceleration);
+
+    let basicActivity = false;
+    for (const activityPath of BASIC_ACTIVITY_PATHS) {
+      basicActivity = basicActivity || !!userinput.get(activityPath);
+    }
+
+    const characterAcceleration = userinput.get(CHARACTER_ACCELERATION_PATH);
 
     const active =
-      userinput.get(paths.actions.startGazeTeleport) ||
-      userinput.get(paths.actions.rightHand.startTeleport) ||
-      userinput.get(paths.actions.leftHand.startTeleport) ||
-      userinput.get(paths.actions.snapRotateRight) ||
-      userinput.get(paths.actions.snapRotateLeft) ||
-      userinput.get(paths.actions.cursor.right.grab) ||
-      userinput.get(paths.actions.cursor.left.grab) ||
-      userinput.get(paths.actions.rightHand.grab) ||
-      userinput.get(paths.actions.leftHand.grab) ||
+      basicActivity ||
       !!(characterAcceleration && characterAcceleration[0]) ||
-      !!(characterAcceleration && characterAcceleration[1]) ||
-      !!userinput.get(paths.actions.angularVelocity);
+      !!(characterAcceleration && characterAcceleration[1]);
 
     if (active) {
       this.resetTimeout();
