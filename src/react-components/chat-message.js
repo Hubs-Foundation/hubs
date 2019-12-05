b73cfffedef6103f0a07756b0b66ac3f21f77433
netpro2k <netpro2k@gmail.com>
2019-12-05 09:42:31 -0800

    Textures need to keep their shape for grabbing

--- src/react-components/chat-message.js
+++ src/react-components/chat-message.js
@@ -9,7 +9,6 @@ import serializeElement from "../utils/serialize-element";
 import { navigateToClientInfo } from "./presence-list";
 
 const messageCanvas = document.createElement("canvas");
-messageCanvas.width = messageCanvas.height = 1024;
 
 const emojiRegex = /(?:[\u2700-\u27bf]|(?:\ud83c[\udde6-\uddff]){2}|[\ud800-\udbff][\udc00-\udfff]|[\u0023-\u0039]\ufe0f?\u20e3|\u3299|\u3297|\u303d|\u3030|\u24c2|\ud83c[\udd70-\udd71]|\ud83c[\udd7e-\udd7f]|\ud83c\udd8e|\ud83c[\udd91-\udd9a]|\ud83c[\udde6-\uddff]|[\ud83c[\ude01-\ude02]|\ud83c\ude1a|\ud83c\ude2f|[\ud83c[\ude32-\ude3a]|[\ud83c[\ude50-\ude51]|\u203c|\u2049|[\u25aa-\u25ab]|\u25b6|\u25c0|[\u25fb-\u25fe]|\u00a9|\u00ae|\u2122|\u2139|\ud83c\udc04|[\u2600-\u26FF]|\u2b05|\u2b06|\u2b07|\u2b1b|\u2b1c|\u2b50|\u2b55|\u231a|\u231b|\u2328|\u23cf|[\u23e9-\u23f3]|[\u23f8-\u23fa]|\ud83c\udccf|\u2934|\u2935|[\u2190-\u21ff])/;
 const urlRegex = /^https?:\/\/(www\.)?[-a-zA-Z0-9@:%._+~#=]{2,256}\.[a-z]{2,6}\b([-a-zA-Z0-9@:%_+.~#?&//=]*)$/;
@@ -112,8 +111,12 @@ function renderChatMessage(body, from, allowEmojiRender, lowResolution) {
 
   return new Promise(resolve => {
     ReactDOM.render(entryDom, el, () => {
+      const textureSize = 1024;
+
       const ratio = el.offsetHeight / el.offsetWidth;
-      const scale = (messageCanvas.height * Math.min(1.0, ratio)) / el.offsetHeight;
+      const scale = (textureSize * Math.min(1.0, 1.0 / ratio)) / el.offsetWidth;
+      messageCanvas.width = el.offsetWidth * scale;
+      messageCanvas.height = el.offsetHeight * scale;
 
       const xhtml = encodeURIComponent(`
           <svg xmlns="http://www.w3.org/2000/svg" width="${messageCanvas.width}px" height="${messageCanvas.height}px">
@@ -125,7 +128,6 @@ function renderChatMessage(body, from, allowEmojiRender, lowResolution) {
       const img = new Image();
 
       img.onload = () => {
-        context.clearRect(0, 0, messageCanvas.width, messageCanvas.height);
         context.drawImage(img, 0, 0);
         ReactDOM.unmountComponentAtNode(el);
         el.remove();
