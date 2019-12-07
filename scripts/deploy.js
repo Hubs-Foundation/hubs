3976f12fa6881709dfe753e72315467fc1aef702
Greg Fodor <gfodor@gmail.com>
2019-12-07 01:14:40 +0000

    New deploy method

--- scripts/deploy.js
+++ scripts/deploy.js
@@ -1,10 +1,11 @@
-import { readFileSync, existsSync, unlinkSync } from "fs";
+import { createReadStream, readFileSync, existsSync, unlinkSync } from "fs";
 import { exec } from "child_process";
 import rmdir from "rimraf";
 import ncp from "ncp";
 import tar from "tar";
 import request from "request";
 import ora from "ora";
+import FormData from "form-data";
 
 if (!existsSync(".ret.credentials")) {
   console.log("Not logged in, so cannot deploy. To log in, run npm run login.");
@@ -38,97 +39,109 @@ const getTs = (() => {
     buildEnv[k.toUpperCase()] = v;
   }
 
-  buildEnv.BUILD_VERSION = `1.0.0.${getTs()}`;
+  const version = getTs();
+
+  buildEnv.BUILD_VERSION = `1.0.0.${version}`;
   buildEnv.ITA_SERVER = "";
   buildEnv.POSTGREST_SERVER = "";
 
-  const env = Object.assign(process.env, buildEnv);
+  //const env = Object.assign(process.env, buildEnv);
 
-  for (const d in ["./dist", "./admin/dist"]) {
-    rmdir(d, err => {
-      if (err) {
-        console.error(err);
-        process.exit(1);
-      }
-    });
-  }
+  //for (const d in ["./dist", "./admin/dist"]) {
+  //  rmdir(d, err => {
+  //    if (err) {
+  //      console.error(err);
+  //      process.exit(1);
+  //    }
+  //  });
+  //}
 
-  step.text = "Building Client.";
+  //step.text = "Building Client.";
 
-  await new Promise((resolve, reject) => {
-    exec("npm ci", {}, err => {
-      if (err) reject(err);
-      resolve();
-    });
-  });
+  //await new Promise((resolve, reject) => {
+  //  exec("npm ci", {}, err => {
+  //    if (err) reject(err);
+  //    resolve();
+  //  });
+  //});
 
-  await new Promise((resolve, reject) => {
-    exec("npm run build", { env }, err => {
-      if (err) reject(err);
-      resolve();
-    });
-  });
+  //await new Promise((resolve, reject) => {
+  //  exec("npm run build", { env }, err => {
+  //    if (err) reject(err);
+  //    resolve();
+  //  });
+  //});
 
-  step.text = "Building Admin Console.";
+  //step.text = "Building Admin Console.";
 
-  await new Promise((resolve, reject) => {
-    exec("npm ci", { cwd: "./admin" }, err => {
-      if (err) reject(err);
-      resolve();
-    });
-  });
+  //await new Promise((resolve, reject) => {
+  //  exec("npm ci", { cwd: "./admin" }, err => {
+  //    if (err) reject(err);
+  //    resolve();
+  //  });
+  //});
 
-  await new Promise((resolve, reject) => {
-    exec("npm run build", { cwd: "./admin", env }, err => {
-      if (err) reject(err);
-      resolve();
-    });
-  });
+  //await new Promise((resolve, reject) => {
+  //  exec("npm run build", { cwd: "./admin", env }, err => {
+  //    if (err) reject(err);
+  //    resolve();
+  //  });
+  //});
 
-  await new Promise(res => {
-    ncp("./admin/dist", "./dist", err => {
-      if (err) {
-        console.error(err);
-        process.exit(1);
-      }
+  //await new Promise(res => {
+  //  ncp("./admin/dist", "./dist", err => {
+  //    if (err) {
+  //      console.error(err);
+  //      process.exit(1);
+  //    }
 
-      res();
-    });
-  });
+  //    res();
+  //  });
+  //});
   step.text = "Preparing Deploy.";
-  const uploadRes = await fetch(`https://${host}/api/ita/deploy/hubs/upload_url`, { headers });
-  const { url, version } = await uploadRes.json();
 
   step.text = "Packaging Build.";
   await tar.c({ gzip: true, C: "dist", file: "_build.tar.gz" }, ["."]);
   step.text = `Uploading Build ${buildEnv.BUILD_VERSION}.`;
+
+  let uploadedUrl;
 
   const runUpload = async attempt => {
     if (attempt > 3) {
       throw new Error("Upload failed.");
     }
 
-    const req = request({ url, method: "put", body: readFileSync("_build.tar.gz") }); // Tried and failed to get this to use a stream :P
-    await new Promise(res => {
-      req.on("error", async () => {
-        step.text = `Upload failed. Retrying attempt #${attempt + 1}/3`;
-        await runUpload(attempt + 1);
-        res();
-      });
+    const formData = new FormData();
+    formData.append("media", createReadStream("_build.tar.gz"));
+    formData.append("promotion_mode", "with_token");
 
-      req.on("end", res);
-    });
+    try {
+      const res = await fetch(`https://${host}/api/v1/media`, { method: "POST", body: formData });
+      const payload = await res.json();
+      const url = new URL(payload.origin);
+      url.searchParams.set("token", payload.meta.access_token);
+      uploadedUrl = url.toString();
+    } catch (e) {
+      step.text = `Upload failed. Retrying attempt #${attempt + 1}/3`;
+      await runUpload(attempt + 1);
+    }
   };
 
   await runUpload(0);
   unlinkSync("_build.tar.gz");
 
   step.text = "Build uploaded, deploying.";
+  console.log(uploadedUrl);
+  console.log(version);
 
   // Wait for S3 flush, kind of a hack.
   await new Promise(res => setTimeout(res, 5000));
 
-  await fetch(`https://${host}/api/ita/deploy/hubs`, { headers, method: "POST", body: JSON.stringify({ version }) });
+  await fetch(`https://${host}/api/ita/deploy/hubs`, {
+    headers,
+    method: "POST",
+    body: JSON.stringify({ url: uploadedUrl, version })
+  });
 
   step.text = `Deployed to ${host}.`;
   step.succeed();
