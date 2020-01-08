1c6cd4bf88baaedba408fcd837ae1841ce539911
Greg Fodor <gfodor@gmail.com>
2020-01-08 21:39:23 +0000

    Fix issues with scene landing page not loading scenes

--- src/systems/lobby-camera-system.js
+++ src/systems/lobby-camera-system.js
@@ -3,7 +3,8 @@ import { CAMERA_MODE_INSPECT } from "./camera-system.js";
 export class LobbyCameraSystem {
   tick() {
     const el = document.querySelector("[scene-preview-camera]");
-    if (el && AFRAME.scenes[0].systems["hubs-systems"].cameraSystem.mode !== CAMERA_MODE_INSPECT) {
+    const hubsSystems = AFRAME.scenes[0].systems["hubs-systems"];
+    if (el && (!hubsSystems || hubsSystems.cameraSystem.mode !== CAMERA_MODE_INSPECT)) {
       el.components["scene-preview-camera"].tick2();
     }
   }
