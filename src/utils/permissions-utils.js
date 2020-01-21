19ec5e669cc9313256fbf368ef08b2e040d1a447
Greg Fodor <gfodor@gmail.com>
2020-01-21 03:00:52 +0000

    Add unprivileged freeze menu

--- src/utils/permissions-utils.js
+++ src/utils/permissions-utils.js
@@ -14,11 +14,13 @@ export function canMove(entity) {
   const networkedTemplate = entity && entity.components.networked && entity.components.networked.data.template;
   const isCamera = networkedTemplate === "#interactable-camera";
   const isPen = networkedTemplate === "#interactable-pen";
+  const isHoldableButton = entity.components.tags && entity.components.tags.data.holdableButton;
   return (
-    window.APP.hubChannel.can("spawn_and_move_media") &&
-    (!isPinned || window.APP.hubChannel.can("pin_objects")) &&
-    (!isCamera || window.APP.hubChannel.can("spawn_camera")) &&
-    (!isPen || window.APP.hubChannel.can("spawn_drawing"))
+    isHoldableButton ||
+    (window.APP.hubChannel.can("spawn_and_move_media") &&
+      (!isPinned || window.APP.hubChannel.can("pin_objects")) &&
+      (!isCamera || window.APP.hubChannel.can("spawn_camera")) &&
+      (!isPen || window.APP.hubChannel.can("spawn_drawing")))
   );
 }
 
