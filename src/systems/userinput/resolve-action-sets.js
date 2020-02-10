2ad9add3660102256b07e7f1f075de15a313ad22
John Shaughnessy <johnfshaughnessy@gmail.com>
2020-02-10 14:51:38 -0800

    Fix mixup between hands and remote constraint checks

--- src/systems/userinput/resolve-action-sets.js
+++ src/systems/userinput/resolve-action-sets.js
@@ -102,18 +102,16 @@ export function resolveActionSets() {
   userinput.toggleSet(
     sets.leftHandHoveringOnInteractable,
     !leftHand.held &&
-      (leftHand.hovered &&
-        ((leftHand.hovered.components.tags && leftHand.hovered.components.tags.data.offersRemoteConstraint) ||
-          (leftHand.hovered.components.tags && leftHand.hovered.components.tags.data.togglesHoveredActionSet) ||
-          leftHand.hovered.components["super-spawner"]))
+      leftHand.hovered &&
+      ((leftHand.hovered.components.tags && leftHand.hovered.components.tags.data.offersHandConstraint) ||
+        leftHand.hovered.components["super-spawner"])
   );
   userinput.toggleSet(
     sets.rightHandHoveringOnInteractable,
     !rightHand.held &&
-      (rightHand.hovered &&
-        ((rightHand.hovered.components.tags && rightHand.hovered.components.tags.data.offersRemoteConstraint) ||
-          (rightHand.hovered.components.tags && rightHand.hovered.components.tags.data.togglesHoveredActionSet) ||
-          rightHand.hovered.components["super-spawner"]))
+      rightHand.hovered &&
+      ((rightHand.hovered.components.tags && rightHand.hovered.components.tags.data.offersHandConstraint) ||
+        rightHand.hovered.components["super-spawner"])
   );
   userinput.toggleSet(
     sets.leftCursorHoveringOnInteractable,
@@ -122,6 +120,7 @@ export function resolveActionSets() {
       !leftRemote.held &&
       leftRemote.hovered &&
       ((leftRemote.hovered.components.tags && leftRemote.hovered.components.tags.data.offersRemoteConstraint) ||
+        (leftRemote.hovered.components.tags && leftRemote.hovered.components.tags.data.togglesHoveredActionSet) ||
         leftRemote.hovered.components["super-spawner"])
   );
   userinput.toggleSet(
@@ -131,6 +130,7 @@ export function resolveActionSets() {
       !rightRemote.held &&
       rightRemote.hovered &&
       ((rightRemote.hovered.components.tags && rightRemote.hovered.components.tags.data.offersRemoteConstraint) ||
+        (rightRemote.hovered.components.tags && rightRemote.hovered.components.tags.data.togglesHoveredActionSet) ||
         rightRemote.hovered.components["super-spawner"])
   );
 
