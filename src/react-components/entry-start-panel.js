b9ad6986ae36a37484def1137220fce55832e2f1
netpro2k <netpro2k@gmail.com>
2020-03-03 03:23:49 -0800

    Delint

--- src/react-components/entry-start-panel.js
+++ src/react-components/entry-start-panel.js
@@ -1,11 +1,14 @@
-import React, { Component } from "react";
+import { Component } from "react";
 import PropTypes from "prop-types";
-import classNames from "classnames";
-import LobbyChatBox from "./lobby-chat-box.js";
 
 // this component is currently just being used for its componentDidMount
 // TODO migrate render from ui-root.js renderEntryStartPanel()
 export default class EntryStartPanel extends Component {
+  static propTypes = {
+    entering: PropTypes.bool,
+    onEnteringCanceled: PropTypes.func
+  };
+
   componentDidMount() {
     if (this.props.entering) {
       this.props.onEnteringCanceled();
