b898a1294118a776fe214ddca59d6d607448ff3b
Greg Fodor <gfodor@gmail.com>
2020-03-20 23:29:59 +0000

    Fix use hubs cloud script

--- scripts/use-hubs-cloud-stack.sh
+++ scripts/use-hubs-cloud-stack.sh
@@ -4,9 +4,10 @@ STACK_NAME=$1
 EXTERNAL_ZONE_NAME=$2
 SHORTLINK_ZONE_NAME=$3
 INTERNAL_ZONE_NAME=$4
+CORS_PROXY_SERVER=$5
 
 if [[ -z "$STACK_NAME" || -z "$EXTERNAL_ZONE_NAME" || -z "$SHORTLINK_ZONE_NAME" || -z "$INTERNAL_ZONE_NAME" ]] ; then 
-  echo -e "Usage: scripts/use-hubs-cloud-stack.sh <stack-name> <domain> <short-link-domain> <internal-domain>
+  echo -e "Usage: scripts/use-hubs-cloud-stack.sh <stack-name> <domain> <short-link-domain> <internal-domain> [cors-proxy-worker]
 
 Switches your local client to connect to a remote Hubs Cloud instance by modifying .env.defaults.
 
@@ -26,13 +27,21 @@ To switch back to the Mozilla dev servers, run scripts/use-mozilla-dev.sh"
   exit 1
 fi
 
+if [ -e $HOST ]; then
+  HOST="hubs.local"
+fi
+
+if [ -e $CORS_PROXY_SERVER ]; then
+  CORS_PROXY_SERVER="$STACK_NAME-cors-proxy.$INTERNAL_ZONE_NAME"
+fi
+
 cat > .env.defaults << EOF
 SHORTLINK_DOMAIN="$SHORTLINK_ZONE_NAME"
 
 RETICULUM_SERVER="$EXTERNAL_ZONE_NAME"
 
 # CORS proxy.
-CORS_PROXY_SERVER="$STACK_NAME-cors-proxy.$INTERNAL_ZONE_NAME"
+CORS_PROXY_SERVER="$CORS_PROXY_SERVER"
 
 # The thumbnailing backend to connect to.
 # See here for the server code: https://github.com/MozillaReality/farspark or https://github.com/MozillaReality/nearspark
@@ -42,7 +51,7 @@ THUMBNAIL_SERVER="$STACK_NAME-nearspark.$INTERNAL_ZONE_NAME"
 ASSET_BUNDLE_SERVER="https://asset-bundles-prod.reticulum.io"
 
 # Comma-separated list of domains which are known to not need CORS proxying
-NON_CORS_PROXY_DOMAINS="$HOST,$EXTERNAL_ZONE_NAME"
+NON_CORS_PROXY_DOMAINS="$HOST,$EXTERNAL_ZONE_NAME,$STACK_NAME-assets.$INTERNAL_ZONE_NAME"
 
 # The root URL under which Hubs expects static assets to be served.
 BASE_ASSETS_PATH=/
