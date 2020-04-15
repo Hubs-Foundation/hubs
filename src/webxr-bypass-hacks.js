3f9ae25a69c8d86de6f9dfbaab39ab5143ffb49b
Brian Peiris <brianpeiris@gmail.com>
2020-04-15 16:06:08 -0700

    Fix chrome webxr detection

--- src/webxr-bypass-hacks.js
+++ src/webxr-bypass-hacks.js
@@ -7,7 +7,12 @@ window.forceWebVR = true;
 HACK Fix a-frame's device detection in Chrome when WebXR or WebVR flags are enabled in
 chrome://flags. See https://github.com/mozilla/hubs/issues/892
 */
-if (!/mobile vr/i.test(navigator.userAgent) && navigator.xr && !navigator.xr.requestDevice) {
+if (
+  !/mobile vr/i.test(navigator.userAgent) &&
+  /Chrome/.test(navigator.userAgent) &&
+  navigator.xr &&
+  !navigator.xr.requestDevice
+) {
   navigator.xr.requestDevice = () => Promise.reject({ message: "Hubs: requestDevice not supported." });
 }
 /*
