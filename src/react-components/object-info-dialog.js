165ae5a4801b2578cc65cbae10d5702768090b1d
John Shaughnessy <johnfshaughnessy@gmail.com>
2020-04-17 16:34:20 -0700

    Add object info modal for mobile screens

--- src/react-components/object-info-dialog.js
+++ src/react-components/object-info-dialog.js
@@ -1,8 +1,11 @@
 import React, { Component } from "react";
 import { rotateInPlaceAroundWorldUp, affixToWorldUp } from "../utils/three-utils";
+import classNames from "classnames";
 import PropTypes from "prop-types";
 import DialogContainer from "./dialog-container.js";
 import styles from "../assets/stylesheets/client-info-dialog.scss";
+import rootStyles from "../assets/stylesheets/ui-root.scss";
+import objectInfoDialogStyles from "../assets/stylesheets/object-info-dialog.scss";
 import { FormattedMessage } from "react-intl";
 import { faTimes } from "@fortawesome/free-solid-svg-icons/faTimes";
 import { faChevronLeft } from "@fortawesome/free-solid-svg-icons/faChevronLeft";
@@ -10,7 +13,45 @@ import { faChevronRight } from "@fortawesome/free-solid-svg-icons/faChevronRight
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import entryStyles from "../assets/stylesheets/entry.scss";
 import { mediaSort } from "../utils/media-sorting.js";
+import circle from "../assets/images/circle.png";
+import smallCircle from "../assets/images/small-circle.png";
+import chevronLeft from "../assets/images/chevron-left.png";
+import chevronRight from "../assets/images/chevron-right.png";
+import remove from "../assets/images/remove.png";
+import unpin from "../assets/images/unpin.png";
+import pin from "../assets/images/pin.png";
+import goTo from "../assets/images/goto.png";
 
+function actionBarItem(ariaLabel, onClickHandler, backgroundImageSrc, foregroundImageSrc, messageId) {
+  return (
+    <div className={objectInfoDialogStyles.actionBarItem}>
+      <button
+        className={classNames({
+          [objectInfoDialogStyles.noDefaultButtonStyle]: true,
+          [objectInfoDialogStyles.flex]: true
+        })}
+        aria-label={ariaLabel}
+        onClick={onClickHandler}
+      >
+        <div className={objectInfoDialogStyles.imageStack}>
+          {backgroundImageSrc ? <img className={objectInfoDialogStyles.stackedImage} src={backgroundImageSrc} /> : null}
+          {foregroundImageSrc ? <img className={objectInfoDialogStyles.stackedImage} src={foregroundImageSrc} /> : null}
+        </div>
+      </button>
+      {messageId && (
+        <div className={objectInfoDialogStyles.subtitle}>
+          <FormattedMessage id={messageId} />
+        </div>
+      )}
+    </div>
+  );
+}
+
+function actionBarItemPlaceholder(showCircle) {
+  return actionBarItem("", () => {}, showCircle ? circle : null);
+}
+
+let uiRoot;
 export default class ObjectInfoDialog extends Component {
   static propTypes = {
     scene: PropTypes.object,
@@ -145,8 +186,64 @@ export default class ObjectInfoDialog extends Component {
   render() {
     const { pinned, onClose } = this.props;
     const isStatic = this.props.el.components.tags && this.props.el.components.tags.data.isStatic;
+    const showNavigationButtons = this.state.mediaEntities.length > 1;
+    uiRoot = uiRoot || document.getElementById("ui-root");
+    const isGhost = uiRoot && uiRoot.firstChild && uiRoot.firstChild.classList.contains("isGhost");
+    const showGoToButton = this.props.scene.is("entered") || isGhost;
+    const showPinOrUnpin =
+      this.props.scene.is("entered") && !isStatic && this.props.hubChannel && this.props.hubChannel.can("pin_objects");
+    const showPinButton = showPinOrUnpin && !pinned;
+    const showUnpinButton = showPinOrUnpin && pinned;
+    const showRemoveButton =
+      this.props.scene.is("entered") &&
+      !pinned &&
+      !isStatic &&
+      this.props.hubChannel &&
+      this.props.hubChannel.can("spawn_and_move_media");
 
-    return (
+    return window.innerWidth < 450 ? (
+      <div className={rootStyles.uiDialog}>
+        <div className={classNames({ [rootStyles.uiDialogBoxContents]: true, [rootStyles.uiInteractive]: true })}>
+          <div className={objectInfoDialogStyles.topBar}>
+            <button
+              aria-label={`Close object info panel`}
+              autoFocus
+              className={classNames({
+                [objectInfoDialogStyles.collapseButton]: true,
+                [objectInfoDialogStyles.noDefaultButtonStyle]: true
+              })}
+              onClick={onClose}
+            >
+              <i>
+                <FontAwesomeIcon icon={faTimes} />
+              </i>
+            </button>
+            <div className={objectInfoDialogStyles.openLink}>
+              <a className={rootStyles.linkText} href={this.props.src} target="_blank" rel="noopener noreferrer">
+                <FormattedMessage id={`object-info.open-link`} />
+              </a>
+            </div>
+          </div>
+          <div className={objectInfoDialogStyles.actionBar}>
+            {showNavigationButtons
+              ? actionBarItem("Previous Object", this.navigatePrev, smallCircle, chevronLeft)
+              : actionBarItemPlaceholder(false)}
+            {showGoToButton
+              ? actionBarItem("Go To", this.enqueueWaypointTravel, circle, goTo, "object-info.waypoint")
+              : actionBarItemPlaceholder(false)}
+            {showPinButton ? actionBarItem("Pin", this.pin, circle, pin, "object-info.pin-button") : null}
+            {showUnpinButton ? actionBarItem("Unpin", this.unpin, circle, unpin, "object-info.unpin-button") : null}
+            {!showPinButton && !showUnpinButton ? actionBarItemPlaceholder(false) : null}
+            {showRemoveButton
+              ? actionBarItem("Remove", this.remove.bind(this), circle, remove, "object-info.remove-button")
+              : actionBarItemPlaceholder(false)}
+            {showNavigationButtons
+              ? actionBarItem("Next Object", this.navigateNext, smallCircle, chevronRight)
+              : actionBarItemPlaceholder(false)}
+          </div>
+        </div>
+      </div>
+    ) : (
       <DialogContainer noOverlay={true} wide={true} {...this.props}>
         <div className={styles.roomInfo}>
           <div className={styles.titleAndClose}>
@@ -166,7 +263,7 @@ export default class ObjectInfoDialog extends Component {
           </div>
           <div className={styles.actionButtonSections}>
             <div className={styles.leftActionButtons}>
-              {this.state.mediaEntities.length > 1 && (
+              {showNavigationButtons && (
                 <button aria-label="Previous Object" className={styles.navigationButton} onClick={this.navigatePrev}>
                   <i>
                     <FontAwesomeIcon icon={faChevronLeft} />
@@ -183,31 +280,24 @@ export default class ObjectInfoDialog extends Component {
                   <FormattedMessage id="object-info.waypoint" />
                 </button>
               )}
-              {this.props.scene.is("entered") &&
-              !pinned &&
-              !isStatic &&
-              this.props.hubChannel &&
-              this.props.hubChannel.can("spawn_and_move_media") ? (
+              {showRemoveButton ? (
                 <button onClick={this.remove.bind(this)}>
                   <FormattedMessage id="object-info.remove-button" />
                 </button>
               ) : (
                 <div className={styles.actionButtonPlaceholder} />
               )}
-              {this.props.scene.is("entered") &&
-                !isStatic &&
-                this.props.hubChannel &&
-                this.props.hubChannel.can("pin_objects") && (
-                  <button className={pinned ? "" : styles.primaryActionButton} onClick={pinned ? this.unpin : this.pin}>
-                    <FormattedMessage id={`object-info.${pinned ? "unpin-button" : "pin-button"}`} />
-                  </button>
-                )}
+              {showPinOrUnpin && (
+                <button className={pinned ? "" : styles.primaryActionButton} onClick={pinned ? this.unpin : this.pin}>
+                  <FormattedMessage id={`object-info.${pinned ? "unpin-button" : "pin-button"}`} />
+                </button>
+              )}
               <a className={styles.cancelText} href="#" onClick={onClose}>
                 <FormattedMessage id="client-info.cancel" />
               </a>
             </div>
             <div className={styles.rightActionButtons}>
-              {this.state.mediaEntities.length > 1 && (
+              {showNavigationButtons && (
                 <button aria-label="Next Object" className={styles.navigationButton} onClick={this.navigateNext}>
                   <i>
                     <FontAwesomeIcon icon={faChevronRight} />
