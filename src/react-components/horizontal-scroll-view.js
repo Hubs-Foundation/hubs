f2e2a871edbab159c5fe36a300248a9af80022fb
John Shaughnessy <johnfshaughnessy@gmail.com>
2020-05-08 18:21:06 -0700

    Cleanup a bit

--- src/react-components/horizontal-scroll-view.js
+++ src/react-components/horizontal-scroll-view.js
@@ -5,6 +5,10 @@ import oStyles from "../assets/stylesheets/object-info-dialog.scss";
 import { mediaSortOrder, DISPLAY_IMAGE } from "../utils/media-sorting";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 
+function getWidth(ref) {
+  return (ref && ref.current && parseInt(window.getComputedStyle(ref.current).width)) || 0;
+}
+
 function clamp(x, min, max) {
   return Math.min(Math.max(x, min), max);
 }
@@ -36,38 +40,42 @@ function lerp(start, end, t) {
   return start + (end - start) * t;
 }
 
-function buttonIndexAtTouchX(navAreaRef, numItems, touchX, currentLeftOffset, itemWidth) {
+function itemIndexAtXWithScroll(x, numItems, itemWidth, scrollviewWidth, currentLeftOffset) {
   const widthOfAllItems = itemWidth * numItems;
-  const scrollviewWidth =
-    (navAreaRef && navAreaRef.current && parseInt(window.getComputedStyle(navAreaRef.current).width)) ||
-    window.innerWidth - 120;
-  if (!navAreaRef || !navAreaRef.current) {
-    console.warn("No navAreaRef!");
-  }
   const effectiveWidth = Math.min(scrollviewWidth, widthOfAllItems);
-
-  const CENTER_PIXEL = window.innerWidth / 2;
-  const FIRST_PIXEL = CENTER_PIXEL - effectiveWidth / 2;
-  const ICONS_ON_SCREEN = effectiveWidth / itemWidth;
-
+  const windowCenter = window.innerWidth / 2;
+  const firstPixel = windowCenter - effectiveWidth / 2;
   const halfItemWidth = itemWidth / 2;
-  const LEFT_OFFSET_FOR_ZEROTH_ITEM = scrollviewWidth / 2 - halfItemWidth;
-  const LEFT_OFFSET_FOR_LAST_ITEM = LEFT_OFFSET_FOR_ZEROTH_ITEM - itemWidth * (numItems - 1);
-  const LEFT_OFFSET_RANGE = LEFT_OFFSET_FOR_LAST_ITEM - LEFT_OFFSET_FOR_ZEROTH_ITEM;
-  const leftOffsetZeroToOne = clamp((currentLeftOffset - LEFT_OFFSET_FOR_ZEROTH_ITEM) / LEFT_OFFSET_RANGE, 0, 1);
-  const CENTER_ICON_UNFLOORED = leftOffsetZeroToOne * (numItems - 1);
-  const FIRST_ICON_UNFLOORED = CENTER_ICON_UNFLOORED - ICONS_ON_SCREEN / 2;
-  const LAST_ICON_UNFLOORED = CENTER_ICON_UNFLOORED + ICONS_ON_SCREEN / 2;
-
-  const touchXInNavItemList = touchX - FIRST_PIXEL;
-  const touchZeroToOne = clamp((touchX - FIRST_PIXEL) / effectiveWidth, 0, 1);
-
-  const approximateIndex =
-    widthOfAllItems <= scrollviewWidth
-      ? lerp(0, numItems, touchXInNavItemList / widthOfAllItems)
-      : lerp(FIRST_ICON_UNFLOORED, LAST_ICON_UNFLOORED + 1, touchZeroToOne);
-
+  const leftOffsetForZerothItem = scrollviewWidth / 2 - halfItemWidth;
+  const leftOffsetForLastItem = leftOffsetForZerothItem - itemWidth * (numItems - 1);
+  const leftOffsetRange = leftOffsetForLastItem - leftOffsetForZerothItem;
+  const leftOffsetZeroToOne = clamp((currentLeftOffset - leftOffsetForZerothItem) / leftOffsetRange, 0, 1);
+  const centerIconUnfloored = leftOffsetZeroToOne * (numItems - 1);
+  const numIconsOnScreen = effectiveWidth / itemWidth;
+  const firstIconUnfloored = centerIconUnfloored - numIconsOnScreen / 2;
+  const lastIconUnfloored = centerIconUnfloored + numIconsOnScreen / 2;
+  const touchZeroToOne = clamp((x - firstPixel) / effectiveWidth, 0, 1);
+  const approximateIndex = lerp(firstIconUnfloored, lastIconUnfloored + 1, touchZeroToOne);
   return clampToIndex(approximateIndex, numItems);
+}
+
+function itemIndexAtXNoScroll(x, numItems, itemWidth) {
+  const widthOfAllItems = numItems * itemWidth;
+  const windowCenter = window.innerWidth / 2;
+  const firstPixel = windowCenter - widthOfAllItems / 2;
+  const xInNavItemList = x - firstPixel;
+  const approximateIndex = lerp(0, numItems, xInNavItemList / widthOfAllItems);
+  return clampToIndex(approximateIndex, numItems);
+}
+
+function itemIndexAtX(numItems, touchX, currentLeftOffset, itemWidth, scrollviewWidth) {
+  const widthOfAllItems = numItems * itemWidth;
+  const contentWillScroll = widthOfAllItems > scrollviewWidth;
+  if (contentWillScroll) {
+    return itemIndexAtXWithScroll(touchX, numItems, itemWidth, scrollviewWidth, currentLeftOffset);
+  } else {
+    return itemIndexAtXNoScroll(touchX, numItems, itemWidth);
+  }
 }
 
 export class HorizontalScrollView extends Component {
@@ -89,7 +97,8 @@ export class HorizontalScrollView extends Component {
     isHandlingTouchInteraction: false
   };
 
-  componentDidMount() {
+  constructor(props) {
+    super(props);
     this.navAreaRef = React.createRef();
   }
 
@@ -97,30 +106,22 @@ export class HorizontalScrollView extends Component {
     const { itemWidth, selectedEl, onWheel, mediaEntities, targetIndex, onItemSelected } = this.props;
     const halfItemWidth = itemWidth / 2;
     const numItems = mediaEntities.length;
-    const scrollviewWidth =
-      (this.navAreaRef &&
-        this.navAreaRef.current &&
-        parseInt(window.getComputedStyle(this.navAreaRef.current).width)) ||
-      window.innerWidth - 120;
-    if (!this.navAreaRef || !this.navAreaRef.current) {
-      console.warn("no nav area ref yet");
-    }
     const widthOfAllItems = itemWidth * numItems;
-    const DISTANCE_TO_CENTER = -1 * halfItemWidth + scrollviewWidth / 2;
-    const willScrollContent = widthOfAllItems > scrollviewWidth;
-    const UNLOCKED_LEFT_OFFSET = this.state.currentLeftOffset;
-    const LOCKED_LEFT_OFFSET = DISTANCE_TO_CENTER - itemWidth * targetIndex;
-    const DRAG_WIDTH_PX = halfItemWidth;
+    const scrollviewWidth = getWidth(this.navAreaRef);
+    const contentWillScroll = widthOfAllItems > scrollviewWidth;
+    const offsetToCenter = -1 * halfItemWidth + scrollviewWidth / 2;
+    const lockedLeftOffset = offsetToCenter - itemWidth * targetIndex;
 
     return (
       <div
         ref={this.navAreaRef}
         className={oStyles.innerNavigationRowContainer}
-        style={{ justifyContent: willScrollContent ? "unset" : "center" }}
+        style={{ justifyContent: contentWillScroll ? "unset" : "center" }}
         onWheel={onWheel}
         onTouchStart={e => {
           const touchX = e.touches.item(0).clientX;
           const currentLeftOffset = parseFloat(window.getComputedStyle(e.currentTarget.children[0]).left);
+
           this.setState({
             isDragging: false,
             touchX,
@@ -129,20 +130,20 @@ export class HorizontalScrollView extends Component {
             initialLeftOffset: currentLeftOffset,
             isHandlingTouchInteraction: true
           });
-          if (!willScrollContent) {
-            onItemSelected(buttonIndexAtTouchX(this.navAreaRef, numItems, touchX, currentLeftOffset, itemWidth));
+          if (!contentWillScroll) {
+            const itemIndex = itemIndexAtX(numItems, touchX, currentLeftOffset, itemWidth, scrollviewWidth);
+            onItemSelected(itemIndex);
           }
         }}
         onTouchMove={e => {
           const touchX = e.touches.item(0).clientX;
-          if (!willScrollContent) {
-            onItemSelected(
-              buttonIndexAtTouchX(this.navAreaRef, numItems, touchX, this.state.currentLeftOffset, itemWidth)
-            );
+          if (!contentWillScroll) {
+            const itemIndex = itemIndexAtX(numItems, touchX, this.state.currentLeftOffset, itemWidth, scrollviewWidth);
+            onItemSelected(itemIndex);
             this.setState({ touchX });
           } else {
             const dX = touchX - this.state.initialTouchX;
-            const isDragging = this.state.isDragging || Math.abs(dX) > DRAG_WIDTH_PX;
+            const isDragging = this.state.isDragging || Math.abs(dX) > halfItemWidth;
             if (isDragging) {
               const currentLeftOffset = this.state.initialLeftOffset + dX;
 
@@ -150,10 +151,9 @@ export class HorizontalScrollView extends Component {
                 currentLeftOffset
               });
 
-              const CENTER_PIXEL = window.innerWidth / 2;
-              onItemSelected(
-                buttonIndexAtTouchX(this.navAreaRef, numItems, CENTER_PIXEL, currentLeftOffset, itemWidth)
-              );
+              const centerPixel = window.innerWidth / 2;
+              const itemIndex = itemIndexAtX(numItems, centerPixel, currentLeftOffset, itemWidth, scrollviewWidth);
+              onItemSelected(itemIndex);
             }
             this.setState({
               isDragging,
@@ -165,22 +165,27 @@ export class HorizontalScrollView extends Component {
           const wasDragging = this.state.isDragging;
           this.setState({ isHandlingTouchInteraction: false, isDragging: false });
           if (!wasDragging) {
-            onItemSelected(
-              buttonIndexAtTouchX(this.navAreaRef, numItems, this.state.touchX, this.state.currentLeftOffset, itemWidth)
+            const itemIndex = itemIndexAtX(
+              numItems,
+              this.state.touchX,
+              this.state.currentLeftOffset,
+              itemWidth,
+              scrollviewWidth
             );
+            onItemSelected(itemIndex);
           }
         }}
       >
         <div
           className={oStyles.innerNavigationRow}
           style={
-            willScrollContent
+            contentWillScroll
               ? this.state.isDragging
                 ? {
-                    left: `${UNLOCKED_LEFT_OFFSET}px`
+                    left: `${this.state.currentLeftOffset}px`
                   }
                 : {
-                    left: `${LOCKED_LEFT_OFFSET}px`,
+                    left: `${lockedLeftOffset}px`,
                     transitionProperty: "left",
                     transitionDuration: "0.25s",
                     transitionTimingFunction: "ease-out"
