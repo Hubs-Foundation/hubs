4cc5e32e08b80a6a528318da30c1476e023acf71
Robert Long <robert@robertlong.me>
2020-05-19 18:46:09 -0700

    Ignore unused variables with leading underscores

--- .eslintrc.js
+++ .eslintrc.js
@@ -18,7 +18,8 @@ module.exports = {
     "no-var": "error",
     "no-throw-literal": "error",
     // Light console usage is useful but remove debug logs before merging to master.
-    "no-console": "off"
+    "no-console": "off",
+    "no-unused-vars": ["error", { varsIgnorePattern: "^_", argsIgnorePattern: "^_", ignoreRestSiblings: true }]
   },
   extends: ["prettier", "plugin:react/recommended", "eslint:recommended"]
 };
