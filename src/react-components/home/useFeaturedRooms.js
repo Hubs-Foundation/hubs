9bf22b0431d6fb772075cef9de1084c1cc897657
Robert Long <robert@robertlong.me>
2020-06-01 16:42:00 -0700

    Fix featuredRooms requests

--- src/react-components/home/useFeaturedRooms.js
+++ src/react-components/home/useFeaturedRooms.js
@@ -6,29 +6,30 @@ export function useFeaturedRooms() {
   const auth = useContext(AuthContext);
   const [featuredRooms, setFeaturedRooms] = useState([]);
 
-  useEffect(() => {
-    const initAsync = async () => {
-      // Fetch favorite + public rooms and merge, sorting by member count
-      const [favoriteRoomsResult, publicRoomsResult] = await Promise.all([
-        auth.isSignedIn
-          ? fetchReticulumAuthenticated(
-              `/api/v1/media/search?source=favorites&type=rooms&user=${auth.credentialsAccountId}`
-            )
-          : Promise.resolve({ entries: [] }),
-        fetchReticulumAuthenticated("/api/v1/media/search?source=rooms&filter=public")
-      ]);
+  useEffect(
+    () => {
+      const initAsync = async () => {
+        // Fetch favorite + public rooms and merge, sorting by member count
+        const [favoriteRoomsResult, publicRoomsResult] = await Promise.all([
+          auth.isSignedIn
+            ? fetchReticulumAuthenticated(`/api/v1/media/search?source=favorites&type=rooms&user=${auth.userId}`)
+            : Promise.resolve({ entries: [] }),
+          fetchReticulumAuthenticated("/api/v1/media/search?source=rooms&filter=public")
+        ]);
 
-      const entries = [...publicRoomsResult.entries, ...favoriteRoomsResult.entries];
-      const ids = entries.map(h => h.id);
-      const featuredRooms = entries
-        .filter((h, i) => ids.lastIndexOf(h.id) === i)
-        .sort((a, b) => b.member_count - a.member_count);
+        const entries = [...publicRoomsResult.entries, ...favoriteRoomsResult.entries];
+        const ids = entries.map(h => h.id);
+        const featuredRooms = entries
+          .filter((h, i) => ids.lastIndexOf(h.id) === i)
+          .sort((a, b) => b.member_count - a.member_count);
 
-      setFeaturedRooms(featuredRooms);
-    };
+        setFeaturedRooms(featuredRooms);
+      };
 
-    initAsync();
-  }, []);
+      initAsync();
+    },
+    [auth.isSignedIn, auth.userId]
+  );
 
   return featuredRooms;
 }
