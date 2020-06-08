75a1df96e77b25f25674cab38b3dab25f08c8747
netpro2k <netpro2k@gmail.com>
2020-06-08 16:55:27 -0700

    Add uv-scroll component to animate UV offsets

--- src/components/scene-components.js
+++ src/components/scene-components.js
@@ -11,6 +11,7 @@ import "./hemisphere-light";
 import "./hide-when-quality";
 import "./layers";
 import "./loop-animation";
+import "./uv-scroll";
 import "./media-loader";
 import "./point-light";
 import "./quack";
