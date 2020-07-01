3696d1f57f4405b52c492667d4ed017de9f04de1
Thomas Ortega <thomasortega@Thomass-MacBook-Pro-2.local>
2020-06-30 17:10:12 -0700

    Fixes lingering highlight
    Fixes mozilla/hubs#1205
    
    When removing the camera, we weren’t updating any state that the in-world-hud.js was observing. Therefore, it was never updating to remove the highlight.
    
    I did a removeState(“camera”) to trigger the redraw, assuming that was the correct methodology to use.

--- src/components/camera-tool.js
+++ src/components/camera-tool.js
@@ -247,6 +247,7 @@ AFRAME.registerComponent("camera-tool", {
   remove() {
     this.cameraSystem.deregister(this.el);
     this.el.sceneEl.emit("camera_removed");
+    this.el.sceneEl.removeState("camera");
     this.stopRecording();
   },
 
