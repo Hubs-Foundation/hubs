42021b0462c7ccbeb8c60bceb655ff4a5c757654
Robert Long <robert@robertlong.me>
2020-07-09 16:45:39 -0700

    Add common Button component

--- src/react-components/home/CreateRoomButton.js
+++ src/react-components/home/CreateRoomButton.js
@@ -1,19 +1,19 @@
 import React from "react";
-import classNames from "classnames";
 import { FormattedMessage } from "react-intl";
 import { createAndRedirectToNewHub } from "../../utils/phoenix-utils";
-import styles from "./Button.scss";
+import { Button } from "../input/Button";
 
 export function CreateRoomButton() {
   return (
-    <button
-      className={classNames(styles.primaryButton, styles.ctaButton)}
+    <Button
+      primary
+      cta
       onClick={e => {
         e.preventDefault();
         createAndRedirectToNewHub(null, null, false);
       }}
     >
       <FormattedMessage id="home.create_a_room" />
-    </button>
+    </Button>
   );
 }
