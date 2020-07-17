fbf5d2976155347bed1107dbbcc9c136de79869f
John Shaughnessy <johnfshaughnessy@gmail.com>
2020-07-17 10:38:49 -0700

    Missed a word

--- src/utils/threejs-world-update.js
+++ src/utils/threejs-world-update.js
@@ -10,7 +10,7 @@ With this patch you must make sure to follow these rules or very strange things 
 - If you modify an object's matrix you MUST decompose() back onto its position, quaternion and scale and you MUST set matrixWorldNeedsUpdate (or matrixNeedsUpdate, but the former is more correct). (applyMatrix() and updateMatrix() handle this for you)
 - If you modify an object's matrixWorld you MUST make sure it has previously been modified so that it is not using its parent matrix as its own, you MUST update its local matrix and you MUST update its position/quaternion/scale. (setMatrixWorld() handles this for you)
 - Before you read an object's matrix you MUST call updateMatrix() or updateMatrices().
-- Before you read object's matrixWorld you MUST call updateMatrices(). (getWorldPosition, getWorldOrientation and getWorldScale handle this for you)
+- Before you read an object's matrixWorld you MUST call updateMatrices(). (getWorldPosition, getWorldOrientation and getWorldScale handle this for you)
 - Do not set matrixIsModified yourself. You could accidentally overwrite parent matrices.
 - Note updateMatrix, updateMatrixWorld, updateWorldMatrix, updateMatrices, setMatrixWorld, matrixNeedsUpdate, matrixWorldNeedsUpdate, and matrixIsModified are all different things. Most already exist in ThreeJS but some have been added here. Double check you are using the one you intend to.
 - Be on the lookout for compatibility issues with third party libraries.
