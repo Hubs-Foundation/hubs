f5ea831a09204d1b4a3ab1d171080730b8d66d1a
RicoZethof <ricozethof@gmail.com>
2020-07-22 14:53:30 +0200

    RZ - attach-camera -> Follows player head

--- CM3D/Scripts/attach-camera.js
+++ CM3D/Scripts/attach-camera.js
@@ -1,54 +1,53 @@
 let attachCamInterval;
-class attachCamera
-{
-    constructor(){
+class attachCamera {
+  constructor() {}
+  attach() {
+    const camHash = "vide";
+    const camEl = getFirstElementFromHash(camHash);
 
+    if (camEl) {
+      attachCamInterval = setInterval(attachCam, 10);
+    } else {
+      console.warn("You need to active your webcam first");
     }
-    attach() {
-        var yCamOffset = 1.6;
-        var camHash = "vide";
 
-        var camEl = getFirstElementFromHash(camHash);
-
-        if (camEl){
-            attachCamInterval = setInterval(attachCam, 10)
-        } else {
-            console.warn("You need to active your webcam first")
-        }
-
-        camEl.object3D.scale.setScalar(0.2);
-        var selfEl = AFRAME.scenes[0].querySelector("#avatar-rig");
-        var povCam = selfEl.querySelector("#avatar-pov-node");
-
-        function attachCam(){ attachObjToAvatar(camEl, selfEl) }
-
-        function attachObjToAvatar(obj, avatar){
-            NAF.utils.getNetworkedEntity(obj).then(networkedEl => {
-                const mine = NAF.utils.isMine(networkedEl);
-                if (!mine) var owned = NAF.utils.takeOwnership(networkedEl);
-                networkedEl.object3D.position.copy( avatar.object3D.position );
-                networkedEl.object3D.rotation.y = povCam.object3D.rotation.y + yCamOffset;
-                networkedEl.object3D.rotation.x = 0;
-                networkedEl.object3D.rotation.z = 0;
-                networkedEl.object3D.position.y += 1.32;
-            })
-        }
-
-        function getFirstElementFromHash(hash){
-            var g = AFRAME.scenes[0].querySelectorAll("[media-loader]");
-            var matches = [];
-            for (let e of g){
-                var m = e.components["media-loader"].attrValue.src.match(hash);
-                if (m && m.length) matches.push(e)
-            }
-            return matches[0]
-        }
+    camEl.object3D.scale.setScalar(0.3);
+    const selfEl = AFRAME.scenes[0].querySelector("#avatar-rig");
+    const povCam = selfEl.querySelector(".model");
+    var headElrotation = new THREE.Quaternion();
+    const headEl = {
+      position: new THREE.Vector3()
     };
 
-    detach() {
-        attachCamInterval = clearInterval();
+    function attachCam() {
+      attachObjToAvatar(camEl, selfEl);
     }
 
+    function attachObjToAvatar(obj, avatar) {
+      NAF.utils.getNetworkedEntity(obj).then(networkedEl => {
+        const mine = NAF.utils.isMine(networkedEl);
+        if (!mine) var owned = NAF.utils.takeOwnership(networkedEl);
+        avatar.querySelectorAll("#avatar-head")[0].object3D.getWorldPosition(headEl.position);
+        networkedEl.object3D.position.copy(headEl.position);
+        networkedEl.object3D.position.y += 0.05;
+        networkedEl.object3D.setRotationFromQuaternion(avatar.querySelectorAll("#avatar-head")[0].object3D.getWorldQuaternion(headElrotation));
+      });
+    }
+
+    function getFirstElementFromHash(hash) {
+      const g = AFRAME.scenes[0].querySelectorAll("[media-loader]");
+      const matches = [];
+      for (const e of g) {
+        const m = e.components["media-loader"].attrValue.src.match(hash);
+        if (m && m.length) matches.push(e);
+      }
+      return matches[0];
+    }
+  }
+
+  detach() {
+    attachCamInterval = clearInterval();
+  }
 }
 
 export default attachCamera;
