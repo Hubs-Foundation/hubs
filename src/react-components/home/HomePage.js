1319dcc3878a022bbe3e3209f62ae8a555e04252
Caspian Baska <caspianbaska@gmail.com>
2020-08-13 16:40:54 +1000

    fix: hash params for login

--- src/react-components/home/HomePage.js
+++ src/react-components/home/HomePage.js
@@ -35,18 +35,11 @@ import logoutButtonHover from "../../assets/images/logout-button-hover.png";
 import logoutButtonHoverWebp from "../../assets/images/logout-button-hover.webp";
 import getRoomMetadata from "../../room-metadata";
 
+import qsTruthy from "../../utils/qs_truthy";
 
 addLocaleData([...en]);
 
-
-const queryArgs = queryString.parse(window.location.search);
-let showLogin = false;
-
-// queryString is including the ? now lol.. investigate later
-if (queryArgs["login"] || queryArgs["?login"]) {
-  showLogin = true;
-}
-
+const showLogin = qsTruthy("login");
 
 const LogoutButton = ({ onLinkClicked }) => {
   const [isShown, setIsShown] = useState(false);
