d9d62fe1e3779db9e488a6e4c20644459bc4e6dc
Robert Long <robert@robertlong.me>
2020-08-17 12:02:56 -0700

    Fix linting

--- src/react-components/input/Button.js
+++ src/react-components/input/Button.js
@@ -1,4 +1,5 @@
 import React from "react";
+import PropTypes from "prop-types";
 import classNames from "classnames";
 import styles from "./Button.scss";
 
@@ -9,6 +10,12 @@ export function Button({ preset, className, children, ...rest }) {
     </button>
   );
 }
+
+Button.propTypes = {
+  preset: PropTypes.string,
+  className: PropTypes.string,
+  children: PropTypes.node
+};
 
 Button.defaultProps = {
   preset: "basic"
--- src/react-components/input/useAccessibleOutlineStyle.js
+++ src/react-components/input/useAccessibleOutlineStyle.js
@@ -4,11 +4,13 @@ import { useEffect, useCallback } from "react";
 // When the user presses the tab key the keyboardUserClass is added to the body and all outlines are re-enabled.
 // This lets us provide a clean look for mouse users, while still maintaining accessibility for keyboard users.
 export function useAccessibleOutlineStyle(keyboardUserClass = "keyboard-user") {
-  const onFirstTab = useCallback(
+  let onFirstTab = null;
+
+  onFirstTab = useCallback(
     e => {
       if (e.key === "Tab") {
         document.body.classList.add(keyboardUserClass);
-        window.removeEventListener("keydown", handleFirstTab);
+        window.removeEventListener("keydown", onFirstTab);
       }
     },
     [keyboardUserClass]
