377625d2a92e8dfea613538f87d5925fca8c8c20
Kevin Lee <kevin@infinite-lee.com>
2020-08-28 16:19:40 -0700

    cleanup + refactor

--- src/utils/i18n.js
+++ src/utils/i18n.js
@@ -3,29 +3,28 @@ import { AVAILABLE_LOCALES, FALLBACK_LOCALES } from "../assets/locales/locale_co
 import defaultLocaleData from "../assets/locales/en.json";
 
 const DEFAULT_LOCALE = "en";
-
-function getLocales() {
-  if (navigator.languages) {
-    return [...navigator.languages];
-  }
-  if (navigator.language) {
-    return [navigator.language];
-  }
-  if (navigator.userLanguage) {
-    return [navigator.userLanguage];
-  }
-}
+const cachedMessages = new Map();
 
 let _locale = DEFAULT_LOCALE;
 let _messages = defaultLocaleData;
 
-function setLocale() {
-  const locales = getLocales();
+function findLocale() {
+  const locales = (() => {
+    if (navigator.languages) {
+      return [...navigator.languages];
+    }
+    if (navigator.language) {
+      return [navigator.language];
+    }
+    if (navigator.userLanguage) {
+      return [navigator.userLanguage];
+    }
+  })();
 
-  const store = window.APP.store;
+  const preferences = window.APP.store.state.preferences;
 
-  if (store.state.preferences.locale && store.state.preferences.locale !== "browser") {
-    locales.unshift(store.state.preferences.locale);
+  if (preferences.locale && preferences.locale !== "browser") {
+    locales.unshift(preferences.locale);
   }
 
   let locale = DEFAULT_LOCALE;
@@ -34,34 +33,40 @@ function setLocale() {
 
     if (!AVAILABLE_LOCALES[locale]) {
       if (FALLBACK_LOCALES[locale]) {
-        locale = FALLBACK_LOCALES[locale];
-        break;
+        return FALLBACK_LOCALES[locale];
       } else {
         continue;
       }
     }
-    break;
+    return locale;
   }
+}
 
-  if (locale === DEFAULT_LOCALE) {
-    _locale = locale;
+function updateLocale() {
+  _locale = findLocale();
+
+  if (_locale === DEFAULT_LOCALE) {
     _messages = defaultLocaleData;
     document.body.dispatchEvent(new CustomEvent("locale-updated"));
   } else {
-    import(`../assets/locales/${locale}.json`).then(({ default: localeData }) => {
-      _locale = locale;
-      _messages = localeData;
-      document.body.dispatchEvent(new CustomEvent("locale-updated"));
-    });
+    if (cachedMessages.has(_locale)) {
+      _messages = cachedMessages.get(_locale);
+    } else {
+      import(`../assets/locales/${_locale}.json`).then(({ default: localeData }) => {
+        cachedMessages[_locale] = localeData;
+        _messages = localeData;
+        document.body.dispatchEvent(new CustomEvent("locale-updated"));
+      });
+    }
   }
 }
 
 const interval = window.setInterval(() => {
   if (window.APP && window.APP.store) {
     window.clearInterval(interval);
-    setLocale();
+    updateLocale();
     window.APP.store.addEventListener("statechanged", () => {
-      setLocale();
+      updateLocale();
     });
   }
 }, 100);
@@ -70,41 +75,38 @@ export const getLocale = () => {
   return _locale;
 };
 
-export const getMessages = (() => {
-  const cachedMessages = new Map();
-  return () => {
-    if (cachedMessages.has(_locale)) {
-      return cachedMessages.get(_locale);
+export const getMessages = () => {
+  if (cachedMessages.has(_locale)) {
+    return cachedMessages.get(_locale);
+  }
+
+  // Swap in translations specified via the admin panel
+  if (configs.APP_CONFIG && configs.APP_CONFIG.translations && configs.APP_CONFIG.translations[_locale]) {
+    const configTranslations = configs.APP_CONFIG.translations[_locale];
+    for (const messageKey in configTranslations) {
+      if (!configTranslations.hasOwnProperty(messageKey)) continue;
+      if (!configTranslations[messageKey]) continue;
+      _messages[messageKey] = configTranslations[messageKey];
     }
+  }
 
-    // Swap in translations specified via the admin panel
-    if (configs.APP_CONFIG && configs.APP_CONFIG.translations && configs.APP_CONFIG.translations[_locale]) {
-      const configTranslations = configs.APP_CONFIG.translations[_locale];
-      for (const messageKey in configTranslations) {
-        if (!configTranslations.hasOwnProperty(messageKey)) continue;
-        if (!configTranslations[messageKey]) continue;
-        _messages[messageKey] = configTranslations[messageKey];
-      }
-    }
+  const entries = [];
+  for (const key in _messages) {
+    if (!_messages.hasOwnProperty(key)) continue;
+    entries.push([key, _messages[key]]);
+  }
 
-    const entries = [];
-    for (const key in _messages) {
-      if (!_messages.hasOwnProperty(key)) continue;
-      entries.push([key, _messages[key]]);
-    }
+  const messages = entries
+    .map(([key, message]) => [
+      key,
+      // Replace nested message keys (e.g. %app-name%) with their messages.
+      message.replace(/%([\w-.]+)%/i, (_match, subkey) => _messages[subkey])
+    ])
+    .reduce((acc, entry) => {
+      acc[entry[0]] = entry[1];
+      return acc;
+    }, {});
 
-    const messages = entries
-      .map(([key, message]) => [
-        key,
-        // Replace nested message keys (e.g. %app-name%) with their messages.
-        message.replace(/%([\w-.]+)%/i, (_match, subkey) => _messages[subkey])
-      ])
-      .reduce((acc, entry) => {
-        acc[entry[0]] = entry[1];
-        return acc;
-      }, {});
-
-    cachedMessages.set(_locale, messages);
-    return messages;
-  };
-})();
+  cachedMessages.set(_locale, messages);
+  return messages;
+};
