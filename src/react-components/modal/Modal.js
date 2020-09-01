27d4752bcccddc43844f72392f7dee2af2dc8c07
Robert Long <robert@robertlong.me>
2020-08-31 18:03:02 -0700

    Add Enter on Device Modal

--- src/react-components/modal/Modal.js
+++ src/react-components/modal/Modal.js
@@ -3,9 +3,9 @@ import PropTypes from "prop-types";
 import classNames from "classnames";
 import styles from "./Modal.scss";
 
-export function Modal({ title, beforeTitle, afterTitle, children, contentClassName, className }) {
+export function Modal({ title, beforeTitle, afterTitle, children, contentClassName, className, disableFullscreen }) {
   return (
-    <div className={classNames(styles.modal, className)}>
+    <div className={classNames(styles.modal, { [styles.smFullscreen]: !disableFullscreen }, className)}>
       {(title || beforeTitle || afterTitle) && (
         <div className={styles.header}>
           <div className={styles.beforeTitle}>{beforeTitle}</div>
@@ -24,5 +24,6 @@ Modal.propTypes = {
   afterTitle: PropTypes.node,
   children: PropTypes.node,
   className: PropTypes.string,
-  contentClassName: PropTypes.string
+  contentClassName: PropTypes.string,
+  disableFullscreen: PropTypes.bool
 };
