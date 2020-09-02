e210af8d4b876abf3145b430d6a4749ae813fe12
Robert Long <robert@robertlong.me>
2020-09-02 10:30:11 -0700

    Add better supoport for SelectInputField options

--- src/react-components/input/SelectInputField.js
+++ src/react-components/input/SelectInputField.js
@@ -6,7 +6,37 @@ import styles from "./SelectInputField.scss";
 import { ReactComponent as CaretDownIcon } from "../icons/CaretDown.svg";
 import { useSelect } from "downshift";
 
-export function SelectInputField({ className, error, description, inputClassName, label, onChange, items, ...rest }) {
+function getSelectedItem(value, options) {
+  if (options.length > 0 && typeof options[0] === "object") {
+    return options.find(item => item.value === value);
+  }
+
+  return value;
+}
+
+function getItemId(item) {
+  return typeof item === "object" ? item.id : item;
+}
+
+function getItemLabel(item) {
+  return typeof item === "object" ? item.label || item.value : item;
+}
+
+function getItemValue(item) {
+  return typeof item === "object" ? item.value : item;
+}
+
+export function SelectInputField({
+  className,
+  error,
+  description,
+  inputClassName,
+  label,
+  onChange,
+  value,
+  options,
+  ...rest
+}) {
   const {
     isOpen,
     selectedItem,
@@ -16,27 +46,34 @@ export function SelectInputField({ className, error, description, inputClassName
     highlightedIndex,
     getItemProps
   } = useSelect({
-    items,
+    items: options,
+    selectedItem: getSelectedItem(value, options),
     ...rest,
-    onSelectedItemChange: onChange
+    onSelectedItemChange: ({ selectedItem }) => {
+      if (onChange) {
+        onChange(getItemValue(selectedItem));
+      }
+    }
   });
+
+  const selectedItemLabel = getItemLabel(selectedItem);
 
   return (
     <InputField {...getLabelProps()} className={className} label={label} error={error} description={description}>
       <div className={classNames(styles.selectInput, { [styles.open]: isOpen }, inputClassName)}>
         <button className={styles.dropdownButton} type="button" {...getToggleButtonProps()}>
-          <span>{selectedItem ? selectedItem.label : "Select..."}</span>
+          <span>{selectedItemLabel !== undefined ? selectedItemLabel : "Select..."}</span>
           <CaretDownIcon />
         </button>
         <ul {...getMenuProps()} className={styles.dropdown}>
           {isOpen &&
-            items.map((item, index) => (
+            options.map((item, index) => (
               <li
                 className={classNames(styles.dropdownItem, { [styles.highlightedItem]: highlightedIndex === index })}
-                key={item.id}
+                key={getItemId(item)}
                 {...getItemProps({ item, index })}
               >
-                {item.label}
+                {getItemLabel(item)}
               </li>
             ))}
         </ul>
@@ -52,10 +89,21 @@ SelectInputField.propTypes = {
   description: PropTypes.node,
   labelClassName: PropTypes.string,
   inputClassName: PropTypes.string,
-  items: PropTypes.array.isRequired,
+  value: PropTypes.oneOfType([PropTypes.string, PropTypes.number]),
+  options: PropTypes.arrayOf(
+    PropTypes.oneOfType([
+      PropTypes.string,
+      PropTypes.number,
+      PropTypes.shape({
+        id: PropTypes.string.isRequired,
+        label: PropTypes.string,
+        value: PropTypes.any.isRequired
+      })
+    ])
+  ).isRequired,
   onChange: PropTypes.func
 };
 
 SelectInputField.defaultProps = {
-  items: []
+  options: []
 };
--- src/react-components/input/SelectInputField.stories.js
+++ src/react-components/input/SelectInputField.stories.js
@@ -9,9 +9,38 @@ export const All = args => (
   <div style={{ display: "flex", flexDirection: "column", gap: "24px" }}>
     <SelectInputField {...args} />
     <SelectInputField label="With Label" {...args} />
+    <SelectInputField label="Selected" {...args} value={2} />
   </div>
 );
 
 All.args = {
-  items: [{ id: 1, label: "Option 1" }, { id: 2, label: "Option 2" }, { id: 3, label: "Option 3" }]
+  options: [
+    { id: "1", label: "Option 1", value: 1 },
+    { id: "2", label: "Option 2", value: 2 },
+    { id: "3", label: "Option 3", value: 3 }
+  ]
+};
+
+export const StringOptions = args => (
+  <div style={{ display: "flex", flexDirection: "column", gap: "24px" }}>
+    <SelectInputField {...args} />
+    <SelectInputField label="With Label" {...args} />
+    <SelectInputField label="Selected" {...args} value="Option B" />
+  </div>
+);
+
+StringOptions.args = {
+  options: ["Option A", "Option B", "Option C"]
+};
+
+export const NumberOptions = args => (
+  <div style={{ display: "flex", flexDirection: "column", gap: "24px" }}>
+    <SelectInputField {...args} />
+    <SelectInputField label="With Label" {...args} />
+    <SelectInputField label="Selected" {...args} value={2} />
+  </div>
+);
+
+NumberOptions.args = {
+  options: [1, 2, 3]
 };
