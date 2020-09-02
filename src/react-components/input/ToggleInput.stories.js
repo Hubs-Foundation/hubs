474e20b670aa8296170384d9b8a33cfcf5fe7b75
Robert Long <robert@robertlong.me>
2020-09-02 14:48:22 -0700

    Add ToggleInput

---
+++ src/react-components/input/ToggleInput.stories.js
@@ -0,0 +1,18 @@
+import React from "react";
+import { ToggleInput } from "./ToggleInput";
+
+export default {
+  title: "ToggleInput",
+  argTypes: { onChange: { action: "onChange" } }
+};
+
+export const All = args => (
+  <div style={{ display: "flex", flexDirection: "column", gap: "8px" }}>
+    <ToggleInput {...args} />
+    <ToggleInput label="Toggle Input" {...args} />
+    <ToggleInput label="Toggle Input" value={false} {...args} />
+    <ToggleInput label="Toggle Input" value={true} {...args} />
+    <ToggleInput label="Toggle Input" value={false} disabled {...args} />
+    <ToggleInput label="Toggle Input" value={true} disabled {...args} />
+  </div>
+);
