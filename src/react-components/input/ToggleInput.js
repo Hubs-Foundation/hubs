0587f6096447578b4856c5c997c7084ee717cb08
Robert Long <robert@robertlong.me>
2020-11-04 10:55:28 -0800

    Fix toggle button alignment

--- src/react-components/input/ToggleInput.js
+++ src/react-components/input/ToggleInput.js
@@ -7,16 +7,16 @@ export const ToggleInput = memo(
   forwardRef(({ className, label, description, value, disabled, ...rest }, ref) => {
     return (
       <label className={classNames(styles.toggleInput, { [styles.disabled]: disabled }, className)}>
+        <div className={classNames(styles.track, { [styles.on]: !!value })}>
+          <div className={styles.button} />
+        </div>
+        <input type="checkbox" {...rest} disabled={disabled} checked={!!value} value={value} ref={ref} />
         {label && (
           <div className={styles.labelContainer}>
             <p className={styles.label}>{label}</p>
             {description && <p className={styles.description}>{description}</p>}
           </div>
         )}
-        <div className={classNames(styles.track, { [styles.on]: !!value })}>
-          <div className={styles.button} />
-        </div>
-        <input type="checkbox" {...rest} disabled={disabled} checked={!!value} value={value} ref={ref} />
       </label>
     );
   })
