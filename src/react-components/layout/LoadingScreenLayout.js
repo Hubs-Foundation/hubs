0d0fa8a5955f338c710329cf35e4a3a7432dd0a7
Robert Long <robert@robertlong.me>
2020-11-12 14:00:46 -0800

    Migrate LoadingScreen component

--- src/react-components/layout/LoadingScreenLayout.js
+++ src/react-components/layout/LoadingScreenLayout.js
@@ -1,15 +1,20 @@
 import React from "react";
 import PropTypes from "prop-types";
 import styles from "./LoadingScreenLayout.scss";
+import { Column } from "../layout/Column";
 
 export function LoadingScreenLayout({ center, bottom, logoSrc }) {
   return (
     <div className={styles.loadingScreenLayout}>
-      <div className={styles.center}>
+      <Column center gap="lg" className={styles.center}>
         <img className={styles.logo} src={logoSrc} />
         {center}
-      </div>
-      {bottom && <div className={styles.bottom}>{bottom}</div>}
+      </Column>
+      {bottom && (
+        <Column center className={styles.bottom}>
+          {bottom}
+        </Column>
+      )}
     </div>
   );
 }
