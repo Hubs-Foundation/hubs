19b1892294cb9165e93b8562577639c1be68a6d9
Robert Long <robert@robertlong.me>
2020-11-16 18:34:30 -0800

    Add logging

--- src/react-components/room/TweetEditorModalContainer.js
+++ src/react-components/room/TweetEditorModalContainer.js
@@ -77,6 +77,8 @@ export function TweetEditorModalContainer({ initialTweet, mediaUrl, contentSubty
     [contentSubtype, mediaUrl]
   );
 
+  console.log({ mediaUrl, mediaThumbnailUrl });
+
   return (
     <TweetEditorModal
       editorState={editorState}
