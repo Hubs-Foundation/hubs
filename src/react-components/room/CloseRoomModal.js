bd3bfe036bb93d2d0e8a6598b94f512897f77a3c
Robert Long <robert@robertlong.me>
2020-12-16 17:15:12 -0800

    Localize CloseRoomModal

--- src/react-components/room/CloseRoomModal.js
+++ src/react-components/room/CloseRoomModal.js
@@ -3,22 +3,27 @@ import { FormattedMessage } from "react-intl";
 import PropTypes from "prop-types";
 import { Modal } from "../modal/Modal";
 import { CloseButton } from "../input/CloseButton";
-import { Button } from "../input/Button";
+import { Button, CancelButton } from "../input/Button";
 import { Column } from "../layout/Column";
 
 export function CloseRoomModal({ onClose, onConfirm }) {
   return (
-    <Modal title="Close Room" beforeTitle={<CloseButton onClick={onClose} />}>
+    <Modal
+      title={<FormattedMessage id="close-room-modal.title" defaultMessage="Close Room" />}
+      beforeTitle={<CloseButton onClick={onClose} />}
+    >
       <Column padding center centerMd="both" grow>
         <p>
-          <FormattedMessage id="close-room.message" />
+          <FormattedMessage
+            id="close-room-modal.message"
+            defaultMessage="Closing this room will remove yourself and others from the room, shutting it down permanently.{linebreak}Are you sure? This action cannot be undone."
+            values={{ linebreak: <br /> }}
+          />
         </p>
         <Button preset="accept" onClick={onConfirm}>
-          <FormattedMessage id="close-room.confirm" />
+          <FormattedMessage id="close-room-modal.confirm" defaultMessage="Yes, Close Room" />
         </Button>
-        <Button preset="cancel" onClick={onClose}>
-          <FormattedMessage id="close-room.cancel" />
-        </Button>
+        <CancelButton onClick={onClose} />
       </Column>
     </Modal>
   );
