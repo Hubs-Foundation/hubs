35b605dfcbc74d6268484c0ddedc681d6ce92842
netpro2k <netpro2k@gmail.com>
2021-05-20 20:58:28 -0700

    Fix potential race condition in permissions checks

--- src/components/player-info.js
+++ src/components/player-info.js
@@ -128,7 +128,7 @@ AFRAME.registerComponent("player-info", {
     this.applyDisplayName();
   },
   can(perm) {
-    return !!this.permissiosn[perm];
+    return !!this.permissions && this.permissiosn[perm];
   },
   applyDisplayName() {
     const store = window.APP.store;
