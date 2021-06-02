f33ffa99b652b01a14df5157f2e8e19422278efb
John Shaughnessy <johnfshaughnessy@gmail.com>
2021-06-02 16:42:05 -0700

    Remove more references to Google Poly.

--- src/react-components/room/PlacePopoverContainer.js
+++ src/react-components/room/PlacePopoverContainer.js
@@ -55,12 +55,12 @@ export function PlacePopoverContainer({ scene, mediaSearchStore, showNonHistorie
               label: <FormattedMessage id="place-popover.item-type.gif" defaultMessage="GIF" />,
               onSelect: () => mediaSearchStore.sourceNavigate("gifs")
             },
-            (configs.integration("poly") || configs.integration("sketchfab")) && {
+            configs.integration("sketchfab") && {
               id: "model",
               icon: ObjectIcon,
               color: "accent2",
               label: <FormattedMessage id="place-popover.item-type.model" defaultMessage="3D Model" />,
-              onSelect: () => mediaSearchStore.sourceNavigate(configs.integration("poly") ? "poly" : "sketchfab")
+              onSelect: () => mediaSearchStore.sourceNavigate("sketchfab")
             },
             {
               id: "avatar",
