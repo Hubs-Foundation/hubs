bfd380cc85694dffca5fabf0b446739e85148fc4
Rupert Rawnsley <rupert.rawnsley@gmail.com>
2021-06-16 13:54:17 +0100

    Added missing space to match PeopleSidebar

--- src/react-components/room/ContentMenu.js
+++ src/react-components/room/ContentMenu.js
@@ -36,7 +36,7 @@ export function PeopleMenuButton(props) {
     <ContentMenuButton {...props}>
       <PeopleIcon />
       <span>
-        <FormattedMessage id="content-menu.people-menu-button" defaultMessage="People" />({props.presenceCount})
+        <FormattedMessage id="content-menu.people-menu-button" defaultMessage="People" /> ({props.presenceCount})
       </span>
     </ContentMenuButton>
   );
