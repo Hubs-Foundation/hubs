f7d401f8fd8be615bd89144eb137fece97ee5d5b
netpro2k <netpro2k@gmail.com>
2021-06-21 18:48:42 -0700

    Restore popstate handler

--- src/change-hub.js
+++ src/change-hub.js
@@ -97,3 +97,11 @@ export async function changeHub(hubId, addToHistory = true) {
   });
 }
 window.changeHub = changeHub;
+  // TODO see if there is a better way to do this with react router
+  window.addEventListener("popstate", function(e) {
+    const qs = new URLSearchParams(location.search);
+    const newHubId = qs.get("hub_id") || document.location.pathname.substring(1).split("/")[0];
+    if (newHubId !== APP.hub.hub_id) {
+      changeHub(newHubId, false);
+    }
+  });
