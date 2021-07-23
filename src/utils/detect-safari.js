0c485b8fb3fbffa80c5282718ff9a21e8d24b4ae
Brian Peiris <brianpeiris@gmail.com>
2021-07-23 06:58:22 -0400

    Force disable panner audio in safari

---
+++ src/utils/detect-safari.js
@@ -0,0 +1,6 @@
+import { detect } from "detect-browser";
+
+export function isSafari() {
+  const browser = detect();
+  return ["iOS", "Mac OS"].includes(browser.os) && ["safari", "ios"].includes(browser.name)
+}
