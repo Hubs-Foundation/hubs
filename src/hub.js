69cc235fda6469e75944bf79c9f327d7b647727f
Takahiro <hogehoge@gachapin.jp>
2021-07-27 00:44:07 -0700

    Import webxr-polyfill before importing other modules

--- src/hub.js
+++ src/hub.js
@@ -1,13 +1,3 @@
-import WebXRPolyfill from "webxr-polyfill";
-
-// WebXR Polyfill for platforms only supporting WebVR API. e.g. Desktop Firefox
-// WebVR API is deprecated. We may drop such platforms support at some point.
-if (!("xr" in navigator) && "getVRDisplays" in navigator) {
-  new WebXRPolyfill({
-    cardboard: false
-  });
-}
-
 import "./utils/debug-log";
 import configs from "./utils/configs";
 import "./utils/theme";
