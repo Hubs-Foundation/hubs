e39e9a3c4fc562d94ef2dc1071007a025f83176f
John Shaughnessy <johnfshaughnessy@gmail.com>
2021-08-21 10:00:27 -0700

    Allow partial audio layers.
    Some audio layers do not want to affect every possible audio parameter,
    so we do not require them to.
    
    -  // TODO: The DEBUG PANEL only wants to set a few settings at a time
    -  // TODO: Perhaps AUDIO ZONEs should be allowed to affect only a few settings at a time

--- src/update-audio-settings.js
+++ src/update-audio-settings.js
@@ -1,8 +1,14 @@
 import { SourceType, MediaAudioDefaults, AvatarAudioDefaults, TargetAudioDefaults } from "./components/audio-params";
 
-export const GAIN_TIME_CONST = 0.2;
+const defaultSettingsForSourceType = Object.freeze(
+  new Map([
+    [SourceType.MEDIA_VIDEO, MediaAudioDefaults],
+    [SourceType.AVATAR_AUDIO_SOURCE, AvatarAudioDefaults],
+    [SourceType.AUDIO_TARGET, TargetAudioDefaults]
+  ])
+);
 
-function applySettings(el, audio, settings) {
+function applySettings(audio, settings) {
   if (audio.panner) {
     audio.setDistanceModel(settings.distanceModel);
     audio.setRolloffFactor(settings.rolloffFactor);
@@ -12,58 +18,25 @@ function applySettings(el, audio, settings) {
     audio.panner.coneOuterAngle = settings.coneOuterAngle;
     audio.panner.coneOuterGain = settings.coneOuterGain;
   }
-
-  // We don't deal with the audioOutputMode here anymore, we just apply the gain.
-  // TODO Remove audioOutputMode from the rest of the app.
-  const gainMultiplier = APP.gainMultipliers.has(el) ? APP.gainMultipliers.get(el) : 1;
-  const gain = APP.clippingState.has(el) || APP.linkedMutedState.has(el) ? 0 : gainMultiplier * settings.gain;
-  audio.gain.gain.setTargetAtTime(gain, audio.context.currentTime, GAIN_TIME_CONST);
+  audio.gain.gain.setTargetAtTime(settings.gain, audio.context.currentTime, 0.1);
 }
 
-const defaultSettingsForSourceType = new Map([
-  [SourceType.MEDIA_VIDEO, MediaAudioDefaults],
-  [SourceType.AVATAR_AUDIO_SOURCE, AvatarAudioDefaults],
-  [SourceType.AUDIO_TARGET, TargetAudioDefaults]
-]);
-
 export function getCurrentAudioSettings(el) {
-  // -- Highest Precedence --
-  //
-  //  DEBUG PANEL
-  //  AUDIO ZONE
-  //  PER-OBJECT OVERRIDES
-  //  SCENE OVERRIDES
-  //  APP DEFAULTS
-  //
-  // -- Lowest Precedence --
-
   // TODO: Add the DEBUG PANEL settings.
-  // TODO: The DEBUG PANEL only wants to set a few settings at a time
-  // TODO: Perhaps AUDIO ZONEs should be allowed to affect only a few settings at a time
-
-  const zoneSettings = APP.zoneOverrides.get(el);
-  if (zoneSettings) {
-    return zoneSettings;
-  }
-
-  const audioOverrides = APP.audioOverrides.get(el);
-  if (audioOverrides) {
-    return audioOverrides;
-  }
-
   const sourceType = APP.sourceType.get(el);
-  if (sourceType !== undefined) {
-    const sceneOverrides = APP.sceneAudioDefaults.get(sourceType);
-    if (sceneOverrides) {
-      return sceneOverrides;
-    }
+  const defaults = defaultSettingsForSourceType.get(sourceType);
+  const sceneOverrides = APP.sceneAudioDefaults.get(sourceType);
+  const audioOverrides = APP.audioOverrides.get(el);
+  const zoneSettings = APP.zoneOverrides.get(el);
+  const settings = Object.assign({}, defaults, sceneOverrides, audioOverrides, zoneSettings);
 
-    const defaults = defaultSettingsForSourceType.get(sourceType);
-    if (defaults) {
-      return defaults;
-    }
+  if (APP.clippingState.has(el) || APP.linkedMutedState.has(el)) {
+    settings.gain = 0;
+  } else if (APP.gainMultipliers.has(el)) {
+    settings.gain = settings.gain * APP.gainMultipliers.get(el);
   }
-  return null;
+
+  return settings;
 }
 
 // TODO: Change this name or the name of the function on audio-settings-system
@@ -71,11 +44,5 @@ export function updateAudioSettings(el, audio) {
   // Follow these rules and you'll have a good time:
   // - If a THREE.Audio or THREE.PositionalAudio is created, call this function.
   // - If you audio settings change, call this function.
-  const settings = getCurrentAudioSettings(el);
-  if (settings) {
-    applySettings(el, audio, settings);
-    return;
-  }
-
-  console.error("Uh oh! Had no settings to apply", el, audio);
+  applySettings(audio, getCurrentAudioSettings(el));
 }
