0625b01f816ba1be022f295d27759a830b283e8e
Matthew Cool <matthewbcool@gmail.com>
2021-08-30 12:27:22 -0700

    adds a special check for HMC with a centered version of the logo for loading screen and entry modal. updates blues in themes to match design and for improved color contrast

--- src/react-components/room/LoadingScreen.js
+++ src/react-components/room/LoadingScreen.js
@@ -3,13 +3,14 @@ import PropTypes from "prop-types";
 import { LoadingScreenLayout } from "../layout/LoadingScreenLayout";
 import { Spinner } from "../misc/Spinner";
 import { useRandomMessageTransition } from "./useRandomMessageTransition";
-
+import configs from "../../utils/configs";
+import hmcLogo from "../../assets/images/hmc-logo.png";
 export function LoadingScreen({ logoSrc, message, infoMessages }) {
   const infoMessage = useRandomMessageTransition(infoMessages);
-
+  const isHmc = configs.feature("show_cloud");
   return (
     <LoadingScreenLayout
-      logoSrc={logoSrc}
+      logoSrc={isHmc ? hmcLogo : logoSrc}
       center={
         <>
           <Spinner />
