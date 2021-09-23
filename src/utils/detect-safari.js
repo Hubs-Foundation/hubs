1a3613ef3c2e3b2d383601e436a53fb5fb517ad0
Brian Peiris <brianpeiris@gmail.com>
2021-09-23 11:26:01 -0400

    Fix browser detection code in node.js

--- src/utils/detect-safari.js
+++ src/utils/detect-safari.js
@@ -2,5 +2,6 @@ import { detect } from "detect-browser";
 
 export function isSafari() {
   const browser = detect();
+  if (!browser) return false;
   return ["iOS", "Mac OS"].includes(browser.os) && ["safari", "ios"].includes(browser.name)
 }
