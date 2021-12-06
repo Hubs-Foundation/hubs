8e689ece4d8ea2d4eca6541e0d9fefbe89398803
Manuel Martin <manmartin@mozilla.com>
2021-12-06 16:50:52 +0100

    Only add sink hack if is not the default sink

--- src/react-components/preferences-screen.js
+++ src/react-components/preferences-screen.js
@@ -814,7 +814,8 @@ class PreferencesScreen extends Component {
         key: "preferredMic",
         prefType: PREFERENCE_LIST_ITEM_TYPE.SELECT,
         options: [{ value: "none", text: "None" }],
-        onChanged: this.onMicSelectionChanged
+        onChanged: this.onMicSelectionChanged,
+        defaultString: "Default"
       },
       preferredCamera: {
         key: "preferredCamera",
@@ -826,7 +827,8 @@ class PreferencesScreen extends Component {
         preferredSpeakers: {
           key: "preferredSpeakers",
           prefType: PREFERENCE_LIST_ITEM_TYPE.SELECT,
-          options: [{ value: "none", text: "None" }]
+          options: [{ value: "none", text: "None" }],
+          defaultString: "Default"
         }
       })
     };
