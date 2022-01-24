3b5260b153166466b09a8a079cb7985bc000bdec
Juunini (쥬니니) <juuni.ni.i@gmail.com>
2022-01-25 08:48:49 +0900

    Using entity's geometry

--- src/components/mirror.js
+++ src/components/mirror.js
@@ -237,15 +237,8 @@ Reflector.ReflectorShader = {
 };
 
 AFRAME.registerComponent("mirror", {
-  schema: {
-    width: { type: "number", default: 1 },
-    height: { type: "number", default: 1 }
-  },
-
   init() {
-    const { width, height } = this.data;
-
-    const reflector = new Reflector(new AFRAME.THREE.PlaneBufferGeometry(width, height), {
+    const reflector = new Reflector(this.el.components.geometry.geometry, {
       color: new AFRAME.THREE.Color(0x7f7f7f),
       textureWidth: window.innerWidth * window.devicePixelRatio,
       textureHeight: window.innerHeight * window.devicePixelRatio
