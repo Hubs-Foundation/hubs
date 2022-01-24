d48f7c942882b4f0ed01777077c4bc9b9611b8e8
Manuel Martin <manmartin@mozilla.com>
2022-01-24 18:28:05 +0100

    Use a callback ref to react to node updates

--- src/react-components/misc/LevelBar.js
+++ src/react-components/misc/LevelBar.js
@@ -1,21 +1,21 @@
-import React, { useRef, useEffect } from "react";
+import React, { useCallback } from "react";
 import PropTypes from "prop-types";
 import classNames from "classnames";
 import styles from "./LevelBar.scss";
 
 export function LevelBar({ className, level }) {
-  const ref = useRef();
-
-  useEffect(
-    () => {
-      const pct = level * 100;
-      if (ref.current.clientWidth > ref.current.clientHeight) {
-        ref.current.style.clipPath = `polygon(0% 100%, ${pct}% 100%, ${pct}% 0%, 0% 0%)`;
-      } else {
-        ref.current.style.clipPath = `polygon(0% 100%, 100% 100%, 100% ${100 - pct}%, 0% ${100 - pct}%)`;
+  const ref = useCallback(
+    node => {
+      if (node) {
+        const pct = level * 100;
+        if (node.clientWidth > node.clientHeight) {
+          node.style.clipPath = `polygon(0% 100%, ${pct}% 100%, ${pct}% 0%, 0% 0%)`;
+        } else {
+          node.style.clipPath = `polygon(0% 100%, 100% 100%, 100% ${100 - pct}%, 0% ${100 - pct}%)`;
+        }
       }
     },
-    [ref, level]
+    [level]
   );
 
   return (
