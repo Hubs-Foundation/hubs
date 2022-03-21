158d89bf64364555de04e7b34d85e2eb73cbc214
Matthew Cool <matthewbcool@gmail.com>
2022-03-21 15:32:52 -0700

    changed locale case

--- src/assets/locales/locale_config.js
+++ src/assets/locales/locale_config.js
@@ -1,7 +1,7 @@
 // Primary language tags for those languages we support
 export const AVAILABLE_LOCALES = {
   en: "English",
-  "zh-cn": "简体中文",
+  "zh-CN": "简体中文",
   pt: "Portugês (Brasil)",
   ja: "日本語",
   es: "Español",
@@ -9,7 +9,7 @@ export const AVAILABLE_LOCALES = {
   fr: "Français",
   ko: "한국어",
   de: "Deutsch",
-  "zh-tw": "繁體中文"
+  "zh-TW": "繁體中文"
 };
 
 // Map from alternative primary language tags to those we support
