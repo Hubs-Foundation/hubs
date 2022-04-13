6832b29ba7ed7a6dfd0a84ebe59057ddfb1864d3
Nguyễn Hữu Minh Khai <nguyenhuuminhkhai@gmail.com>
2022-04-13 16:56:24 +0700

    Credentials google

--- src/utilities/constants.js
+++ src/utilities/constants.js
@@ -4,8 +4,8 @@ const expireCookies = 1;
 const API_ROOT = "https://vdc-dev.gemiso.com/larchiveum";
 const ENV = "https://vdc-dev.gemiso.com/larchiveum";
 
-//const APP_ROOT = "https://larchiveum.link";
-const APP_ROOT = "https://hubs.local:8080";
+const APP_ROOT = "https://larchiveum.link";
+// const APP_ROOT = "https://hubs.local:8080";
 
 const naverApp = {
     clientID: 'pSh1Vk5d8Df3XZAURRC5',
@@ -21,10 +21,15 @@ const facebookApp = {
     //clientID: '339289624917102',
     clientID: '4835484659880198',
 }
+// WETOP
+// const googleApp = {
+//     clientID: "58132589047-m5tdln5da7dho7gb7mnp7kqitdnv5dvq.apps.googleusercontent.com",
+//     clientSecret: "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"
+// }
 
 const googleApp = {
-    clientID: "58132589047-m5tdln5da7dho7gb7mnp7kqitdnv5dvq.apps.googleusercontent.com",
-    clientSecret: "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"
+    clientID: "58132589047-k1el1pt544kf47oc1pgqsr6qa1st887u.apps.googleusercontent.com",
+    clientSecret: "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"
 }
 
 export {
