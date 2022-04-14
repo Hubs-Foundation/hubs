fb42183d35c6bb7513ce86453348160a4be4f4c3
netpro2k <netpro2k@gmail.com>
2022-04-13 17:20:43 -0700

    Inline AFRAME entity utils we are using

--- src/components/animation.js
+++ src/components/animation.js
@@ -19,8 +19,67 @@ delete AFRAME.components.animation; // hubs
 var colorHelperFrom = new THREE.Color();
 var colorHelperTo = new THREE.Color();
 
-var getComponentProperty = utils.entity.getComponentProperty;
-var setComponentProperty = utils.entity.setComponentProperty;
+/**
+ * Split a delimited component property string (e.g., `material.color`) to an object
+ * containing `component` name and `property` name. If there is no delimiter, just return the
+ * string back.
+ *
+ * Cache arrays from splitting strings via delimiter to save on memory.
+ *
+ * @param {string} str - e.g., `material.opacity`.
+ * @param {string} delimiter - e.g., `.`.
+ * @returns {array} e.g., `['material', 'opacity']`.
+ */
+var propertyPathCache = {};
+function getComponentPropertyPath(str, delimiter) {
+  delimiter = delimiter || ".";
+  if (!propertyPathCache[delimiter]) {
+    propertyPathCache[delimiter] = {};
+  }
+  if (str.indexOf(delimiter) !== -1) {
+    propertyPathCache[delimiter][str] = str.split(delimiter);
+  } else {
+    propertyPathCache[delimiter][str] = str;
+  }
+  return propertyPathCache[delimiter][str];
+}
+
+/**
+ * Get component property using encoded component name + component property name with a
+ * delimiter.
+ */
+const getComponentProperty = function(el, name, delimiter) {
+  var splitName;
+  delimiter = delimiter || ".";
+  if (name.indexOf(delimiter) !== -1) {
+    splitName = getComponentPropertyPath(name, delimiter);
+    if (splitName.constructor === String) {
+      return el.getAttribute(splitName);
+    }
+    return el.getAttribute(splitName[0])[splitName[1]];
+  }
+  return el.getAttribute(name);
+};
+
+/**
+ * Set component property using encoded component name + component property name with a
+ * delimiter.
+ */
+const setComponentProperty = function(el, name, value, delimiter) {
+  var splitName;
+  delimiter = delimiter || ".";
+  if (name.indexOf(delimiter) !== -1) {
+    splitName = getComponentPropertyPath(name, delimiter);
+    if (splitName.constructor === String) {
+      el.setAttribute(splitName, value);
+    } else {
+      el.setAttribute(splitName[0], splitName[1], value);
+    }
+    return;
+  }
+  el.setAttribute(name, value);
+};
+
 var splitCache = {};
 
 var TYPE_COLOR = "color";
