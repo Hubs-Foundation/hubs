952b0bf86ddd180664e0977fee2d06c402a7b146
Nguyễn Hữu Minh Khai <nguyenhuuminhkhai@gmail.com>
2022-05-24 18:06:12 +0700

    set https://api.larchiveum.link

--- src/utilities/constants.js
+++ src/utilities/constants.js
@@ -2,11 +2,11 @@ const expireCookies = 7;
 
 // const API_ROOT = "http://localhost:7500";
 // const ENV = "http://localhost:7500";
-const API_ROOT = "https://dev-api.sectic.com/larchiveum";
-const ENV = "https://dev-api.sectic.com/larchiveum";
+const API_ROOT = "https://api.larchiveum.link";
+const ENV = "https://api.larchiveum.link";
 
-//const APP_ROOT = "https://larchiveum.link";
-const APP_ROOT = "https://hubs.local:8080";
+const APP_ROOT = "https://larchiveum.link";
+//const APP_ROOT = "https://hubs.local:8080";
 
 const naverApp = {
   clientID: "pSh1Vk5d8Df3XZAURRC5",
