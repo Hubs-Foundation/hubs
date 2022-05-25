097bb6c6029ca4e1a418ef114760a8f895bc0f16
Meng Zhang <meng@pixelshift.ai>
2022-05-25 17:05:07 +0800

    Ignore displayName regex check

--- src/storage/store.js
+++ src/storage/store.js
@@ -388,6 +388,10 @@ export default class Store extends EventTarget {
     // Cleanup unsupported properties
     if (!valid) {
       errors.forEach(error => {
+        // Ignore jsonschema error for displayName, as jsonschema validator don't enable 'unicode'
+        // flag for RegExp constructor before 1.2.9.
+        if (error.property === "instance.profile.displayName") return;
+
         console.error(`Removing invalid preference from store: ${error.message}`);
         delete error.instance[error.argument];
       });
