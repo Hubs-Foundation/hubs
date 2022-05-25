b23a2c3a6fabc7ddda6562f6fa00ac7b69d05488
Geng Tan <geng.tan@limeade.com>
2022-05-25 11:27:03 -0400

    turkey-docker-update

--- scripts/docker/run.sh
+++ scripts/docker/run.sh
@@ -1,14 +1,14 @@
 
-turkeyCfg_postgrest_server=""
-turkeyCfg_thumbnail_server="nearspark.reticulum.io"
-turkeyCfg_base_assets_path="https://$SUB_DOMAIN-assets.$DOMAIN/hubs/"
-turkeyCfg_non_cors_proxy_domains="$SUB_DOMAIN.$DOMAIN,$SUB_DOMAIN-assets.$DOMAIN"
-turkeyCfg_reticulum_server="$SUB_DOMAIN.$DOMAIN"
-turkeyCfg_cors_proxy_server="$SUB_DOMAIN-cors.$DOMAIN"
-turkeyCfg_ga_tracking_id=""
-turkeyCfg_shortlink_domain="$SUB_DOMAIN.$DOMAIN"
-turkeyCfg_ita_server=""
-turkeyCfg_sentry_dsn=""
+export turkeyCfg_postgrest_server=""
+export turkeyCfg_thumbnail_server="nearspark.reticulum.io"
+export turkeyCfg_base_assets_path="https://$SUB_DOMAIN-assets.$DOMAIN/hubs/"
+export turkeyCfg_non_cors_proxy_domains="$SUB_DOMAIN.$DOMAIN,$SUB_DOMAIN-assets.$DOMAIN"
+export turkeyCfg_reticulum_server="$SUB_DOMAIN.$DOMAIN"
+export turkeyCfg_cors_proxy_server="$SUB_DOMAIN-cors.$DOMAIN"
+export turkeyCfg_ga_tracking_id=""
+export turkeyCfg_shortlink_domain="$SUB_DOMAIN.$DOMAIN"
+export turkeyCfg_ita_server=""
+export turkeyCfg_sentry_dsn=""
 
 find /www/hubs/ -type f -name *.html -exec sed -i "s/{{rawhubs-base-assets-path}}\//https:\/\/${SUB_DOMAIN}-assets.${DOMAIN}\/hubs\//g" {} \;           
 find /www/hubs/ -type f -name *.html -exec sed -i "s/{{rawhubs-base-assets-path}}/https:\/\/${SUB_DOMAIN}-assets.${DOMAIN}\/hubs\//g" {} \; 
