a0a869fc7b655c9aee6a6b59c76234499ce3813c
Orics <nguyenngockhai25@gmail.com>
2022-06-07 14:09:36 +0700

    change root URL

--- src/utilities/constants.js
+++ src/utilities/constants.js
@@ -2,7 +2,7 @@ const expireCookies = 7;
 
 // const API_ROOT = "http://localhost:7500";
 // const ENV = "http://localhost:7500";
-const API_ROOT = "https://api.larchiveum.link";
+//const API_ROOT = "https://api.larchiveum.link";
 const ENV = "https://api.larchiveum.link";
 
 // const APP_ROOT = "https://hubs.local:8080";
