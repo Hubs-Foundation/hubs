9b6fb1139ff97e4e447a7caa3058088353578be6
netpro2k <netpro2k@gmail.com>
2022-06-06 23:39:41 -0700

    Factor out RenderTargetRecorder

--- src/systems/bit-camera-system.js
+++ src/systems/bit-camera-system.js
@@ -1,12 +1,8 @@
 import { defineQuery, enterQuery, exitQuery, hasComponent } from "bitecs";
-import { CameraTool, Interacted, TextButton } from "../bit-components";
-import { addAndArrangeMedia, addMedia, pixelsToPNG } from "../utils/media-utils";
+import { CameraTool, Interacted } from "../bit-components";
+import { addMedia, pixelsToPNG } from "../utils/media-utils";
+import { RenderTargetRecorder } from "../utils/render-target-recorder";
 import { SOUND_CAMERA_TOOL_COUNTDOWN, SOUND_CAMERA_TOOL_TOOK_SNAPSHOT } from "./sound-effects-system";
-
-const { detect } = require("detect-browser");
-const browser = detect();
-
-const isFirefox = browser.name === "firefox";
 
 // Prefer h264 if available due to faster decoding speec on most platforms
 const videoCodec = ["h264", "vp9,opus", "vp8,opus", "vp9", "vp8"].find(
@@ -37,82 +33,14 @@ const CAMERA_STATE = {
 const CAPTURE_DURATIONS = [3, 7, 15, 30, 60];
 
 const renderTargets = new Map();
-const videoStates = new Map();
-
-function blitFramebuffer(renderer, src, srcX0, srcY0, srcX1, srcY1, dest, dstX0, dstY0, dstX1, dstY1) {
-  const gl = renderer.getContext();
-
-  // Copies from one framebuffer to another. Note that at the end of this function, you need to restore
-  // the original framebuffer via setRenderTarget
-  const srcFramebuffer = renderer.properties.get(src).__webglFramebuffer;
-  const destFramebuffer = renderer.properties.get(dest).__webglFramebuffer;
-
-  if (srcFramebuffer && destFramebuffer) {
-    gl.bindFramebuffer(gl.READ_FRAMEBUFFER, srcFramebuffer);
-    gl.bindFramebuffer(gl.DRAW_FRAMEBUFFER, destFramebuffer);
-
-    if (gl.checkFramebufferStatus(gl.READ_FRAMEBUFFER) === gl.FRAMEBUFFER_COMPLETE) {
-      gl.blitFramebuffer(srcX0, srcY0, srcX1, srcY1, dstX0, dstY0, dstX1, dstY1, gl.COLOR_BUFFER_BIT, gl.LINEAR);
-    }
-  }
-}
-
-function createVideoState() {
-  const videoCanvas = document.createElement("canvas");
-  videoCanvas.width = CAPTURE_WIDTH;
-  videoCanvas.height = CAPTURE_HEIGHT;
-  const videoContext = videoCanvas.getContext("2d");
-  const videoImageData = videoContext.createImageData(CAPTURE_WIDTH, CAPTURE_HEIGHT);
-  const videoPixels = new Uint8Array(CAPTURE_WIDTH * CAPTURE_HEIGHT * 4);
-  videoImageData.data.set(videoPixels);
-
-  // Create a separate render target for video because we need to flip (and sometimes downscale) it before
-  // encoding it to video.
-  const videoRenderTarget = new THREE.WebGLRenderTarget(CAPTURE_WIDTH, CAPTURE_HEIGHT, {
-    format: THREE.RGBAFormat,
-    minFilter: THREE.LinearFilter,
-    magFilter: THREE.NearestFilter,
-    encoding: THREE.sRGBEncoding,
-    depth: false,
-    stencil: false
-  });
-
-  const renderer = AFRAME.scenes[0].renderer;
-  const tmpRenderTarget = renderer.getCurrentRenderTarget();
-  // Used to set up framebuffer in three.js as a side effect
-  renderer.setRenderTarget(videoRenderTarget);
-  renderer.setRenderTarget(tmpRenderTarget);
-
-  return { videoCanvas, videoContext, videoImageData, videoPixels, videoRenderTarget };
-}
+const videoRecorders = new Map();
 
 function clicked(eid) {
   return hasComponent(APP.world, Interacted, eid);
 }
 
-function createStream(videoCanvas) {
-  const stream = videoCanvas.captureStream(VIDEO_FPS);
-  console.log("stream", stream);
-
-  // This adds hacks for current browser issues with media recordings when audio tracks are muted or missing.
-  const attachBlankAudioIfNeeded = () => {
-    // Chrome has issues when the audio tracks are silent so we only do this for FF.
-    // https://bugs.chromium.org/p/chromium/issues/detail?id=1223382
-    if (isFirefox) {
-      // FF 73+ seems to fail to decode videos with no audio track, so we always include a silent track.
-      const context = THREE.AudioContext.getContext();
-      const oscillator = context.createOscillator();
-      const gain = context.createGain();
-      const destination = context.createMediaStreamDestination();
-      gain.gain.setValueAtTime(0.0001, context.currentTime);
-      oscillator.connect(destination);
-      gain.connect(destination);
-      stream.addTrack(destination.stream.getAudioTracks()[0]);
-    }
-  };
-
-  const captureAudio = false;
-
+function createRecorder(captureAudio) {
+  let srcAudioTrack;
   // NOTE: if we don't have a self audio track, we can end up generating an empty video (browser bug?)
   // if no audio comes through on the listener source. (Eg the room is otherwise silent.)
   // So for now, if we don't have a track, just disable audio capture.
@@ -124,123 +52,63 @@ function createStream(videoCanvas) {
       APP.audioListener.getInput().connect(destination);
     }
     context.createMediaStreamSource(APP.dialog._micProducer?.track).connect(destination);
-    const audio = destination.stream.getAudioTracks()[0];
-    stream.addTrack(audio);
+    srcAudioTrack = destination.stream.getAudioTracks()[0];
+  }
+
+  return new RenderTargetRecorder(
+    AFRAME.scenes[0].renderer,
+    videoMimeType,
+    CAPTURE_WIDTH,
+    CAPTURE_HEIGHT,
+    VIDEO_FPS,
+    srcAudioTrack
+  );
+}
+
+function endRecording(world, eid, cancel) {
+  const recorder = videoRecorders.get(eid);
+  if (cancel) {
+    recorder.cancel();
   } else {
-    attachBlankAudioIfNeeded();
+    recorder.save().then(file => {
+      const { entity, orientation } = addMedia(file, "#interactable-media", undefined, "video-camera", false);
+
+      const cameraObj = world.eid2obj.get(eid);
+      entity.object3D.position.copy(cameraObj.localToWorld(new THREE.Vector3(0, -0.5, 0)));
+
+      AFRAME.scenes[0].systems["hubs-systems"].soundEffectsSystem.playSoundOneShot(SOUND_CAMERA_TOOL_TOOK_SNAPSHOT);
+
+      // const { entity, orientation } = addAndArrangeMedia(
+      //   this.el,
+      //   new File([blob], "capture", { type: mimeType.split(";")[0] }), // Drop codec
+      //   "video-camera",
+      //   this.localSnapCount,
+      //   !!this.playerIsBehindCamera
+      // );
+
+      // entity.addEventListener(
+      //   "video-loaded",
+      //   () => {
+      //     // If we were recording audio, then pause the video immediately after starting.
+
+      //     // Or, to limit the # of concurrent videos playing, if it was a short clip, let it loop
+      //     // a few times and then pause it.
+      //     if (captureAudio || recordingDuration <= MAX_DURATION_TO_LIMIT_LOOPS * 1000) {
+      //       setTimeout(() => {
+      //         if (!NAF.utils.isMine(entity) && !NAF.utils.takeOwnership(entity)) return;
+      //         entity.components["media-video"].tryUpdateVideoPlaybackState(true);
+      //       }, captureAudio ? 0 : recordingDuration * VIDEO_LOOPS + 100);
+      //     }
+      //   },
+      //   { once: true }
+      // );
+
+      // this.localSnapCount++;
+
+      // orientation.then(() => this.el.sceneEl.emit("object_spawned", { objectType: ObjectTypes.CAMERA }));
+    });
   }
-  return stream;
-}
-
-function beginRecording(world, eid) {
-  const videoState = createVideoState();
-  videoStates.set(eid, videoState);
-
-  // const recorder = new RenderTargetRecorder(renderer, srcRenderTarget, srcAudioTrack)
-  // recorder.start()
-  // recorder.captureFrame()
-  // recorder.stop()
-
-  videoState.videoRecorder = new MediaRecorder(createStream(videoState.videoCanvas), { mimeType: videoMimeType });
-
-  const chunks = [];
-  const recordingStartTime = performance.now();
-
-  videoState.videoRecorder.ondataavailable = e => chunks.push(e.data);
-
-  videoState.videoRecorder.onstop = async () => {
-    console.log("onstop");
-
-    if (chunks.length === 0) return;
-    const mimeType = chunks[0].type;
-    const recordingDuration = performance.now() - recordingStartTime;
-    const blob = new Blob(chunks, { type: mimeType });
-    const file = new File([blob], "capture", { type: mimeType.split(";")[0] }); // Drop codec
-    console.log(chunks, blob, file);
-    chunks.length = 0;
-
-    const { entity, orientation } = addMedia(file, "#interactable-media", undefined, "video-camera", false);
-
-    const cameraObj = world.eid2obj.get(eid);
-    entity.object3D.position.copy(cameraObj.localToWorld(new THREE.Vector3(0, -0.5, 0)));
-
-    AFRAME.scenes[0].systems["hubs-systems"].soundEffectsSystem.playSoundOneShot(SOUND_CAMERA_TOOL_TOOK_SNAPSHOT);
-
-    // const { entity, orientation } = addAndArrangeMedia(
-    //   this.el,
-    //   new File([blob], "capture", { type: mimeType.split(";")[0] }), // Drop codec
-    //   "video-camera",
-    //   this.localSnapCount,
-    //   !!this.playerIsBehindCamera
-    // );
-
-    entity.addEventListener(
-      "video-loaded",
-      () => {
-        // If we were recording audio, then pause the video immediately after starting.
-
-        // Or, to limit the # of concurrent videos playing, if it was a short clip, let it loop
-        // a few times and then pause it.
-        if (captureAudio || recordingDuration <= MAX_DURATION_TO_LIMIT_LOOPS * 1000) {
-          setTimeout(() => {
-            if (!NAF.utils.isMine(entity) && !NAF.utils.takeOwnership(entity)) return;
-            entity.components["media-video"].tryUpdateVideoPlaybackState(true);
-          }, captureAudio ? 0 : recordingDuration * VIDEO_LOOPS + 100);
-        }
-      },
-      { once: true }
-    );
-
-    // this.localSnapCount++;
-
-    // orientation.then(() => this.el.sceneEl.emit("object_spawned", { objectType: ObjectTypes.CAMERA }));
-  };
-
-  videoState.videoRecorder.start();
-  APP.hubChannel.beginRecording();
-}
-
-function copyVideoFrame(_world, eid) {
-  const renderer = AFRAME.scenes[0].renderer;
-  const srcRenterTarget = renderTargets.get(eid);
-  const videoState = videoStates.get(eid);
-
-  blitFramebuffer(
-    renderer,
-    srcRenterTarget,
-    0,
-    0,
-    RENDER_WIDTH,
-    RENDER_HEIGHT,
-    videoState.videoRenderTarget,
-    0,
-    CAPTURE_HEIGHT,
-    CAPTURE_WIDTH,
-    0
-  );
-  renderer.readRenderTargetPixels(
-    videoState.videoRenderTarget,
-    0,
-    0,
-    CAPTURE_WIDTH,
-    CAPTURE_HEIGHT,
-    videoState.videoPixels
-  );
-  videoState.videoImageData.data.set(videoState.videoPixels);
-  videoState.videoContext.putImageData(videoState.videoImageData, 0, 0);
-}
-
-function endRecording(_world, eid, cancel) {
-  const videoState = videoStates.get(eid);
-  if (videoState.videoRecorder) {
-    if (cancel) {
-      videoState.videoRecorder.onstop = () => {};
-    }
-    console.log("calling stop");
-    videoState.videoRecorder.stop();
-    console.log("called stop");
-  }
-  videoStates.delete(eid);
+  videoRecorders.delete(eid);
   APP.hubChannel.endRecording();
 }
 
@@ -404,10 +272,14 @@ export function cameraSystem(world) {
         CameraTool.state[camera] = CAMERA_STATE.IDLE;
       } else if (world.time.elapsed >= CameraTool.snapTime[camera]) {
         if (CameraTool.state[camera] === CAMERA_STATE.COUNTDOWN_VIDEO) {
-          CameraTool.state[camera] = CAMERA_STATE.RECORDING_VIDEO;
-          // setup video recording
-          beginRecording(world, camera);
           CameraTool.snapTime[camera] = world.time.elapsed + CAPTURE_DURATIONS[CameraTool.captureDurIdx[camera]] * 1000;
+          CameraTool.state[camera] = CAMERA_STATE.RECORDING_VIDEO;
+
+          const recorder = createRecorder(false);
+          videoRecorders.set(camera, recorder);
+          recorder.start();
+
+          APP.hubChannel.beginRecording();
         } else {
           CameraTool.state[camera] = CAMERA_STATE.SNAP_PHOTO;
         }
@@ -436,16 +308,16 @@ export function cameraSystem(world) {
     } else if (CameraTool.state[camera] === CAMERA_STATE.RECORDING_VIDEO) {
       if (clicked(CameraTool.cancelRef[camera])) {
         console.log("Cancel video");
-        CameraTool.state[camera] = CAMERA_STATE.IDLE;
         endRecording(world, camera, true);
+        CameraTool.state[camera] = CAMERA_STATE.IDLE;
       } else if (world.time.elapsed >= CameraTool.snapTime[camera]) {
         console.log("done recording");
-        CameraTool.state[camera] = CAMERA_STATE.IDLE;
         endRecording(world, camera, false);
+        CameraTool.state[camera] = CAMERA_STATE.IDLE;
       } else {
         // TODO should only copy at VIDEO_FPS
         // also use CanvasCaptureMediaStreamTrack to sync when available
-        copyVideoFrame(world, camera);
+        videoRecorders.get(camera).captureFrame(renderTargets.get(camera));
       }
     }
 
