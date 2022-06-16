f6e18e12c9ff8c696b4f186f4c5758a1abcea344
robert patterson <robpattersonx@gmail.com>
2022-06-16 16:47:36 -0700

    more

--- src/react-components/home/CreateEventButton.js
+++ src/react-components/home/CreateEventButton.js
@@ -12,8 +12,11 @@ export function CreateEventButton() {
 
 
   return (
-    <Button style={{backgroundColor: "#ff1100" ,  borderRadius: "30px", border: "none" }} as="a" href="/tokens">
-    <FormattedMessage id="home-page.have-code" defaultMessage="Create Event" />
+    <Button style={{backgroundColor: "#ff1100" ,  borderRadius: "30px", border: "none" }}
+      thick={breakpoint === "sm" || breakpoint === "md"}
+      xl={breakpoint !== "sm" && breakpoint !== "md"}
+      preset="landing"as="a" href="/tokens">
+    <FormattedMessage id="create-room-button" defaultMessage="Create Event" />
   </Button>
   );
 }
