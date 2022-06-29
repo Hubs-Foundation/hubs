09a5594e18a1c07add229c39b808bf044cde7092
Nick Grato <nickgrato@ngrato-27545.local>
2022-06-29 13:54:05 -0700

    fixing build errors

--- src/react-components/banner/Banner.js
+++ src/react-components/banner/Banner.js
@@ -113,8 +113,8 @@ const Banner = () => {
   const Label = () => {
     return (
       <>
-        I’m okay with Mozilla handling my info as explained in this{" "}
-        <a href="https://github.com/mozilla/hubs/blob/master/PRIVACY.md" target="_blank">
+        I'm okay with Mozilla handling my info as explained in this{" "}
+        <a href="https://github.com/mozilla/hubs/blob/master/PRIVACY.md" rel="noopener noreferrer" target="_blank">
           Privacy Notice
         </a>
       </>
@@ -137,11 +137,16 @@ const Banner = () => {
   const Success = () => {
     return (
       <div className={styles.message_wrapper}>
-        <img src={cone} alt="celebrate" className={styles.success_image} />
+        <img src={cone} className={styles.success_image} />
         <div>
-          <h3 className={styles.message_title}>You're on the list</h3>
+          <h3 className={styles.message_title}>
+            <FormattedMessage defaultMessage="You're on the list" id="banner.success-title" />
+          </h3>
           <p className={styles.message_body}>
-            Keep an eye out for product updates and an invite to join us as a tester in August.
+            <FormattedMessage
+              defaultMessage="Keep an eye out for product updates and an invite to join us as a tester in August."
+              id="banner.success-message"
+            />
           </p>
           <Button
             preset="primary"
@@ -149,7 +154,7 @@ const Banner = () => {
               setIsExpanded(false);
             }}
           >
-            Got it
+            <FormattedMessage defaultMessage="Got it" id="banner.success-cta" />
           </Button>
         </div>
       </div>
@@ -164,13 +169,29 @@ const Banner = () => {
     return (
       <div className={styles.message_wrapper}>
         <div>
-          <img src={warning_icon} alt="icon" className={styles.error_icon} />
+          <img src={warning_icon} className={styles.error_icon} />
         </div>
         <div>
-          <h3 className={styles.message_title}>We ran into a problem</h3>
+          <h3 className={styles.message_title}>
+            <FormattedMessage defaultMessage="We ran into a problem" id="banner.error-title" />
+          </h3>
+
           <p className={styles.message_body}>
-            Sorry, we were unable to add you to the mailing list, please try again later. If the problem persists please
-            reach out on our <a href="https://discord.com/invite/dFJncWwHun" target='_blank'>Discord</a>.
+            <FormattedMessage
+              defaultMessage="{message}"
+              id="banner.error-body"
+              values={{
+                message: (
+                  <>
+                    Sorry, we were unable to add you to the mailing list, please try again later. If the problem
+                    persists please reach out on our{" "}
+                    <a href="https://discord.com/invite/dFJncWwHun" rel="noopener noreferrer" target="_blank">
+                      Discord
+                    </a>.
+                  </>
+                )
+              }}
+            />
           </p>
           <Button
             preset="primary"
@@ -178,7 +199,7 @@ const Banner = () => {
               setIsExpanded(false);
             }}
           >
-            Got it
+            <FormattedMessage defaultMessage="Got it" id="banner.error-cta" />
           </Button>
         </div>
       </div>
@@ -190,8 +211,12 @@ const Banner = () => {
       <div className={styles.banner_container}>
         {/* BRANDING  */}
         <div className={styles.branding_container}>
-          <h2>Join the next evolution of Hubs!</h2>
-          <p>Be the first to get a sneak peek!</p>
+          <h2>
+            <FormattedMessage defaultMessage="Join the next evolution of Hubs!" id="banner.title" />
+          </h2>
+          <p>
+            <FormattedMessage defaultMessage="Be the first to get a sneak peek!" id="banner.subtitle" />
+          </p>
         </div>
 
         <Button
@@ -200,7 +225,8 @@ const Banner = () => {
             setIsExpanded(true);
           }}
         >
-          Find out More<span className={styles.down_arrow}>
+          <FormattedMessage defaultMessage="Find out More" id="banner.expand-cta" />
+          <span className={styles.down_arrow}>
             <ArrowIcon />
           </span>
         </Button>
@@ -211,7 +237,9 @@ const Banner = () => {
         <div className={styles.expand_wrapper}>
           <div className={styles.expand_container}>
             <div className={styles.expand_header}>
-              <h2 className={styles.expand_title}>Join the next evolution of Hubs!</h2>
+              <h2 className={styles.expand_title}>
+                <FormattedMessage defaultMessage="Join the next evolution of Hubs!" id="banner.expand_title" />
+              </h2>
               <CloseButton
                 onClick={() => {
                   setIsExpanded(false);
@@ -222,11 +250,23 @@ const Banner = () => {
             <div className={styles.expand_contents}>
               {/* MESSAGING  */}
               <div className={styles.expand_messaging}>
-                <p>We're working on a new service that makes it easier than ever to deploy a Hub of your own.</p>
-                <p>
-                  Sign up here to be the first to know about our new service, as well as the latest Hubs news, product
-                  features and offerings. We can't wait to show you what we've been working on!
-                </p>
+                <FormattedMessage
+                  defaultMessage="{message}"
+                  id="banner.expand-body"
+                  values={{
+                    message: (
+                      <>
+                        <p>
+                          We're working on a new service that makes it easier than ever to deploy a Hub of your own.
+                        </p>
+                        <p>
+                          Sign up here to be the first to know about our new service, as well as the latest Hubs news,
+                          product features and offerings. We can't wait to show you what we've been working on!
+                        </p>
+                      </>
+                    )
+                  }}
+                />
               </div>
 
               {/* FORM  */}
@@ -265,14 +305,14 @@ const Banner = () => {
                     {/* ACTIONS  */}
                     <div className={styles.expand_actions}>
                       <Button type="submit" preset="primary" disabled={!confirm}>
-                        Join the Mailing List
+                        <FormattedMessage defaultMessage="Join the Mailing List" id="banner.form-cta" />
                       </Button>
                       <Button
                         onClick={() => {
                           setIsExpanded(false);
                         }}
                       >
-                        Not Interested
+                        <FormattedMessage defaultMessage="Not Interested" id="banner.form-decline" />
                       </Button>
                     </div>
                   </div>
