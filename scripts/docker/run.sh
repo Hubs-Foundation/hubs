f0515748d0f30c6193186c5e8c0aa33fa2bb5308
Geng Tan <tanfarming@hotmail.com>
2022-08-10 16:11:52 -0400

    Update run.sh

--- scripts/docker/run.sh
+++ scripts/docker/run.sh
@@ -1,14 +1,10 @@
 
-export turkeyCfg_postgrest_server=""
-export turkeyCfg_thumbnail_server="nearspark.reticulum.io"
-export turkeyCfg_base_assets_path="https://$SUB_DOMAIN-assets.$DOMAIN/hubs/"
-export turkeyCfg_non_cors_proxy_domains="$SUB_DOMAIN.$DOMAIN,$SUB_DOMAIN-assets.$DOMAIN"
-export turkeyCfg_reticulum_server="$SUB_DOMAIN.$DOMAIN"
-export turkeyCfg_cors_proxy_server="$SUB_DOMAIN-cors.$DOMAIN"
-export turkeyCfg_ga_tracking_id=""
-export turkeyCfg_shortlink_domain="$SUB_DOMAIN.$DOMAIN"
-export turkeyCfg_ita_server=""
-export turkeyCfg_sentry_dsn=""
+turkeyCfg_thumbnail_server="${turkeyCfg_thumbnail_server:nearspark.reticulum.io}"
+turkeyCfg_base_assets_path="${turkeyCfg_base_assets_path:https://$SUB_DOMAIN-assets.$DOMAIN/hubs/}"
+turkeyCfg_non_cors_proxy_domains="${turkeyCfg_non_cors_proxy_domains:$SUB_DOMAIN.$DOMAIN,$SUB_DOMAIN-assets.$DOMAIN}"
+turkeyCfg_reticulum_server="${turkeyCfg_reticulum_server:$SUB_DOMAIN.$DOMAIN}"
+turkeyCfg_cors_proxy_server="${turkeyCfg_cors_proxy_server:$SUB_DOMAIN-cors.$DOMAIN}"
+turkeyCfg_shortlink_domain="${turkeyCfg_shortlink_domain:$SUB_DOMAIN.$DOMAIN}"
 
 find /www/hubs/ -type f -name *.html -exec sed -i "s/{{rawhubs-base-assets-path}}\//https:\/\/${SUB_DOMAIN}-assets.${DOMAIN}\/hubs\//g" {} \;           
 find /www/hubs/ -type f -name *.html -exec sed -i "s/{{rawhubs-base-assets-path}}/https:\/\/${SUB_DOMAIN}-assets.${DOMAIN}\/hubs\//g" {} \; 
