4816d40957910743b0f47153e9f8b8bffe8289b9
netpro2k <netpro2k@gmail.com>
2022-08-17 19:41:11 -0700

    Minor tweaks to video menu layout

--- src/prefabs/video-menu.tsx
+++ src/prefabs/video-menu.tsx
@@ -5,12 +5,15 @@ import { createElementEntity, createRef } from "../utils/jsx-entity";
 
 function Slider({ trackRef, headRef, ...props }: any) {
   return (
-    <entity {...props}>
+    <entity {...props} name="Slider">
       <entity
-        name="Track"
+        name="Slider:Track"
         video-menu-item
         object3D={
-          new Mesh(new PlaneBufferGeometry(1.0, 0.1), new MeshBasicMaterial({ opacity: 0.50, color: 0x333333, transparent: true }))
+          new Mesh(
+            new PlaneBufferGeometry(1.0, 0.05),
+            new MeshBasicMaterial({ opacity: 0.5, color: 0x000000, transparent: true })
+          )
         }
         cursor-raycastable
         remote-hover-target
@@ -18,23 +21,34 @@ function Slider({ trackRef, headRef, ...props }: any) {
         holdable-button
         ref={trackRef}
       >
-        <entity object3D={new Mesh(new BoxBufferGeometry(0.1, 0.1, 0.05), new MeshBasicMaterial())} ref={headRef} />
+        <entity
+          name="Slider:Head"
+          object3D={new Mesh(new BoxBufferGeometry(0.05, 0.05, 0.05), new MeshBasicMaterial())}
+          ref={headRef}
+        />
       </entity>
     </entity>
   );
 }
 
 export function VideoMenuPrefab() {
-  const uiZ = 0.01;
+  const uiZ = 0.001;
   const playButtonRef = createRef();
   const timeLabelRef = createRef();
   const headRef = createRef();
   const trackRef = createRef();
+  const halfHeight = 9 / 16 / 2;
+
   return (
     <entity video-menu={{ playButtonRef, timeLabelRef, headRef, trackRef }}>
       <Button ref={playButtonRef} video-menu-item position={[0, 0, uiZ]} width={0.3} height={0.2} text="Play" />
-      <Label ref={timeLabelRef} scale={[1, 1, 1]} position={[-0.2, -0.15, uiZ]} />
-      <Slider trackRef={trackRef} headRef={headRef} position={[0, -0.22, uiZ]} />
+      <Label
+        text={{ anchorY: "top", anchorX: "right" }}
+        ref={timeLabelRef}
+        scale={[0.5, 0.5, 0.5]}
+        position={[0.5 - 0.02, halfHeight - 0.02, uiZ]}
+      />
+      <Slider trackRef={trackRef} headRef={headRef} position={[0, -halfHeight + 0.025, uiZ]} />
     </entity>
   );
 }
