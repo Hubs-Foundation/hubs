10680ea7fd443879e39864a84efd78743630cf27
John Shaughnessy <john@johnshaughnessy.com>
2022-08-30 16:04:52 -0400

    Remove unused code

--- src/utils/create-fail-media.js
+++
@@ -1,8 +0,0 @@
-import { addEntity } from "bitecs";
-import { addObject3DComponent } from "../utils/jsx-entity";
-
-export function createFailMedia(world) {
-  const eid = addEntity(world);
-  addObject3DComponent(world, eid, new THREE.Mesh(new THREE.BoxGeometry(), new THREE.MeshBasicMaterial()));
-  return eid;
-}
