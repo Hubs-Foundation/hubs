2c291b3656b48b673b96597d18cafb9f5bb0154e
John Shaughnessy <john@johnshaughnessy.com>
2022-08-30 13:54:38 -0400

    Use MediaParams in inflateMediaLoader

--- src/inflators/media-loader.ts
+++ src/inflators/media-loader.ts
@@ -1,12 +1,14 @@
 import { addComponent } from "bitecs";
+import { HubsWorld } from "../app";
 import { MediaLoader } from "../bit-components";
 import { MEDIA_LOADER_FLAGS } from "../bit-systems/media-loading";
+import { MediaParams } from "../prefabs/media";
 
-export function inflateMediaLoader(world, eid, { src, recenter, resize }) {
+export function inflateMediaLoader(world: HubsWorld, eid: number, { src, recenter, resize }: MediaParams) {
   addComponent(world, MediaLoader, eid);
   let flags = 0;
   if (recenter) flags |= MEDIA_LOADER_FLAGS.RECENTER;
   if (resize) flags |= MEDIA_LOADER_FLAGS.RESIZE;
   MediaLoader.flags[eid] = flags;
-  MediaLoader.src[eid] = APP.getSid(src);
+  MediaLoader.src[eid] = APP.getSid(src)!;
 }
