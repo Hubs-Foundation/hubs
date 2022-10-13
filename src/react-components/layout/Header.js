1c3fe67c6a3152057ba7adb673eeaa3383f6f637
Rupert Rawnsley <rupert.rawnsley@gmail.com>
2022-10-13 09:50:23 +0100

    Added missing include in Header

--- src/react-components/layout/Header.js
+++ src/react-components/layout/Header.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useContext } from "react";
 import PropTypes from "prop-types";
 import { FormattedMessage } from "react-intl";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
