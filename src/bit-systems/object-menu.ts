ff76d6e5a43d7d9d8f7ac8a4c10600da37e16000
John Shaughnessy <john@johnshaughnessy.com>
2022-11-25 18:16:18 -0500

    Move menu to hovered ObjectMenuTargets

--- src/bit-systems/object-menu.ts
+++ src/bit-systems/object-menu.ts
@@ -1,23 +1,36 @@
 import { UserInputSystem } from "aframe";
-import { hasComponent } from "bitecs";
+import { defineQuery, hasComponent } from "bitecs";
 import type { HubsWorld } from "../app";
-import { Interacted, ObjectMenu } from "../bit-components";
-import { anyEntityWith } from "../utils/bit-utils";
+import { HoveredRemoteRight, Interacted, ObjectMenu, ObjectMenuTarget } from "../bit-components";
+import { anyEntityWith, findAncestorWithComponent } from "../utils/bit-utils";
 import type { EntityID } from "../utils/networking-types";
+import { setMatrixWorld } from "../utils/three-utils";
 
 function clicked(world: HubsWorld, eid: EntityID) {
   return hasComponent(world, Interacted, eid);
 }
 
-let menu: EntityID | null;
-export function objectMenuSystem(world: HubsWorld, userinput: UserInputSystem) {
-  if (!menu) {
-    menu = anyEntityWith(world, ObjectMenu);
-  }
-  if (!menu) {
-    return; // TODO: Fix initialization to be deterministic
+function objectMenuTarget(world: HubsWorld, menu: EntityID, sceneIsFrozen: boolean) {
+  if (!sceneIsFrozen) {
+    return 0;
   }
 
+  const hovered = hoveredQuery(world);
+  const target = hovered.find(eid => findAncestorWithComponent(world, ObjectMenuTarget, eid));
+  return target || ObjectMenu.targetRef[menu];
+}
+
+function moveToTarget(world: HubsWorld, menu: EntityID) {
+  const targetObj = world.eid2obj.get(ObjectMenu.targetRef[menu])!;
+  targetObj.updateMatrices();
+
+  const menuObj = world.eid2obj.get(menu)!;
+
+  // TODO: position the menu more carefully...
+  setMatrixWorld(menuObj, targetObj.matrixWorld);
+}
+
+function handleClicks(world: HubsWorld, menu: EntityID) {
   if (clicked(world, ObjectMenu.pinButtonRef[menu])) {
     console.log("Clicked pin");
   } else if (clicked(world, ObjectMenu.cameraFocusButtonRef[menu])) {
@@ -45,4 +58,47 @@ export function objectMenuSystem(world: HubsWorld, userinput: UserInputSystem) {
   } else if (clicked(world, ObjectMenu.scaleButtonRef[menu])) {
     console.log("Clicked scale");
   }
+}
+
+function render(world: HubsWorld, menu: EntityID, frozen: boolean) {
+  const visible = !!(ObjectMenu.targetRef[menu] && frozen);
+
+  const obj = world.eid2obj.get(ObjectMenu.pinButtonRef[menu])!.parent!;
+  obj.visible = visible;
+
+  [
+    ObjectMenu.pinButtonRef[menu],
+    ObjectMenu.cameraFocusButtonRef[menu],
+    ObjectMenu.cameraTrackButtonRef[menu],
+    ObjectMenu.removeButtonRef[menu],
+    ObjectMenu.dropButtonRef[menu],
+    ObjectMenu.inspectButtonRef[menu],
+    ObjectMenu.deserializeDrawingButtonRef[menu],
+    ObjectMenu.openLinkButtonRef[menu],
+    ObjectMenu.refreshButtonRef[menu],
+    ObjectMenu.cloneButtonRef[menu],
+    ObjectMenu.rotateButtonRef[menu],
+    ObjectMenu.mirrorButtonRef[menu],
+    ObjectMenu.scaleButtonRef[menu]
+  ].forEach(buttonRef => {
+    const buttonObj = world.eid2obj.get(buttonRef)!;
+    // Parent visibility doesn't block raycasting, so we must set each button to be invisible
+    // TODO: Ensure that children of invisible entities aren't raycastable
+    buttonObj.visible = visible;
+  });
+}
+
+const hoveredQuery = defineQuery([HoveredRemoteRight]);
+export function objectMenuSystem(world: HubsWorld, sceneIsFrozen: boolean, userinput: UserInputSystem) {
+  const menu = anyEntityWith(world, ObjectMenu) as EntityID | null;
+  if (!menu) {
+    return; // TODO: Fix initialization so that this is assigned via preload.
+  }
+
+  ObjectMenu.targetRef[menu] = objectMenuTarget(world, menu, sceneIsFrozen);
+  if (ObjectMenu.targetRef[menu]) {
+    moveToTarget(world, menu);
+    handleClicks(world, menu);
+  }
+  render(world, menu, sceneIsFrozen);
 }
