f025691a437fa9bb9139d935b7664c38a6c7fe6f
John Shaughnessy <john@johnshaughnessy.com>
2022-11-26 02:09:28 -0500

    Reimplement pinning

--- src/bit-components.js
+++ src/bit-components.js
@@ -174,6 +174,7 @@ EnvironmentSettings.map = new Map();
 // TODO: Store this data elsewhere, since only one or two will ever exist.
 export const ObjectMenu = defineComponent({
   pinButtonRef: Types.eid,
+  unpinButtonRef: Types.eid,
   cameraFocusButtonRef: Types.eid,
   cameraTrackButtonRef: Types.eid,
   removeButtonRef: Types.eid,
