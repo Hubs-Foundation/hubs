221d757cac619d021e37f899ea091cfab8207991
Takahiro <hogehoge@gachapin.jp>
2022-12-10 00:08:22 -0800

    Remove a compile warning

--- src/react-components/room/ExitedRoomScreenContainer.js
+++ src/react-components/room/ExitedRoomScreenContainer.js
@@ -2,13 +2,13 @@ import React from "react";
 import PropTypes from "prop-types";
 import configs from "../../utils/configs";
 import { ExitedRoomScreen } from "./ExitedRoomScreen";
-import constants from "../../constants";
+import { TERMS } from "../../constants";
 
 export function ExitedRoomScreenContainer({ reason }) {
   return (
     <ExitedRoomScreen
       showTerms={configs.feature("show_terms")}
-      termsUrl={configs.link("terms_of_use", constants.TERMS)}
+      termsUrl={configs.link("terms_of_use", TERMS)}
       showSourceLink={configs.feature("show_source_link")}
       reason={reason}
       contactEmail={configs.translation("contact-email")}
