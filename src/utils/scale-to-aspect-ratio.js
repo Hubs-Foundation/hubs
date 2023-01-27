f89c7e09d27b81fdf5e66d9e2aa290cb2fb84c0c
John Shaughnessy <john@johnshaughnessy.com>
2023-01-26 22:55:19 -0500

    Revert changes

--- src/utils/scale-to-aspect-ratio.js
+++ src/utils/scale-to-aspect-ratio.js
@@ -1,7 +1,6 @@
 export function scaleToAspectRatio(el, ratio) {
-  const obj = el.object3DMap.mesh;
   const width = Math.min(1.0, 1.0 / ratio);
   const height = Math.min(1.0, ratio);
-  obj.scale.set(width, height, 1);
-  obj.matrixNeedsUpdate = true;
+  el.object3DMap.mesh.scale.set(width, height, 1);
+  el.object3DMap.mesh.matrixNeedsUpdate = true;
 }
