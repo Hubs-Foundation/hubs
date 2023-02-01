b8f662500b39ea197095b6ea1d8bff2b3a78bb53
Manuel Martin <manmartin@mozilla.com>
2023-02-01 15:43:45 +0100

    Use job runner to handle the texture creation

--- src/bit-systems/particle-emitter.ts
+++ src/bit-systems/particle-emitter.ts
@@ -1,18 +1,17 @@
-import { defineQuery, enterQuery } from "bitecs";
+import { defineQuery, enterQuery, exitQuery } from "bitecs";
 import { ParticleEmitter } from "lib-hubs/packages/three-particle-emitter/lib/esm/index";
+import { Texture } from "three";
 
 import { HubsWorld } from "../app";
 import { ParticleEmitterTag } from "../bit-components";
+import { JobRunner } from "../utils/coroutine-utils";
 import { proxiedUrlFor } from "../utils/media-url-utils";
 import { resolveUrl, textureLoader } from "../utils/media-utils";
 
-const particleEmitterQuery = defineQuery([ParticleEmitterTag]);
-const particleEmitterEnterQuery = enterQuery(particleEmitterQuery);
-
-const setTexture = async (world: HubsWorld, eid: number) => {
+function* setTexture(world: HubsWorld, eid: number) {
   let src = APP.sid2str.get(ParticleEmitterTag.src[eid]);
 
-  const result = await resolveUrl(src);
+  const result: URL = yield resolveUrl(src);
   let canonicalUrl = result.origin;
 
   // handle protocol relative urls
@@ -23,18 +22,28 @@ const setTexture = async (world: HubsWorld, eid: number) => {
   // todo: we don't need to proxy for many things if the canonical URL has permissive CORS headers
   src = proxiedUrlFor(canonicalUrl);
 
-  const texture = await textureLoader.loadAsync(src);
+  const texture: Texture = yield textureLoader.loadAsync(src);
 
   const particleEmitter: ParticleEmitter = world.eid2obj.get(eid)! as ParticleEmitter;
   particleEmitter.material.uniforms.map.value = texture;
   particleEmitter.visible = true;
 
   ParticleEmitterTag.updateParticles[eid] = 1;
-};
+}
 
+const particleEmitterQuery = defineQuery([ParticleEmitterTag]);
+const particleEmitterEnterQuery = enterQuery(particleEmitterQuery);
+const particleEmitterExitQuery = exitQuery(particleEmitterQuery);
+
+const jobs = new JobRunner();
 export function particleEmitterSystem(world: HubsWorld) {
   particleEmitterEnterQuery(world).forEach(eid => {
-    setTexture(world, eid);
+    jobs.stop(eid);
+    jobs.add(eid, () => setTexture(world, eid));
+  });
+  particleEmitterExitQuery(world).forEach(function (eid) {
+    // Object3D resources release happens in remove-object3D-system
+    jobs.stop(eid);
   });
   particleEmitterQuery(world).forEach(eid => {
     const particleEmitter: ParticleEmitter = world.eid2obj.get(eid)! as ParticleEmitter;
@@ -48,5 +57,5 @@ export function particleEmitterSystem(world: HubsWorld) {
       particleEmitter.update(world.time.delta / 1000);
     }
   });
-  // The resources release happens in remove-object3D-system
+  jobs.tick();
 }
