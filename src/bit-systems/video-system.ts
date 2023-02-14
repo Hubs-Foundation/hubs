32dbe493a869627000c8d6f079f3074044ba9b8a
Manuel Martin <manmartin@mozilla.com>
2023-02-14 18:02:48 +0100

    Add mediavideo playback change component

--- src/bit-systems/video-system.ts
+++ src/bit-systems/video-system.ts
@@ -1,7 +1,7 @@
-import { defineQuery, enterQuery, hasComponent } from "bitecs";
+import { defineQuery, enterQuery, exitQuery, hasComponent, removeComponent } from "bitecs";
 import { Mesh, MeshStandardMaterial } from "three";
 import { HubsWorld } from "../app";
-import { MediaVideo, NetworkedVideo, Owned } from "../bit-components";
+import { MediaVideo, MediaVideoPlaybackChanged, NetworkedVideo, Owned } from "../bit-components";
 import { AudioSystem } from "../systems/audio-system";
 import { makeAudioSourceEntity } from "./audio-emitter-system";
 
@@ -9,26 +9,44 @@ enum Flags {
   PAUSED = 1 << 0
 }
 
+const video2audio = new Map<number, number>();
+
 const OUT_OF_SYNC_SEC = 5;
 const networkedVideoQuery = defineQuery([NetworkedVideo]);
 const mediaVideoQuery = defineQuery([MediaVideo]);
 const mediaVideoEnterQuery = enterQuery(mediaVideoQuery);
+const mediaVideoExitQuery = exitQuery(mediaVideoQuery);
+const mediaPlaybackUpdated = defineQuery([MediaVideo, MediaVideoPlaybackChanged]);
 export function videoSystem(world: HubsWorld, audioSystem: AudioSystem) {
-  mediaVideoEnterQuery(world).forEach(function (eid) {
-    const videoObj = world.eid2obj.get(eid) as Mesh;
+  mediaVideoEnterQuery(world).forEach(function (videoEid) {
+    const videoObj = world.eid2obj.get(videoEid) as Mesh;
     const video = (videoObj.material as MeshStandardMaterial).map!.image as HTMLVideoElement;
-    if (MediaVideo.autoPlay[eid]) {
+    if (MediaVideo.autoPlay[videoEid]) {
       video.play().catch(() => {
         // Need to deal with the fact play() may fail if user has not interacted with browser yet.
         console.error("Error auto-playing video.");
       });
     }
-    const audio = world.eid2obj.get(makeAudioSourceEntity(world, video, audioSystem))!;
+    const audioEid = makeAudioSourceEntity(world, video, audioSystem);
+    video2audio.set(videoEid, audioEid);
+    const audio = world.eid2obj.get(audioEid)!;
     videoObj.add(audio);
     // Note in media-video we call updateMatrixWorld here to force PositionalAudio's updateMatrixWorld to run even
     // if it has an invisible parent. We don't want to have invisible parents now.
   });
-
+  mediaVideoExitQuery(world).forEach(videoEid => {
+    video2audio.delete(videoEid);
+  });
+  mediaPlaybackUpdated(world).forEach(videoEid => {
+    const audio = video2audio.get(videoEid)!;
+    const videoEl = (world.eid2obj.get(videoEid) as any).material.map.image as HTMLVideoElement;
+    if (videoEl.paused) {
+      APP.isAudioPaused.add(audio);
+    } else {
+      APP.isAudioPaused.delete(audio);
+    }
+    removeComponent(world, MediaVideoPlaybackChanged, videoEid);
+  });
   networkedVideoQuery(world).forEach(function (eid) {
     const video = (world.eid2obj.get(eid) as any).material.map.image as HTMLVideoElement;
     if (hasComponent(world, Owned, eid)) {
