8514748b4873849640519b78d0e477d93bd6e30d
John Shaughnessy <john@johnshaughnessy.com>
2023-03-01 22:36:55 -0500

    Remove test function

--- src/hub.js
+++ src/hub.js
@@ -1393,10 +1393,6 @@ document.addEventListener("DOMContentLoaded", async () => {
     scene.emit("hub_updated", { hub });
   });
 
-  window.testChangeHost = () => {
-    hubPhxChannel.push("test_change_host", {});
-  };
-
   hubPhxChannel.on("host_changed", ({ host, port, turn }) => {
     console.log("Dialog host changed. Disconnecting from current host and connecting to the new one.", {
       hub_id: APP.hub.hub_id,
