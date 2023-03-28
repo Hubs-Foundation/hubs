0380c91d374d523306f983e2287885379cea3a7f
Brandon <brandon@brandons-mbp.lan>
2023-03-28 18:40:00 -0400

    fix error with download command

--- src/message-dispatch.js
+++ src/message-dispatch.js
@@ -10,7 +10,7 @@ import { LogMessageType } from "./react-components/room/ChatSidebar";
 import { createNetworkedEntity } from "./utils/create-networked-entity";
 import qsTruthy from "./utils/qs_truthy";
 import { add, testAsset, respawn } from "./utils/chat-commands";
-import {downloadSavedEntityStates} from "./utils/entity-state-utils"
+import downloadSavedEntityStates from "./utils/entity-state-utils";
 
 let uiRoot;
 // Handles user-entered messages
@@ -260,7 +260,7 @@ export default class MessageDispatch extends EventTarget {
         break;
       case "download":
         {
-          downloadSavedEntityStates(APP.hubChannel,);
+          downloadSavedEntityStates(APP.hubChannel);
         }
         break;
     }
