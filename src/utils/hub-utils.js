152d569f1adead8c26ee3cb856c2d0d7f6e041d8
Michael Morran <mmorran@mozilla.com>
2023-04-24 10:23:34 -0500

    fixing forced util

--- src/utils/hub-utils.js
+++ src/utils/hub-utils.js
@@ -55,7 +55,6 @@ export function isRoomOwner(clientId) {
 }
 
 export function isLockedDownDemoRoom() {
-  return true;
   const hubId = getCurrentHubId();
   if (configs.feature("is_locked_down_demo_room")) {
     const idArr = configs.feature("is_locked_down_demo_room").replace(/\s/g, "").split(",");
