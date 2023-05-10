4cb51dfba733c75090121dff80c863eb6853a03c
Nikki Sharpley <nsharpley@mozilla.com>
2023-05-10 14:33:06 -0400

    fix svg type for ts

--- src/react-components/misc/AppLogo.tsx
+++ src/react-components/misc/AppLogo.tsx
@@ -1,14 +1,14 @@
 import React from "react";
 
 import configs from "../../utils/configs";
-import { ReactComponent as HmcLogo } from "../icons/HmcLogo.svg";
+import HmcLogo from "../icons/HmcLogo.svg";
 import { isHmc } from "../../utils/isHmc";
 import { useLogo } from "../styles/theme";
 
-export function AppLogo({ className }:{ className?: string } ) {
+export function AppLogo({ className }: { className?: string }) {
   const logo = useLogo();
 
-  if (isHmc() && !logo ) {
+  if (isHmc() && !logo) {
     return <HmcLogo className="hmc-logo" />;
   } else {
     return <img className={className} alt={configs.translation("app-name")} src={logo} />;
