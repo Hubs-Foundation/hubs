1675dc2717d1b5f7c7de6f0ab28a8f350e13b95e
Nikki Sharpley <nsharpley@mozilla.com>
2023-05-16 11:21:21 -0400

    add comment

--- src/react-components/misc/AppLogo.tsx
+++ src/react-components/misc/AppLogo.tsx
@@ -7,6 +7,8 @@ import { useLogo } from "../styles/theme";
 
 export function AppLogo({ className }: { className?: string }) {
   const logo = useLogo();
+
+  // Display HMC logo if account and HMC and no custom logo is configured
   const shouldDisplayHmcLogo = isHmc() && !logo;
 
   return shouldDisplayHmcLogo ? (
