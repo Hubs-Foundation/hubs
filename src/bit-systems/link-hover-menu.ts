dae46ab1762c3ff90b3299d1f697e6dced716150
Manuel Martin <manmartin@mozilla.com>
2023-06-01 15:14:11 +0200

    Add support of link types in the link hover menu

--- src/bit-systems/link-hover-menu.ts
+++ src/bit-systems/link-hover-menu.ts
@@ -1,7 +1,11 @@
 import { defineQuery, enterQuery, entityExists } from "bitecs";
 import type { HubsWorld } from "../app";
-import { Interacted, Link, LinkHoverMenu, LinkHoverMenuItem, HoveredRemoteRight } from "../bit-components";
-import { anyEntityWith } from "../utils/bit-utils";
+import { Interacted, Link, LinkHoverMenu, LinkHoverMenuItem, HoveredRemoteRight, TextTag } from "../bit-components";
+import { anyEntityWith, findChildWithComponent } from "../utils/bit-utils";
+import { isHubsRoomUrl, isLocalHubsAvatarUrl, isLocalHubsSceneUrl, isLocalHubsUrl } from "../utils/media-url-utils";
+import { Text as TroikaText } from "troika-three-text";
+import { handleExitTo2DInterstitial } from "../utils/vr-interstitial";
+import { changeHub } from "../change-hub";
 
 const NULL_EID = 0;
 
@@ -17,10 +21,31 @@ export function linkHoverMenuSystem(world: HubsWorld) {
   const menuObject = world.eid2obj.get(menuEid)!;
 
   // Save Link object eid in LinkHoverMenu when hovered.
-  hoveredEnterQuery(world).forEach((eid: number) => {
+  hoveredEnterQuery(world).forEach(async (eid: number) => {
     LinkHoverMenu.targetObjectRef[menuEid] = eid;
     const targetObject = world.eid2obj.get(eid)!;
     targetObject.add(menuObject);
+
+    const buttonRef = LinkHoverMenu.linkButtonRef[menuEid];
+    let text = findChildWithComponent(world, TextTag, buttonRef)!;
+    let textObj = world.eid2obj.get(text)! as TroikaText;
+    const mayChangeScene = (APP.scene?.systems as any).permissions.canOrWillIfCreator("update_hub");
+    const src = APP.getString(Link.url[eid])!;
+    let hubId;
+    let label = "open link";
+    if (await isLocalHubsAvatarUrl(src)) {
+      label = "use avatar";
+    } else if ((await isLocalHubsSceneUrl(src)) && mayChangeScene) {
+      label = "use scene";
+    } else if ((hubId = await isHubsRoomUrl(src))) {
+      const url = new URL(src);
+      if (url.hash && APP.hub!.hub_id === hubId) {
+        label = "go to";
+      } else {
+        label = "visit room";
+      }
+    }
+    textObj.text = label;
   });
 
   // Check if the cursor it hovered on Link object or Link menu button object.
@@ -30,11 +55,37 @@ export function linkHoverMenuSystem(world: HubsWorld) {
   if (hovered && entityExists(world, linkEid)) {
     // Hovered then make the menu visible and handle clicks if needed.
     menuObject.visible = true;
-    if (clickedMenuItemQuery(world).length > 0) {
-      // TODO: Change the action and label depending on 
-      //       target (eg: switching avatars for avatar url)
-      window.open(APP.getString(Link.url[linkEid])!);
-    }
+    clickedMenuItemQuery(world).forEach(async eid => {
+      const linkEid = LinkHoverMenu.targetObjectRef[menuEid];
+      const src = APP.getString(Link.url[linkEid])!;
+      const exitImmersive = async () => await handleExitTo2DInterstitial(false, () => {}, true);
+
+      const mayChangeScene = (APP.scene?.systems as any).permissions.canOrWillIfCreator("update_hub");
+      let hubId;
+      if (await isLocalHubsAvatarUrl(src)) {
+        const avatarId = new URL(src).pathname.split("/").pop();
+        window.APP.store.update({ profile: { avatarId } });
+        APP.scene!.emit("avatar_updated");
+      } else if ((await isLocalHubsSceneUrl(src)) && mayChangeScene) {
+        APP.scene!.emit("scene_media_selected", src);
+      } else if ((hubId = await isHubsRoomUrl(src))) {
+        const url = new URL(src);
+        if (url.hash && APP.hub!.hub_id === hubId) {
+          // move to waypoint w/o writing to history
+          window.history.replaceState(null, "", window.location.href.split("#")[0] + url.hash);
+        } else if (await isLocalHubsUrl(src)) {
+          const waypoint = url.hash && url.hash.substring(1);
+          // move to new room without page load or entry flow
+          changeHub(hubId, true, waypoint);
+        } else {
+          await exitImmersive();
+          location.href = src;
+        }
+      } else {
+        await exitImmersive();
+        window.open(src);
+      }
+    });
   } else {
     // Not hovered or target object has already been deleted
     // then make the menu invisible and forget the Link object.
