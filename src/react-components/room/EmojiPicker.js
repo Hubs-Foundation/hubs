1d77d42b6828aa2f885ec72fd689e1e1af4f55f2
Nikki Sharpley <nsharpley@mozilla.com>
2023-06-19 17:15:18 -0400

    remove theme from picker as not working

--- src/react-components/room/EmojiPicker.js
+++ src/react-components/room/EmojiPicker.js
@@ -2,9 +2,5 @@ import React from "react";
 import Picker from "emoji-picker-react";
 
 export function EmojiPicker(props) {
-  const {
-    state: { preferences: theme }
-  } = window.APP.store;
-
-  return <Picker {...props} Theme={theme.theme.includes("dark") ? "dark" : "light"} />;
+  return <Picker {...props} emojiStyle="twitter" />;
 }
