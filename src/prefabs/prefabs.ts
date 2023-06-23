55358e72a43c0134735d92d97899ef16f31d982b
John Shaughnessy <john@johnshaughnessy.com>
2023-06-23 14:31:22 -0400

    Check spawn_and_move_media permissions

--- src/prefabs/prefabs.ts
+++ src/prefabs/prefabs.ts
@@ -8,8 +8,22 @@ type CameraPrefabT = () => EntityDef;
 type CubeMediaPrefabT = () => EntityDef;
 type MediaPrefabT = (params: MediaLoaderParams) => EntityDef;
 
+type Permission =
+  | "spawn_camera"
+  | "spawn_and_move_media"
+  | "update_hub"
+  | "pin_objects"
+  | "spawn_emoji"
+  | "amplify_audio"
+  | "fly"
+  | "voice_chat"
+  | "spawn_drawing"
+  | "tweet"
+  | "kick_users"
+  | "mute_users";
+
 export type PrefabDefinition = {
-  permission?: "spawn_camera";
+  permission: Permission;
   template: CameraPrefabT | CubeMediaPrefabT | MediaPrefabT;
 };
 
@@ -17,6 +31,6 @@ export type PrefabName = "camera" | "cube" | "media" | "duck";
 
 export const prefabs = new Map<PrefabName, PrefabDefinition>();
 prefabs.set("camera", { permission: "spawn_camera", template: CameraPrefab });
-prefabs.set("cube", { template: CubeMediaFramePrefab });
-prefabs.set("media", { template: MediaPrefab });
-prefabs.set("duck", { template: DuckPrefab });
+prefabs.set("cube", { permission: "spawn_and_move_media", template: CubeMediaFramePrefab });
+prefabs.set("media", { permission: "spawn_and_move_media", template: MediaPrefab });
+prefabs.set("duck", { permission: "spawn_and_move_media", template: DuckPrefab });
