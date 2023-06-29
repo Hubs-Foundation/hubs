8a0ef4e0701fcd0613eb5070386c73e9e1b81ffc
Nikki Sharpley <nsharpley@mozilla.com>
2023-06-29 14:44:32 -0400

    remove unused useeffect

--- src/react-components/room/UserProfileSidebar.js
+++ src/react-components/room/UserProfileSidebar.js
@@ -1,4 +1,4 @@
-import React, { useCallback, useEffect } from "react";
+import React, { useCallback } from "react";
 import PropTypes from "prop-types";
 import { Sidebar } from "../sidebar/Sidebar";
 import { CloseButton } from "../input/CloseButton";
