e36071fdfecf477e908839c88a7516f1ad340157
kontopoulosdm <kontopoulosdm@gmail.com>
2023-06-29 10:37:52 +0300

    vlmodel

--- src/bit-systems/agent-system.js
+++ src/bit-systems/agent-system.js
@@ -4,68 +4,79 @@ import { FromatNewText, UpdateTextSystem, lowerIndex, raiseIndex } from "./agent
 import { hasComponent } from "bitecs";
 import { PermissionStatus } from "../utils/media-devices-utils";
 
+const startWidth = window.innerWidth;
+const startHeight = window.innerHeight;
+
 const agentQuery = defineQuery([Agent]);
 const enterAgentQuery = enterQuery(agentQuery);
-const params = {
-  format: THREE.DepthFormat,
-  type: THREE.UnsignedShortType
-};
+const orthoScene = new THREE.Scene();
+const planeGeometry = new THREE.PlaneGeometry(startWidth, startHeight);
+const basicMat = new THREE.MeshBasicMaterial();
+const plane = new THREE.Mesh(planeGeometry, basicMat);
+const orthoCamera = new THREE.OrthographicCamera(
+  -startWidth / 2,
+  startWidth / 2,
+  startHeight / 2,
+  -startHeight / 2,
+  0.1,
+  1000
+);
+const orthoRenderTarget = new THREE.WebGLRenderTarget(5, 5);
+const depthRenderTarget = new THREE.WebGLRenderTarget(5, 5);
 
-let target, plane, format, type, depthTexture, postMaterial, mediaRecorder, eid;
+depthRenderTarget.texture.minFilter = THREE.NearestFilter;
+depthRenderTarget.texture.magFilter = THREE.NearestFilter;
+depthRenderTarget.stencilBuffer = THREE.DepthFormat === THREE.DepthStencilFormat ? true : false;
+depthRenderTarget.depthTexture = new THREE.DepthTexture();
+depthRenderTarget.depthTexture.format = parseFloat(THREE.DepthFormat);
+depthRenderTarget.depthTexture.type = parseFloat(THREE.UnsignedShortType);
 
-export let isRecording = false;
+orthoRenderTarget.texture.format = THREE.RGBAFormat;
+orthoRenderTarget.texture.minFilter = THREE.LinearFilter;
 
-function init() {
-  format = parseFloat(params.format);
-  type = parseFloat(params.type);
+orthoCamera.position.z = 5;
+orthoScene.add(plane);
 
-  target = new THREE.WebGLRenderTarget(window.innerWidth, window.innerHeight);
-  target.texture.minFilter = THREE.NearestFilter;
-  target.texture.magFilter = THREE.NearestFilter;
-  target.stencilBuffer = format === THREE.DepthStencilFormat ? true : false;
-  target.depthTexture = new THREE.DepthTexture();
-  target.depthTexture.format = format;
-  target.depthTexture.type = type;
+const postMaterial = new THREE.ShaderMaterial({
+  vertexShader: `
+    varying vec2 vUv;
 
-  depthTexture = target.depthTexture;
-  postMaterial = new THREE.ShaderMaterial({
-    vertexShader: `
-      varying vec2 vUv;
-  
-      void main() {
-        vUv = uv;
-        gl_Position = projectionMatrix * modelViewMatrix * vec4(position, 1.0);
-      }
-    `,
-    fragmentShader: `
-      #include <packing>  
-      varying vec2 vUv;
-      uniform sampler2D tDiffuse;
-      uniform sampler2D tDepth;
-      uniform float cameraNear;
-      uniform float cameraFar;
-  
-      float readDepth( sampler2D depthSampler, vec2 coord ) {
-        float fragCoordZ = texture2D( depthSampler, coord ).x;
-        float viewZ = perspectiveDepthToViewZ( fragCoordZ, cameraNear, cameraFar );
-        return viewZToOrthographicDepth( viewZ, cameraNear, cameraFar );
-      } 
-      
-      void main() {
-        float depth = readDepth(tDepth, vUv);
-        float normalizedDepth = (depth - cameraNear) / (cameraFar - cameraNear);
-        gl_FragColor.rgb = vec3(1.0-depth);
-        gl_FragColor.a = 1.0;
-    }  
-    `,
-    uniforms: {
-      cameraNear: { value: 0.1 },
-      cameraFar: { value: 20 },
-      tDiffuse: { value: null },
-      tDepth: { value: null }
+    void main() {
+      vUv = uv;
+      gl_Position = projectionMatrix * modelViewMatrix * vec4(position, 1.0);
     }
-  });
-}
+  `,
+  fragmentShader: `
+    #include <packing>  
+    varying vec2 vUv;
+    uniform sampler2D tDiffuse;
+    uniform sampler2D tDepth;
+    uniform float cameraNear;
+    uniform float cameraFar;
+
+    float readDepth( sampler2D depthSampler, vec2 coord ) {
+      float fragCoordZ = texture2D( depthSampler, coord ).x;
+      float viewZ = perspectiveDepthToViewZ( fragCoordZ, cameraNear, cameraFar );
+      return viewZToOrthographicDepth( viewZ, cameraNear, cameraFar );
+    } 
+    
+    void main() {
+      float depth = readDepth(tDepth, vUv);
+      float normalizedDepth = (depth - cameraNear) / (cameraFar - cameraNear);
+      gl_FragColor.rgb = vec3(1.0-depth);
+      gl_FragColor.a = 1.0;
+  }  
+  `,
+  uniforms: {
+    cameraNear: { value: 0.1 },
+    cameraFar: { value: 20 },
+    tDiffuse: { value: null },
+    tDepth: { value: null }
+  }
+});
+
+let mediaRecorder, eid, width, height;
+export let isRecording = false;
 
 function clicked(eid) {
   return hasComponent(APP.world, Interacted, eid);
@@ -81,34 +92,114 @@ function setArrows(world, prevArrowEid, nextArrowEid, value) {
 }
 
 function captureDepthPOV() {
+  width = window.innerWidth;
+  height = window.innerHeight;
+
   const renderer = AFRAME.scenes[0].renderer;
   const scene = AFRAME.scenes[0].object3D;
   const camera = AFRAME.scenes[0].camera;
 
-  if (target) target.dispose();
-  scene.remove(plane);
+  orthoRenderTarget.setSize(width, height);
+  depthRenderTarget.setSize(width, height);
 
-  renderer.setRenderTarget(target);
+  if (width / 2 !== orthoCamera.right || height / 2 !== orthoCamera.top) {
+    Object.assign(orthoCamera, {
+      left: -width / 2,
+      right: width / 2,
+      top: height / 2,
+      bottom: -height / 2
+    });
+  }
+
+  plane.geometry.parameters.width = width;
+  plane.geometry.parameters.height = height;
+
+  postMaterial.uniforms.tDiffuse.value = depthRenderTarget.texture;
+  postMaterial.uniforms.tDepth.value = depthRenderTarget.depthTexture;
+  plane.material = postMaterial;
+
+  renderer.setRenderTarget(depthRenderTarget);
   renderer.render(scene, camera);
-
-  var planeGeometry = new THREE.PlaneGeometry(8, 4.5);
-  postMaterial.uniforms.tDiffuse.value = target.texture;
-  postMaterial.uniforms.tDepth.value = target.depthTexture;
-  plane = new THREE.Mesh(planeGeometry, postMaterial);
-  scene.add(plane);
 
   renderer.setRenderTarget(null);
-  renderer.render(scene, camera);
+  renderer.render(orthoScene, orthoCamera);
+
+  const pictureUrl = renderer.domElement.toDataURL("image/png");
+  const link = document.createElement("a");
+
+  link.href = pictureUrl;
+  link.download = "depth.png";
+  link.click();
 }
 
-function snapPOV() {}
+function snapPOV(agentObj) {
+  const renderer = AFRAME.scenes[0].renderer;
+  const scene = AFRAME.scenes[0].object3D;
+  const camera = AFRAME.scenes[0].camera;
 
-// Save POV as image
-// const pictureUrl = renderer.domElement.toDataURL("image/png");
-// const link = document.createElement("a");
-// link.href = pictureUrl;
-// link.download = "camera_pov.png";
-// link.click();
+  agentObj.visible = false;
+  renderer.render(scene, camera);
+
+  const pictureUrl = renderer.domElement.toDataURL("image/png");
+  const canvas = renderer.domElement;
+  canvas.toBlob(blob => {
+    // Step 2: Create FormData object and append the Blob
+    const formData = new FormData();
+    formData.append("file", blob, "camera_pov.png");
+
+    // Step 3: Send the FormData to the API using fetch
+    const apiEndpoint = "https://192.168.169.219:5035/cap_lxmert/"; // Replace with your API endpoint
+
+    fetch(apiEndpoint, {
+      method: "POST",
+      body: formData
+    })
+      .then(response => {
+        if (!response.ok) {
+          throw new Error("Error posting image to API");
+        }
+        return response.json();
+      })
+      .then(data => {
+        // Handle the API response
+        console.log(data);
+      })
+      .catch(error => {
+        // Handle any errors that occur during the fetch request
+        console.error(error);
+      });
+  }, "image/png");
+
+  // const formData = new FormData();
+  // formData.append('file', blob, 'camera_pov.png');
+  // const requestData = { file: pictureUrl };
+
+  // //send to api
+
+  // const apiURL = "https://192.168.169.219:5035/cap_lxmert/";
+
+  // fetch(apiURL, { method: "POST", headers: { "Content-type": "multipart/form-data" }, JSON.stringify(requestData)})
+  //   .then(response => {
+  //     if (response.ok) {
+  //       return response.json();
+  //     } else {
+  //       throw new Error("Error: " + response.status);
+  //     }
+  //   })
+  //   .then(data => {
+  //     const responseText = data.transcriptions[0];
+  //     UpdateTextSystem(world, FromatNewText(responseText));
+  //   })
+  //   .catch(error => {
+  //     console.log(error);
+  //   });
+
+  // const link = document.createElement("a");
+
+  // link.href = pictureUrl;
+  // link.download = "camera_pov.png";
+  // link.click();
+}
 
 function recordUser(world) {
   isRecording = true;
@@ -230,7 +321,8 @@ export function AgentSystem(world) {
   if (clicked(Agent.micRef[eid])) {
     // if (!isRecording) recordUser(world);
     // else stopRecording();
-    // snapPOV();
+
+    snapPOV(agentObj);
     captureDepthPOV();
   }
 
@@ -244,5 +336,3 @@ export function AgentSystem(world) {
 // if (renderArrows) {
 //   hideArrows(world, Agent.prevRef[eid], Agent.nextRef[eid]);
 // } else showArrows(world, Agent.prevRef[eid], Agent.nextRef[eid]);
-
-init();
