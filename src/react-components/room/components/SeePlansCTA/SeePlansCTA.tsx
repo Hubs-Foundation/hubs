8d142887aa9456e55940c60a7470f5ce13eefa15
Nikki Sharpley <nsharpley@mozilla.com>
2023-07-05 13:50:58 -0400

    toolbar plan added

--- src/react-components/room/components/SeePlansCTA/SeePlansCTA.tsx
+++ src/react-components/room/components/SeePlansCTA/SeePlansCTA.tsx
@@ -1,7 +1,7 @@
 import React from "react";
 import { FormattedMessage, defineMessage, useIntl } from "react-intl";
 import styles from "./SeePlansCTA.scss";
-import configs from "../../../../utils/configs";
+import hubsLogo from "../../../../assets/images/hubs-logo.png";
 
 const logoAlt = defineMessage({
   id: "see-plans-cta.logo",
@@ -13,8 +13,8 @@ const SeePlansCTA = () => {
   const logoAltText = intl.formatMessage(logoAlt);
 
   return (
-    <a className={styles.see_plans_button} href="/" target="_blank">
-      <img src={configs.image("logo")} alt={logoAltText} />
+    <a className={styles.see_plans_button} href="https://hubs.mozilla.com/#subscribe" target="_blank">
+      <img className={styles.logo} src={hubsLogo} alt={logoAltText} />
       <FormattedMessage id="see-plans-cta.button" defaultMessage={"See Plans"} />
     </a>
   );
