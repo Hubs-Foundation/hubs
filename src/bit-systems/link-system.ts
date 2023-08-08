881220768981d6f145c8d866c8e342e0dc182490
Manuel Martin <manmartin@mozilla.com>
2023-08-08 16:12:16 +0200

    use coroutines instead of async

--- src/bit-systems/link-system.ts
+++ src/bit-systems/link-system.ts
@@ -1,30 +1,43 @@
-import { defineQuery, enterQuery } from "bitecs";
+import { defineQuery, enterQuery, exitQuery } from "bitecs";
 import type { HubsWorld } from "../app";
 import { Link } from "../bit-components";
 import { isHubsRoomUrl, isLocalHubsAvatarUrl, isLocalHubsSceneUrl, isLocalHubsUrl } from "../utils/media-url-utils";
 import { LinkType } from "../inflators/link";
+import { JobRunner } from "../utils/coroutine-utils";
+import { EntityID } from "../utils/networking-types";
 
 const linkQuery = defineQuery([Link]);
 const linkEnterQuery = enterQuery(linkQuery);
+const linkExitQuery = exitQuery(linkQuery);
 
-export function linkSystem(world: HubsWorld) {
-  linkEnterQuery(world).forEach(async eid => {
-    const mayChangeScene = (APP.scene?.systems as any).permissions.canOrWillIfCreator("update_hub");
-    const src = APP.getString(Link.url[eid])!;
-    let hubId;
-    if (await isLocalHubsAvatarUrl(src)) {
-      Link.type[eid] = LinkType.AVATAR;
-    } else if ((await isLocalHubsSceneUrl(src)) && mayChangeScene) {
-      Link.type[eid] = LinkType.SCENE;
-    } else if ((hubId = await isHubsRoomUrl(src))) {
-      const url = new URL(src);
-      if (url.hash && APP.hub!.hub_id === hubId) {
-        Link.type[eid] = LinkType.WAYPOINT;
-      } else if (await isLocalHubsUrl(src)) {
-        Link.type[eid] = LinkType.ROOM;
-      } else {
-        Link.type[eid] = LinkType.ROOM_URL;
-      }
+function* updateLinkType(world: HubsWorld, eid: EntityID) {
+  const mayChangeScene = (APP.scene?.systems as any).permissions.canOrWillIfCreator("update_hub");
+  const src = APP.getString(Link.url[eid])!;
+  let hubId;
+  if ((yield isLocalHubsAvatarUrl(src)) as boolean) {
+    Link.type[eid] = LinkType.AVATAR;
+  } else if (((yield isLocalHubsSceneUrl(src)) as boolean) && mayChangeScene) {
+    Link.type[eid] = LinkType.SCENE;
+  } else if ((hubId = (yield isHubsRoomUrl(src)) as string)) {
+    const url = new URL(src);
+    if (url.hash && APP.hub!.hub_id === hubId) {
+      Link.type[eid] = LinkType.WAYPOINT;
+    } else if ((yield isLocalHubsUrl(src)) as boolean) {
+      Link.type[eid] = LinkType.ROOM;
+    } else {
+      Link.type[eid] = LinkType.ROOM_URL;
     }
+  }
+}
+
+const jobs = new JobRunner();
+export function linkSystem(world: HubsWorld) {
+  linkEnterQuery(world).forEach(eid => {
+    jobs.stop(eid);
+    jobs.add(eid, () => updateLinkType(world, eid));
   });
+  linkExitQuery(world).forEach(eid => {
+    jobs.stop(eid);
+  });
+  jobs.tick();
 }
