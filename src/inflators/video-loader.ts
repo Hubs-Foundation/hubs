0b28797f10b2bbe9ca3cbb7c001ce657c28b796a
Manuel Martin <manmartin@mozilla.com>
2023-08-09 15:35:03 +0200

    Fix media loader initialization

--- src/inflators/video-loader.ts
+++ src/inflators/video-loader.ts
@@ -1,12 +1,11 @@
 import { HubsWorld } from "../app";
-import { MediaVideo } from "../bit-components";
+import { MediaVideoLoaderData } from "../bit-components";
 import { ProjectionMode } from "../utils/projection-mode";
 import { inflateMediaLoader } from "./media-loader";
-import { VIDEO_FLAGS } from "./video";
 
 export interface VideoLoaderParams {
   src: string;
-  projection: ProjectionMode;
+  projection?: ProjectionMode;
   autoPlay: boolean;
   controls: boolean;
   loop: boolean;
@@ -21,9 +20,5 @@ export function inflateVideoLoader(world: HubsWorld, eid: number, params: VideoL
     isObjectMenuTarget: false
   });
 
-  const { autoPlay, controls, loop, projection } = params;
-  autoPlay !== false && (MediaVideo.flags[eid] |= VIDEO_FLAGS.AUTOPLAY);
-  loop !== false && (MediaVideo.flags[eid] |= VIDEO_FLAGS.LOOP);
-  controls !== false && (MediaVideo.flags[eid] |= VIDEO_FLAGS.CONTROLS);
-  MediaVideo.projection[eid] = APP.getSid(projection !== undefined ? projection : ProjectionMode.FLAT);
+  MediaVideoLoaderData.set(eid, params);
 }
