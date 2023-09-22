57c95bc0a2239374711ccc5ae0efe99ebe27cc0a
Manuel Martin <manmartin@mozilla.com>
2023-09-22 15:38:22 +0200

    Remove hack

--- src/systems/bit-physics.ts
+++ src/systems/bit-physics.ts
@@ -19,24 +19,6 @@ function addPhysicsShapes(world: HubsWorld, physicsSystem: PhysicsSystem, eid: n
   const bodyId = PhysicsShape.bodyId[eid];
   const obj = world.eid2obj.get(eid)!;
   const shape = getShapeFromPhysicsShape(eid);
-
-  // TODO HACK this should be handled in shape generation code or just be unsupported
-  const isSceneObject = !!findAncestorWithComponent(world, SceneRoot, eid);
-  if (isSceneObject && !hasComponent(world, Rigidbody, eid) && shape.fit === CONSTANTS.FIT.ALL) {
-    obj.updateMatrices();
-    const offset = new Vector3().copy(shape.offset as Vector3);
-    const orientation = new Quaternion().copy(shape.orientation as Quaternion);
-    offset.applyMatrix4(obj.matrixWorld);
-    orientation.multiply(obj.getWorldQuaternion(new Quaternion()));
-    shape.offset.x += offset.x;
-    shape.offset.y += offset.y;
-    shape.offset.z += offset.z;
-    shape.orientation.x = orientation.x;
-    shape.orientation.y = orientation.y;
-    shape.orientation.z = orientation.z;
-    shape.orientation.w = orientation.w;
-  }
-
   const shapeId = physicsSystem.addShapes(bodyId, obj, shape);
   PhysicsShape.shapeId[eid] = shapeId;
 }
