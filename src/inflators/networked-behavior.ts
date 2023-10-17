9834bf24a568d40689cb3ef8fd4f75dd03c4dae4
Manuel Martin <manmartin@mozilla.com>
2023-10-17 20:17:50 +0200

    Default parameters for networked variables

--- src/inflators/networked-behavior.ts
+++ src/inflators/networked-behavior.ts
@@ -1,9 +1,20 @@
 import { addComponent } from "bitecs";
-import { Networked, NetworkedBehavior } from "../bit-components";
+import { Networked, NetworkedBehavior, NetworkedBehaviorData } from "../bit-components";
 import { HubsWorld } from "../app";
 
-export function inflateNetworkedBehavior(world: HubsWorld, eid: number): number {
+type NetworkedBehaviorType = {
+  [key: string]: any;
+};
+
+export function inflateNetworkedBehavior(world: HubsWorld, eid: number, params: NetworkedBehaviorType): number {
   addComponent(world, Networked, eid);
   addComponent(world, NetworkedBehavior, eid);
+  if (params) {
+    const data = NetworkedBehaviorData.get(eid) || new Map();
+    for (let key in params) {
+      data.set(key, params[key]);
+      NetworkedBehaviorData.set(eid, data);
+    }
+  }
   return eid;
 }
