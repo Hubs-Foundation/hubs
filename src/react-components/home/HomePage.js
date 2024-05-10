4f4f186f1e93b75a04887565005a192c825698fb
Manuel Martin <manmartin@mozilla.com>
2024-05-10 10:41:46 +0200

    Skip homepage when creating a new room

--- src/react-components/home/HomePage.js
+++ src/react-components/home/HomePage.js
@@ -1,4 +1,4 @@
-import React, { useContext, useEffect } from "react";
+import React, { useContext, useEffect, useMemo, useState } from "react";
 import { FormattedMessage, useIntl } from "react-intl";
 import classNames from "classnames";
 import configs from "../../utils/configs";
@@ -28,12 +28,13 @@ export function HomePage() {
   const { results: favoriteRooms } = useFavoriteRooms();
   const { results: publicRooms } = usePublicRooms();
 
+  const qs = useMemo(() => new URLSearchParams(location.search), []);
+  const [newScene] = useState(qs.has("new"));
+
   const sortedFavoriteRooms = Array.from(favoriteRooms).sort((a, b) => b.member_count - a.member_count);
   const sortedPublicRooms = Array.from(publicRooms).sort((a, b) => b.member_count - a.member_count);
   const wrapInBold = chunk => <b>{chunk}</b>;
   useEffect(() => {
-    const qs = new URLSearchParams(location.search);
-
     // Support legacy sign in urls.
     if (qs.has("sign_in")) {
       const redirectUrl = new URL("/signin", window.location);
@@ -49,142 +50,144 @@ export function HomePage() {
       qs.delete("new");
       createAndRedirectToNewHub(null, null, true, qs);
     }
-  }, []);
+  }, [qs]);
 
   const canCreateRooms = !configs.feature("disable_room_creation") || auth.isAdmin;
   const email = auth.email;
   return (
-    <PageContainer className={styles.homePage}>
-      <Container>
-        <div className={styles.hero}>
-          {auth.isSignedIn ? (
-            <div className={styles.signInContainer}>
-              <span>
-                <FormattedMessage
-                  id="header.signed-in-as"
-                  defaultMessage="Signed in as {email}"
-                  values={{ email: maskEmail(email) }}
-                />
-              </span>
-              <a href="#" onClick={auth.signOut} className={styles.mobileSignOut}>
-                <FormattedMessage id="header.sign-out" defaultMessage="Sign Out" />
-              </a>
+    !newScene && (
+      <PageContainer className={styles.homePage}>
+        <Container>
+          <div className={styles.hero}>
+            {auth.isSignedIn ? (
+              <div className={styles.signInContainer}>
+                <span>
+                  <FormattedMessage
+                    id="header.signed-in-as"
+                    defaultMessage="Signed in as {email}"
+                    values={{ email: maskEmail(email) }}
+                  />
+                </span>
+                <a href="#" onClick={auth.signOut} className={styles.mobileSignOut}>
+                  <FormattedMessage id="header.sign-out" defaultMessage="Sign Out" />
+                </a>
+              </div>
+            ) : (
+              <SignInButton mobile />
+            )}
+            <div className={styles.logoContainer}>
+              <AppLogo />
             </div>
-          ) : (
-            <SignInButton mobile />
-          )}
-          <div className={styles.logoContainer}>
-            <AppLogo />
-          </div>
-          <div className={styles.appInfo}>
-            <div className={styles.appDescription}>{configs.translation("app-description")}</div>
-            {canCreateRooms && <CreateRoomButton />}
-            <PWAButton />
-          </div>
-          <div className={styles.heroImageContainer}>
-            <img
-              alt={intl.formatMessage(
-                {
-                  id: "home-page.hero-image-alt",
-                  defaultMessage: "Screenshot of {appName}"
-                },
-                { appName: configs.translation("app-name") }
-              )}
-              src={configs.image("home_background")}
-            />
-          </div>
-        </div>
-      </Container>
-      {configs.feature("show_feature_panels") && (
-        <Container className={classNames(styles.features, styles.colLg, styles.centerLg)}>
-          <Column padding gap="xl" className={styles.card}>
-            <img src={configs.image("landing_rooms_thumb")} />
-            <h3>
-              <FormattedMessage id="home-page.rooms-title" defaultMessage="Instantly create rooms" />
-            </h3>
-            <p>
-              <FormattedMessage
-                id="home-page.rooms-blurb"
-                defaultMessage="Share virtual spaces with your friends, co-workers, and communities. When you create a room with Hubs, you’ll have a private virtual meeting space that you can instantly share <b>- no downloads or VR headset necessary.</b>"
-                values={{ b: wrapInBold }}
+            <div className={styles.appInfo}>
+              <div className={styles.appDescription}>{configs.translation("app-description")}</div>
+              {canCreateRooms && <CreateRoomButton />}
+              <PWAButton />
+            </div>
+            <div className={styles.heroImageContainer}>
+              <img
+                alt={intl.formatMessage(
+                  {
+                    id: "home-page.hero-image-alt",
+                    defaultMessage: "Screenshot of {appName}"
+                  },
+                  { appName: configs.translation("app-name") }
+                )}
+                src={configs.image("home_background")}
               />
-            </p>
-          </Column>
-          <Column padding gap="xl" className={styles.card}>
-            <img src={configs.image("landing_communicate_thumb")} />
-            <h3>
-              <FormattedMessage id="home-page.communicate-title" defaultMessage="Communicate and Collaborate" />
+            </div>
+          </div>
+        </Container>
+        {configs.feature("show_feature_panels") && (
+          <Container className={classNames(styles.features, styles.colLg, styles.centerLg)}>
+            <Column padding gap="xl" className={styles.card}>
+              <img src={configs.image("landing_rooms_thumb")} />
+              <h3>
+                <FormattedMessage id="home-page.rooms-title" defaultMessage="Instantly create rooms" />
+              </h3>
+              <p>
+                <FormattedMessage
+                  id="home-page.rooms-blurb"
+                  defaultMessage="Share virtual spaces with your friends, co-workers, and communities. When you create a room with Hubs, you’ll have a private virtual meeting space that you can instantly share <b>- no downloads or VR headset necessary.</b>"
+                  values={{ b: wrapInBold }}
+                />
+              </p>
+            </Column>
+            <Column padding gap="xl" className={styles.card}>
+              <img src={configs.image("landing_communicate_thumb")} />
+              <h3>
+                <FormattedMessage id="home-page.communicate-title" defaultMessage="Communicate and Collaborate" />
+              </h3>
+              <p>
+                <FormattedMessage
+                  id="home-page.communicate-blurb"
+                  defaultMessage="Choose an avatar to represent you, put on your headphones, and jump right in. Hubs makes it easy to stay connected with voice and text chat to other people in your private room."
+                />
+              </p>
+            </Column>
+            <Column padding gap="xl" className={styles.card}>
+              <img src={configs.image("landing_media_thumb")} />
+              <h3>
+                <FormattedMessage id="home-page.media-title" defaultMessage="An easier way to share media" />
+              </h3>
+              <p>
+                <FormattedMessage
+                  id="home-page.media-blurb"
+                  defaultMessage="Share content with others in your room by dragging and dropping photos, videos, PDF files, links, and 3D models into your space."
+                />
+              </p>
+            </Column>
+          </Container>
+        )}
+        {sortedPublicRooms.length > 0 && (
+          <Container className={styles.roomsContainer}>
+            <h3 className={styles.roomsHeading}>
+              <FormattedMessage id="home-page.public--rooms" defaultMessage="Public Rooms" />
             </h3>
-            <p>
-              <FormattedMessage
-                id="home-page.communicate-blurb"
-                defaultMessage="Choose an avatar to represent you, put on your headphones, and jump right in. Hubs makes it easy to stay connected with voice and text chat to other people in your private room."
-              />
-            </p>
-          </Column>
-          <Column padding gap="xl" className={styles.card}>
-            <img src={configs.image("landing_media_thumb")} />
-            <h3>
-              <FormattedMessage id="home-page.media-title" defaultMessage="An easier way to share media" />
+            <Column grow padding className={styles.rooms}>
+              <MediaGrid center>
+                {sortedPublicRooms.map(room => {
+                  return (
+                    <MediaTile
+                      key={room.id}
+                      entry={room}
+                      processThumbnailUrl={(entry, width, height) =>
+                        scaledThumbnailUrlFor(entry.images.preview.url, width, height)
+                      }
+                    />
+                  );
+                })}
+              </MediaGrid>
+            </Column>
+          </Container>
+        )}
+        {sortedFavoriteRooms.length > 0 && (
+          <Container className={styles.roomsContainer}>
+            <h3 className={styles.roomsHeading}>
+              <FormattedMessage id="home-page.favorite-rooms" defaultMessage="Favorite Rooms" />
             </h3>
-            <p>
-              <FormattedMessage
-                id="home-page.media-blurb"
-                defaultMessage="Share content with others in your room by dragging and dropping photos, videos, PDF files, links, and 3D models into your space."
-              />
-            </p>
+            <Column grow padding className={styles.rooms}>
+              <MediaGrid center>
+                {sortedFavoriteRooms.map(room => {
+                  return (
+                    <MediaTile
+                      key={room.id}
+                      entry={room}
+                      processThumbnailUrl={(entry, width, height) =>
+                        scaledThumbnailUrlFor(entry.images.preview.url, width, height)
+                      }
+                    />
+                  );
+                })}
+              </MediaGrid>
+            </Column>
+          </Container>
+        )}
+        {isHmc() ? (
+          <Column center>
+            <SocialBar />
           </Column>
-        </Container>
-      )}
-      {sortedPublicRooms.length > 0 && (
-        <Container className={styles.roomsContainer}>
-          <h3 className={styles.roomsHeading}>
-            <FormattedMessage id="home-page.public--rooms" defaultMessage="Public Rooms" />
-          </h3>
-          <Column grow padding className={styles.rooms}>
-            <MediaGrid center>
-              {sortedPublicRooms.map(room => {
-                return (
-                  <MediaTile
-                    key={room.id}
-                    entry={room}
-                    processThumbnailUrl={(entry, width, height) =>
-                      scaledThumbnailUrlFor(entry.images.preview.url, width, height)
-                    }
-                  />
-                );
-              })}
-            </MediaGrid>
-          </Column>
-        </Container>
-      )}
-      {sortedFavoriteRooms.length > 0 && (
-        <Container className={styles.roomsContainer}>
-          <h3 className={styles.roomsHeading}>
-            <FormattedMessage id="home-page.favorite-rooms" defaultMessage="Favorite Rooms" />
-          </h3>
-          <Column grow padding className={styles.rooms}>
-            <MediaGrid center>
-              {sortedFavoriteRooms.map(room => {
-                return (
-                  <MediaTile
-                    key={room.id}
-                    entry={room}
-                    processThumbnailUrl={(entry, width, height) =>
-                      scaledThumbnailUrlFor(entry.images.preview.url, width, height)
-                    }
-                  />
-                );
-              })}
-            </MediaGrid>
-          </Column>
-        </Container>
-      )}
-      {isHmc() ? (
-        <Column center>
-          <SocialBar />
-        </Column>
-      ) : null}
-    </PageContainer>
+        ) : null}
+      </PageContainer>
+    )
   );
 }
