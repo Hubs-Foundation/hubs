0cf1e115f9e7630c470694b56473ffc8518da31f
Matt Hargett <plaztiksyke@gmail.com>
2025-06-23 11:25:17 -0700

    legacy modules import globalThis as a module. provide this shim. tsc will fail unless we manually supply types for pdfjs until we upgrade to latest pdfjs.

---
+++ src/utils/globalThis-shim.js
@@ -0,0 +1,2 @@
+// Shim for libraries that try to import globalThis as a module
+module.exports = globalThis;
--- webpack.config.js
+++ webpack.config.js
@@ -309,6 +309,9 @@ module.exports = async (env, argv) => {
           },
     resolve: {
       alias: {
+        // Handle libraries that try to import globalThis as a module
+        globalThis$: path.resolve(__dirname, "./src/utils/globalThis-shim.js"),
+        
         // aframe and networked-aframe are still using commonjs modules. three and bitecs are peer dependanciees
         // but they are "smart" and have builds for both ESM and CJS depending on if import or require is used.
         // This forces the ESM version to be used otherwise we end up with multiple instances of the libraries,
