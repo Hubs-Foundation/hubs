31cddc59c016af05113faaeceb9b549113c8fffc
Syed Hussain Ather <7979313+HussainAther@users.noreply.github.com>
2026-08-29 12:54:53 -0400

    Fix admin scene editor links

--- src/react-components/if-feature.js
+++ src/react-components/if-feature.js
@@ -1,7 +1,13 @@
-import configs from "../utils/configs";
+import { useContext } from "react";
 import PropTypes from "prop-types";
+import configs from "../utils/configs";
+import { AuthContext } from "./auth/AuthContext";
 
 export default function IfFeature(props) {
+  // configs.feature("enable_spoke") depends on admin state.
+  // Subscribe to auth changes so this component re-renders when that state resolves.
+  useContext(AuthContext);
+
   return (configs.feature(props.name) && props.children) || null;
 }
 
